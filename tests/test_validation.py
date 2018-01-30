27092c747faba13934c95494add8a0ac8623eab6
lukasheinrich <lukas.heinrich@gmail.com>
2018-01-30 12:21:21 +0100

    histosys test

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -21,8 +21,8 @@ def test_validation_1bin_shapesys():
     pdf  = pyhf.hfpdf.hepdata_like(source['bindata']['sig'], source['bindata']['bkg'], source['bindata']['bkgerr'])
     data = source['bindata']['data'] + pdf.auxdata
     muTest = 1.0
-    init_pars  = [1.0]*3 #mu + gam1 + gam2
-    par_bounds = [[0,10]] * 3
+    init_pars  = [1.0]*2 #mu + gam1
+    par_bounds = [[0,10]] * 2
     clsobs, cls_exp = pyhf.runOnePoint(muTest, data,pdf,init_pars,par_bounds)[-2:]
     cls_obs = 1./clsobs
     cls_exp = [1./x for x in cls_exp]
@@ -69,6 +69,59 @@ def test_validation_1bin_normsys():
                         'name': 'bkg_norm',
                         'type': 'normsys',
                         'data': {'lo': 0.90, 'hi': 1.10}
+                    }
+                ]
+            }
+        }
+    }
+    pdf  = pyhf.hfpdf(spec)
+    data = source['bindata']['data'] + pdf.auxdata
+
+    muTest = 1.0
+    init_pars  = [1.0, 0.0] #mu + alpha
+    par_bounds = [[0,10],[-5,5]]
+    clsobs, cls_exp = pyhf.runOnePoint(muTest, data,pdf,init_pars,par_bounds)[-2:]
+    cls_obs = 1./clsobs
+    cls_exp = [1./x for x in cls_exp]
+    assert (cls_obs - expected_result['obs'])/expected_result['obs'] < VALIDATION_TOLERANCE
+    for result,expected_result in zip(cls_exp, expected_result['exp']):
+        assert (result-expected_result)/expected_result < VALIDATION_TOLERANCE
+
+
+def test_validation_2bin_histosys():
+    expected_result = {
+        'obs': 0.09436700514736625,
+        'exp': [
+            8.131143652258812e-06,
+            0.0001396307700293439,
+            0.0020437905684851376,
+            0.022094931468776054,
+            0.14246926685789288,
+        ]
+    }
+    source = json.load(open('validation/data/2bin_histosys_example2.json'))
+    spec = {
+        'singlechannel': {
+            'signal': {
+                'data': source['bindata']['sig'],
+                'mods': [
+                    {
+                        'name': 'mu',
+                        'type': 'normfactor',
+                        'data': None
+                    }
+                ]
+            },
+            'background': {
+                'data': source['bindata']['bkg'],
+                'mods': [
+                    {
+                        'name': 'bkg_norm',
+                        'type': 'histosys',
+                        'data': {
+                            'lo_hist': source['bindata']['bkgsys_dn'],
+                            'hi_hist': source['bindata']['bkgsys_up'],
+                        }
                     }
                 ]
             }
