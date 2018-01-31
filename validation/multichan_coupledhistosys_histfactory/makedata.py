3b3976cfc953492be3f4a77d4322c859dffb833e
lukasheinrich <lukas.heinrich@gmail.com>
2018-01-31 14:44:22 +0100

    add test

---
+++ validation/multichan_coupledhistosys_histfactory/makedata.py
@@ -0,0 +1,26 @@
+import ROOT
+
+import json
+import sys
+source_data = json.load(open(sys.argv[1]))
+root_file   = sys.argv[2]
+
+f = ROOT.TFile(root_file, 'RECREATE')
+
+
+hists = []
+for cname, channel_def in source_data['channels'].iteritems():
+    print ('CH',cname)
+    binning = channel_def['binning']
+    bindata = channel_def['bindata']
+
+
+    for hist, data in bindata.iteritems():
+        print ('{}_{}'.format(cname,hist))
+        h = ROOT.TH1F('{}_{}'.format(cname,hist),'{}_{}'.format(cname,hist),*binning)
+        hists += [h]
+        for i,v in enumerate(data):
+            h.SetBinContent(i+1,v)
+        h.Sumw2()
+
+f.Write()
