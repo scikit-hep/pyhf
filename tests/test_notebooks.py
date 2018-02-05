5e1a6eadb151baeba2e2957b228e7e88a972cced
lukasheinrich <lukas.heinrich@gmail.com>
2018-02-05 14:06:40 +0100

    mote notebook testing via papermill

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -1,5 +1,14 @@
 import os
 import papermill as pm
+import sys
 
 def test_notebooks():
-    pm.execute_notebook('examples/notebooks/ShapeFactor.ipynb','/dev/null', kernel_name='python')
+    common_kwargs = {'output': '/dev/null', 'kernel_name':'python{}'.format(sys.version_info.major)}
+
+    pm.execute_notebook('examples/notebooks/ShapeFactor.ipynb',**common_kwargs)
+    pm.execute_notebook('examples/notebooks/multichannel-coupled-histo.ipynb',parameters = {
+        'validation_datadir': 'validation/data'
+    }, **common_kwargs)
+    pm.execute_notebook('examples/notebooks/multiBinPois.ipynb',parameters = {
+        'validation_datadir': 'validation/data'
+    }, **common_kwargs)
