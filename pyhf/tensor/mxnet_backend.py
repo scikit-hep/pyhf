0e8b5e41a755ead2910fe412daf0a7c5b4d4d1ab
Matthew Feickert <matthew.feickert@cern.ch>
2018-02-11 14:13:39 +0100

    Add MXNet backend stack() and concatenate()
    Other methods are added as well, but these are the two that were the
    most important

--- pyhf/tensor/mxnet_backend.py
+++ pyhf/tensor/mxnet_backend.py
@@ -8,7 +8,7 @@ class mxnet_backend(object):
     """Backend for MXNet"""
 
     def __init__(self, **kwargs):
-        self.session = kwargs.get('session')
+        pass
 
     def tolist(self, tensor_in):
         """
@@ -62,13 +62,27 @@ class mxnet_backend(object):
             MXNet NDArray: ndarray of the sum over the axes
         """
         tensor_in = self.astensor(tensor_in)
-        if axis is None or tensor_in.shape == nd.Size([]):
+        if axis is None or tensor_in.shape == nd.array([]).size:
             return nd.sum(tensor_in)
         else:
             return nd.sum(tensor_in, axis)
 
     def product(self, tensor_in, axis=None):
-        pass
+        """
+        Product of array elements over given axes.
+
+        Args:
+            tensor_in: tensor object
+            axis: the axes over which to take the product
+
+        Returns:
+            MXNet NDArray: ndarray of the product over the axes
+        """
+        tensor_in = self.astensor(tensor_in)
+        if axis is None:
+            return nd.prod(tensor_in)
+        else:
+            return nd.prod(tensor_in, axis)
 
     def ones(self, shape):
         """
@@ -165,19 +179,85 @@ class mxnet_backend(object):
         return nd.exp(tensor_in)
 
     def stack(self, sequence, axis=0):
-        pass
+        """
+        Join a sequence of arrays along a new axis.
+
+        The axis parameter specifies the index of the new axis in the dimensions
+        of the result. For example, if axis=0 it will be the first dimension and
+        if axis=-1 it will be the last dimension.
+
+        Args:
+            sequence: sequence of arrays
+            axis: the axis along which to join the arrays
+
+        Returns:
+            MXNet NDArray: ndarray comprised of the elements of the sequence
+        """
+        return nd.stack(*sequence, axis=axis)
 
     def where(self, mask, tensor_in_1, tensor_in_2):
-        pass
+        """
+        Apply a boolean selection mask to the elements of the input tensors
+
+        Example:
+            >>> mxnet_backend.where(
+                mxnet_backend.astensor([1, 0, 1]),
+                mxnet_backend.astensor([1, 1, 1]),
+                mxnet_backend.astensor([2, 2, 2]))
+            >>> [1. 2. 1.]
+
+        Args:
+            mask: Boolean mask (boolean or tensor object of booleans)
+            tensor_in_1: tensor object
+            tensor_in_2: tensor object
+
+        Returns:
+            MXNet NDArray: The result of the mask being applied to the tensors
+        """
+        mask = self.astensor(mask)
+        tensor_in_1 = self.astensor(tensor_in_1)
+        tensor_in_2 = self.astensor(tensor_in_2)
+        return nd.multiply(mask, tensor_in_1) + nd.multiply(nd.subtract(1, mask), tensor_in_2)
 
     def concatenate(self, sequence):
-        pass
+        """
+        Join the elements of the sequence
+
+        Args:
+            sequence: the sequence of arrays to join
+
+        Returns:
+            MXNet NDArray: The ndarray of the joined elements
+        """
+        return nd.concat(*sequence, dim=0)
 
     def simple_broadcast(self, *args):
-        pass
+        """
+        Does this work?
+        There should be a more MXNet-style way to do this
+        """
+        broadcast = []
+        max_dim = max(map(len, args))
+        for arg in args:
+            broadcast.append(self.astensor(arg)
+                             if len(arg) > 1 else arg * self.ones(max_dim))
+        return broadcast
 
     def poisson(self, n, lam):
-        pass
+        return self.normal(n, lam, self.sqrt(lam))
 
     def normal(self, x, mu, sigma):
-        pass
+        import math
+        from numbers import Number
+        x = self.astensor(x)
+        mu = self.astensor(mu)
+        sigma = self.astensor(sigma)
+        # Is needed?
+        # normal = nd.random.normal(loc=mu, scale=sigma)
+
+        def log_prob(value, loc, scale):
+            variance = scale ** 2
+            log_scale = math.log(scale) if isinstance(
+                scale, Number) else scale.log()
+            return -((value - loc) ** 2) / (2 * variance) - log_scale - math.log(math.sqrt(2 * math.pi))
+        return self.exp(log_prob(x, mu, sigma))
