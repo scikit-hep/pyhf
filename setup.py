8c4985d31be5d788a77bddf2917583764dcebf06
lukasheinrich <lukas.heinrich@gmail.com>
2018-02-13 01:02:04 +0100

    Bump version: 0.0.4 → 0.0.5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
   name = 'pyhf',
-  version = '0.0.4',
+  version = '0.0.5',
   description = '(partial) pure python histfactory implementation',
   url = '',
   author = 'Lukas Heinrich',
