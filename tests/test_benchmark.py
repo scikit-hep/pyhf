f73547b85077694b4840094e9afccc2418ea162c
Matthew Feickert <matthew.feickert@cern.ch>
2018-04-03 10:48:23 +0200

    Benchmark runOnePoint()
    Benchmark runOnePoint() so that a fit is actually done and there will be
    variation in timing.
    
    At the moment only the NumPy and PyTorch backends are benchmarked in the
    test function as there are scaling problems with TensorFlow and the
    MXNet optimizer has not been completed.
    
    The source that is used is just repeating values to ensure
    reproducibility in the CI. Poisson variations with a fixed seed could
    also work, but preliminary testing shows that the NumPy optimizer, which
    does not take advantage of auto differentiation, is not always able to
    complete the fit at high bin values which would result in a failing test.
    Of interest is that PyTorch is able to do so.

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -9,21 +9,22 @@ import numpy as np
 import pytest
 
 
-def generate_source(n_bins):
+def generate_source_static(n_bins):
     """
     Create the source structure for the given number of bins.
+
+    Args:
+        n_bins: `list` of number of bins
+
+    Returns:
+        source
     """
     binning = [n_bins, -0.5, n_bins + 0.5]
-    data = [120.0]
-    bkg = [100.0]
-    bkgerr = [10.0]
-    sig = [30.0]
-    if n_bins > 1:
-        for bin in range(1, n_bins):
-            data.append(np.random.poisson(180.0))
-            bkg.append(np.random.poisson(150.0))
-            bkgerr.append(np.random.poisson(10.0))
-            sig.append(np.random.poisson(95.0))
+    data = [120.0] * n_bins
+    bkg = [100.0] * n_bins
+    bkgerr = [10.0] * n_bins
+    sig = [30.0] * n_bins
+
     source = {
         'binning': binning,
         'bindata': {
@@ -36,33 +37,80 @@ def generate_source(n_bins):
     return source
 
 
-def logpdf(source):
-    pdf = hepdata_like(source['bindata']['sig'],
-                       source['bindata']['bkg'],
-                       source['bindata']['bkgerr'])
-    data = source['bindata']['data'] + pdf.config.auxdata
+def generate_source_poisson(n_bins):
+    """
+    Create the source structure for the given number of bins.
+    Sample from a Poisson distribution
 
-    return pdf.logpdf(pdf.config.suggested_init(), data)
+    Args:
+        n_bins: `list` of number of bins
+
+    Returns:
+        source
+    """
+    np.random.seed(0)  # Fix seed for reproducibility
+    binning = [n_bins, -0.5, n_bins + 0.5]
+    data = np.random.poisson(120.0, n_bins).tolist()
+    bkg = np.random.poisson(100.0, n_bins).tolist()
+    bkgerr = np.random.poisson(10.0, n_bins).tolist()
+    sig = np.random.poisson(30.0, n_bins).tolist()
+
+    source = {
+        'binning': binning,
+        'bindata': {
+            'data': data,
+            'bkg': bkg,
+            'bkgerr': bkgerr,
+            'sig': sig
+        }
+    }
+    return source
 
 
-bins = [1, 2, 3, 4, 5, 10, 15, 20, 25, 50, 100]
+def runOnePoint(pdf, data):
+    return pyhf.runOnePoint(1.0, data, pdf,
+                            pdf.config.suggested_init(),
+                            pdf.config.suggested_bounds())
+
+
+# bins = [1, 10, 50, 100, 200, 500, 800, 1000]
+bins = [1, 10, 50, 100, 200]
 bin_ids = ['{}_bins'.format(n_bins) for n_bins in bins]
 
 
 @pytest.mark.parametrize('n_bins', bins, ids=bin_ids)
 @pytest.mark.parametrize('backend', [numpy_backend(poisson_from_normal=True),
-                                     tensorflow_backend(session=tf.Session()),
+                                     # tensorflow_backend(session=tf.Session()),
                                      pytorch_backend(),
-                                     mxnet_backend()],
-                         ids=['numpy', 'tensorflow', 'pytorch', 'mxnet'])
-def test_logpdf(benchmark, backend, n_bins):
+                                     # mxnet_backend(),
+                                     ],
+                         ids=['numpy',
+                              # 'tensorflow',
+                              'pytorch',
+                              # 'mxnet',
+                              ])
+def test_runOnePoint(benchmark, backend, n_bins):
     """
-    Benchmark the performance of simplemodels.hepdata_like().logpdf()
+    Benchmark the performance of pyhf.runOnePoint()
     for various numbers of bins and different backends
+
+    Args:
+        benchmark: pytest benchmark
+        backend: `pyhf` tensorlib given by pytest parameterization
+        n_bins: `list` of number of bins given by pytest parameterization
+
+    Returns:
+        None
     """
     default_backend = pyhf.tensorlib
-    pyhf.tensorlib = backend
-    source = generate_source(n_bins)
-    assert benchmark(logpdf, source) is not None
+    pyhf.set_backend(backend)
+
+    source = generate_source_static(n_bins)
+    pdf = hepdata_like(source['bindata']['sig'],
+                       source['bindata']['bkg'],
+                       source['bindata']['bkgerr'])
+    data = source['bindata']['data'] + pdf.config.auxdata
+    assert benchmark(runOnePoint, pdf, data) is not None
+
     # Reset backend
-    pyhf.tensorlib = default_backend
+    pyhf.set_backend(default_backend)
