3a04452834ac2b1bda72fa83e04af2f03bb4cd28
Giordon Stark <kratsg@gmail.com>
2018-04-14 08:24:55 -0500

    test_import works now

--- tests/test_import.py
+++ tests/test_import.py
@@ -10,12 +10,14 @@ def test_import_prepHistFactory():
     data = [binvalue for k in pdf.config.channel_order for binvalue
             in spec['data'][k]] + pdf.config.auxdata
 
+    samples = {'channel1': [sample['name'] for sample in spec['channels']['channel1']['samples']]}
+
     assert data == [122.0, 112.0, 0, 0, 0]
 
     assert 'channel1' in spec['channels']
-    assert 'signal' in spec['channels']['channel1']
-    assert 'background1' in spec['channels']['channel1']
-    assert 'background2' in spec['channels']['channel1']
+    assert 'signal' in samples['channel1']
+    assert 'background1' in samples['channel1']
+    assert 'background2' in samples['channel1']
 
     assert pdf.expected_actualdata(
         pdf.config.suggested_init()).tolist() == [120.0, 110.0]
