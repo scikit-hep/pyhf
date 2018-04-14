b106c2f908d2cc3ead580579d0e69a7bfb108908
Giordon Stark <kratsg@gmail.com>
2018-04-14 10:49:05 -0500

    update simplemodels

--- pyhf/simplemodels.py
+++ pyhf/simplemodels.py
@@ -2,23 +2,26 @@ from . import hfpdf
 
 def hepdata_like(signal_data, bkg_data, bkg_uncerts):
     spec = {
-        'singlechannel': {
-            'samples': [
-                {
-                    'name': 'signal',
-                    'data': signal_data,
-                    'modifiers': [
-                        {'name': 'mu', 'type': 'normfactor', 'data': None}
-                    ]
-                },
-                {
-                    'name': 'background',
-                    'data': bkg_data,
-                    'modifiers': [
-                        {'name': 'uncorr_bkguncrt', 'type': 'shapesys', 'data': bkg_uncerts}
-                    ]
-                }
-            ]
-        }
+        'channels': [
+            {
+                'name': 'singlechannel',
+                'samples': [
+                    {
+                        'name': 'signal',
+                        'data': signal_data,
+                        'modifiers': [
+                            {'name': 'mu', 'type': 'normfactor', 'data': None}
+                        ]
+                    },
+                    {
+                        'name': 'background',
+                        'data': bkg_data,
+                        'modifiers': [
+                            {'name': 'uncorr_bkguncrt', 'type': 'shapesys', 'data': bkg_uncerts}
+                        ]
+                    }
+                ]
+            }
+        ]
     }
     return hfpdf(spec)
