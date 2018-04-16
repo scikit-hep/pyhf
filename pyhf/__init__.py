53c45aaa57ea38faf4c2befeeef71c41fd8d39f3
Giordon Stark <kratsg@gmail.com>
2018-04-15 22:46:30 -0500

    bad rebase, remove extra

--- pyhf/__init__.py
+++ pyhf/__init__.py
@@ -2,36 +2,6 @@ import logging
 import pyhf.optimize as optimize
 import pyhf.tensor as tensor
 
-from .tensor.numpy_backend import numpy_backend
-from .optimize.opt_scipy import scipy_optimizer
-try:
-    from .tensor.pytorch_backend import pytorch_backend
-    from .optimize.opt_pytorch import pytorch_optimizer
-    assert pytorch_backend
-    assert pytorch_optimizer
-except ImportError:
-    pass
-
-try:
-    from .tensor.tensorflow_backend import tensorflow_backend
-    from .optimize.opt_tflow import tflow_optimizer
-    assert tensorflow_backend
-    assert tflow_optimizer
-except ImportError:
-    pass
-
-try:
-    from .tensor.mxnet_backend import mxnet_backend
-    # from .optimize.opt_mxnet import mxnet_optimizer
-    assert mxnet_backend
-    # assert mxnet_optimizer
-except ImportError:
-    pass
-
-
-tensorlib = numpy_backend()
-optimizer = scipy_optimizer()
-
 log = logging.getLogger(__name__)
 tensorlib = tensor.numpy_backend()
 default_backend = tensorlib
