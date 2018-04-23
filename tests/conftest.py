e6a955da511edd03a8d22659e8372815b322afd3
Giordon Stark <kratsg@gmail.com>
2018-04-23 08:45:59 -0500

    add test import for MBJ

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,3 +5,16 @@ import pyhf
 def reset_backend():
   yield reset_backend
   pyhf.set_backend(pyhf.default_backend)
+
+import pytest
+def pytest_addoption(parser):
+    parser.addoption("--run-slow", action="store_true", default=False, help="run slow tests")
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("--run-slow"):
+        # --runslow given in cli: do not skip slow tests
+        return
+    skip_slow = pytest.mark.skip(reason="need --run-slow option to run")
+    for item in items:
+        if "slow" in item.keywords:
+            item.add_marker(skip_slow)
