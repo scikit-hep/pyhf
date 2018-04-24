f26ed702ca30db523fe5ec461be518c9b7ac21ff
Matthew Feickert <matthew.feickert@cern.ch>
2018-04-24 17:01:50 +0200

    Add normal_cdf() to TensorFlow backend

--- pyhf/tensor/tensorflow_backend.py
+++ pyhf/tensor/tensorflow_backend.py
@@ -116,5 +116,29 @@ class tensorflow_backend(object):
         x = self.astensor(x)
         mu = self.astensor(mu)
         sigma = self.astensor(sigma)
-        normal = tf.distributions.Normal(mu,sigma)
+        normal = tf.distributions.Normal(mu, sigma)
         return normal.prob(x)
+
+    def normal_cdf(self, x, mu=0, sigma=1):
+        """
+        The cumulative distribution function for the Normal distribution
+
+        Example::
+
+            >>> pyhf.tensorlib.normal_cdf(0.8)
+            0.7881446
+
+        Args:
+            x (`tensor` or `float`): The observed value of the random variable
+                                      to evaluate the CDF for
+            mu (`tensor` or `float`): The mean of the Normal distribution
+            sigma (`tensor` or `float`): The standard deviation of the Normal distribution
+
+        Returns:
+            TensorFlow Tensor: The CDF
+        """
+        x = self.astensor(x)
+        mu = self.astensor(mu)
+        sigma = self.astensor(sigma)
+        normal = tf.distributions.Normal(mu, sigma)
+        return normal.cdf(x)
