b9b294cb0d41a06d737b961127d120d2c3607b6e
Giordon Stark <kratsg@gmail.com>
2018-08-23 16:19:10 -0700

    workspace to basedir

--- pyhf/commandline.py
+++ pyhf/commandline.py
@@ -11,11 +11,11 @@ def pyhf():
     pass
 
 @pyhf.command()
-@click.option('--entrypoint-xml', required=True, prompt='Top-level XML', help='The top-level XML file for the workspace definition.', type=click.Path(exists=True))
-@click.option('--workspace', required=True, prompt='Workspace directory', help='The location of workspace.', type=click.Path(exists=True))
+@click.option('--entrypoint-xml', required=True, prompt='Top-level XML', help='The top-level XML file for the PDF definition.', type=click.Path(exists=True))
+@click.option('--basedir', required=True, prompt='Base directory', help='The base directory for the XML files to point relative to.', type=click.Path(exists=True))
 @click.option('--output-file', required=True, prompt='Output file', help='The location of the output json file. If not specified, prints to screen.')
 @click.option('--tqdm/--no-tqdm', default=True)
-def xml2json(entrypoint_xml, workspace, output_file, tqdm):
-    spec = readxml.parse(entrypoint_xml, workspace, enable_tqdm=tqdm)
+def xml2json(entrypoint_xml, basedir, output_file, tqdm):
+    spec = readxml.parse(entrypoint_xml, basedir, enable_tqdm=tqdm)
     json.dump(spec, open(output_file, 'w+'), indent=4, sort_keys=True)
     log.info("Written to {0:s}".format(output_file))
