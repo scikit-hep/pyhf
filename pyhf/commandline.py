2c12f0929e7225b7598486a71e96652430dad53c
Giordon Stark <kratsg@gmail.com>
2018-08-23 10:48:26 -0700

    no need for checking if path exists for output json file

--- pyhf/commandline.py
+++ pyhf/commandline.py
@@ -9,7 +9,7 @@ from . import readxml
 @click.command()
 @click.option('--entrypoint-xml', required=True, prompt='Top-level XML', help='The top-level XML file for the workspace definition.', type=click.Path(exists=True))
 @click.option('--workspace', required=True, prompt='Workspace directory', help='The location of workspace.', type=click.Path(exists=True))
-@click.option('--output-file', required=True, prompt='Output file', help='The location of the output json file. If not specified, prints to screen.', type=click.Path(exists=False))
+@click.option('--output-file', required=True, prompt='Output file', help='The location of the output json file. If not specified, prints to screen.')
 def xml2json(entrypoint_xml, workspace, output_file=None):
     spec = readxml.parse(entrypoint_xml, workspace, enable_tqdm=True)
     json.dump(spec, open(output_file, 'w+'), indent=4, sort_keys=True)
