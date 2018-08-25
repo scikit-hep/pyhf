45f74df0a0daf276e13c94fc9b4f11de7c3a0e2d
lukasheinrich <lukas.heinrich@gmail.com>
2018-08-25 16:59:03 +0200

    remove hardcoded channel

--- pyhf/commandline.py
+++ pyhf/commandline.py
@@ -38,7 +38,7 @@ def cls(workspace,output_file):
     if len(measurements) > 1:
         log.warning('multiple measurements definded. Taking the first %s', measurements[0]['name'])
     p = Model({'channels':d['channels']}, poiname=measurements[0]['config']['poi'])
-    result = runOnePoint(1.0, d['data']['channel1'] + p.config.auxdata, p)
+    result = runOnePoint(1.0, sum((d['data'][c['name']] for c in d['channels']),[]) + p.config.auxdata, p)
     result = {'CLs_obs': result[-2].tolist()[0], 'CLs_exp': result[-1].ravel().tolist()}
     if output_file is None:
         print(json.dumps(result, indent=4, sort_keys=True))
