47c032e9c9ddc5f2a6813c3cdf0d57d3154edfe1
Lukas <lukas.heinrich@gmail.com>
2018-09-19 11:23:36 +0200

    add interesting validation case (#272)
    * add interesting validation case

---
+++ validation/run_single.py
@@ -0,0 +1,42 @@
+import ROOT
+import sys
+
+infile = sys.argv[1]
+
+infile = ROOT.TFile.Open(infile)
+workspace = infile.Get("combined")
+data = workspace.data("obsData")
+
+sbModel = workspace.obj("ModelConfig")
+poi = sbModel.GetParametersOfInterest().first()
+
+sbModel.SetSnapshot(ROOT.RooArgSet(poi))
+
+bModel = sbModel.Clone()
+bModel.SetName("bonly")
+poi.setVal(0)
+bModel.SetSnapshot(ROOT.RooArgSet(poi))
+
+ac = ROOT.RooStats.AsymptoticCalculator(data, bModel, sbModel)
+ac.SetPrintLevel(10)
+ac.SetOneSided(True)
+ac.SetQTilde(True)
+
+
+calc = ROOT.RooStats.HypoTestInverter(ac)
+calc.SetConfidenceLevel(0.95)
+calc.UseCLs(True)
+calc.RunFixedScan(1,1,1)
+
+result = calc.GetInterval()
+
+index = 0
+
+w = result.GetExpectedPValueDist(index)
+v = w.GetSamplingDistribution()
+
+CLs_obs = result.CLs(index)
+CLs_exp = list(v)[3:-3]
+
+import json
+print(json.dumps({'CLs_obs': CLs_obs, 'CLs_exp': CLs_exp}, sort_keys=True, indent=4))
---
+++ validation/shared_nuispar_across_types/make_data.py
@@ -0,0 +1,22 @@
+import ROOT
+
+sig = 'sig',[3,1]
+nom = 'nom',[12,13]
+
+histo_up  = 'hup',[14,15]
+histo_dn  = 'hdn',[10,11]
+
+data = 'data',[15,16]
+
+import ROOT
+f = ROOT.TFile.Open('data.root','recreate')
+
+
+for n,h in [sig,nom,histo_up,histo_dn,data]:
+  rh = ROOT.TH1F(n,n,2,-0.5,1.5)
+  for i,c in enumerate(h):
+    rh.SetBinContent(1+i,c)
+  rh.Sumw2()
+  rh.Write()
+
+f.Close()
