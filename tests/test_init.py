01527c750e66278e1ab91101a08e23b1f262fc40
Giordon Stark <kratsg@gmail.com>
2018-09-19 19:56:10 -0700

    get coverage up finally

---
+++ tests/test_init.py
@@ -0,0 +1,27 @@
+import pytest
+import sys
+import copy
+
+@pytest.mark.parametrize("param", [
+        ["numpy", "numpy_backend", pytest.raises(ImportError)],
+        ["torch", "pytorch_backend", pytest.raises(AttributeError)],
+        ["tensorflow", "tensorflow_backend", pytest.raises(AttributeError)],
+        ["mxnet", "mxnet_backend", pytest.raises(AttributeError)],
+    ],
+    ids=["numpy", "pytorch", "tensorflow", "mxnet"])
+def test_missing_backends(param):
+    backend_name, module_name, expectation = param
+
+    # delete all of pyhf to force a reload
+    for k in sys.modules.keys():
+        if k.startswith('pyhf'): del sys.modules[k]
+
+    # hide
+    CACHE_BACKEND, sys.modules[backend_name] = sys.modules[backend_name], None
+
+    with expectation:
+        import pyhf.tensor
+        getattr(pyhf.tensor, module_name)
+
+    # put back
+    CACHE_BACKEND, sys.modules[backend_name] = None, CACHE_BACKEND
