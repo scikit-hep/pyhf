1aea83d7920a2ffaf012b202dce3666dc03a4836
Matthew Feickert <matthew.feickert@cern.ch>
2018-09-21 03:39:50 +0200

    Run Selenium's Chrome in headless mode
    Remove geckodriver as using Chrome and run in a seperate stage to tests

--- binder/trigger_binder.py
+++ binder/trigger_binder.py
@@ -1,19 +1,30 @@
 #!/usr/bin/env python
 
 import argparse
+import time
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
 
 
 def main(args):
-    driver = webdriver.Firefox()
+    options = Options()
+    options.set_headless()
+    options.add_argument('--no-sandbox')
+    if args.chromedriver_path is not None:
+        print('Chrome Driver path: {}'.format(args.chromedriver_path))
+        driver = webdriver.Chrome(args.chromedriver_path, chrome_options=options)
+    else:
+        driver = webdriver.Chrome(chrome_options=options)
+    print('Chrome Headless Browser Invoked')
     driver.get(args.url)
-    WebDriverWait(driver, 10)
+    time.sleep(10)
     driver.close()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--chromedriver-path', dest='chromedriver_path',
+                        type=str, default=None, help='Path to ChromeDriver')
     parser.add_argument('--url', dest='url',
                         type=str, default=None, help='URL for Selinium to open')
     args = parser.parse_args()
