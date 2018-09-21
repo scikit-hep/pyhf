2ab3332df5816b4ae76ee90a6492718bef8cd1e9
Matthew Feickert <matthew.feickert@cern.ch>
2018-09-21 13:33:10 +0200

    Add verbose flag to trigger_binder.py

--- binder/trigger_binder.py
+++ binder/trigger_binder.py
@@ -11,11 +11,11 @@ def main(args):
     options.set_headless()
     options.add_argument('--no-sandbox')
     if args.chromedriver_path is not None:
-        print('Chrome Driver path: {}'.format(args.chromedriver_path))
         driver = webdriver.Chrome(args.chromedriver_path, chrome_options=options)
     else:
         driver = webdriver.Chrome(chrome_options=options)
-    print('Chrome Headless Browser Invoked')
+    if args.is_verbose:
+        print('Chrome Headless Browser Invoked')
     driver.get(args.url)
     time.sleep(10)
     driver.close()
@@ -23,8 +23,11 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--verbose', dest='is_verbose',
+                        action='store_true',
+                        help='Print out more information')
     parser.add_argument('--chromedriver-path', dest='chromedriver_path',
-                        type=str, default=None, help='Path to ChromeDriver')
+                        type=str, default=None, help='System path to ChromeDriver')
     parser.add_argument('--url', dest='url',
                         type=str, default=None, help='URL for Selinium to open')
     args = parser.parse_args()
