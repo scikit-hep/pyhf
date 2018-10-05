67991ac3b35e8af58a09879c9efb08fef58ba2f0
Giordon Stark <kratsg@users.noreply.github.com>
2018-10-05 11:00:39 -0700

    Update optimizers to allow for custom iterations for constrained fits (#291)
    * Introduces relaxation and use-defined settings to ML optimizers

--- pyhf/optimize/opt_tflow.py
+++ pyhf/optimize/opt_tflow.py
@@ -1,8 +1,15 @@
+import logging
 import numpy as np
 import tensorflow as tf
+
+log = logging.getLogger(__name__)
+
 class tflow_optimizer(object):
     def __init__(self, tensorlib):
         self.tb = tensorlib
+        self.relax = 0.1
+        self.maxit = 1000
+        self.eps = 1e-4
 
     def unconstrained_bestfit(self, objective, data, pdf, init_pars, par_bounds):
         #the graph
@@ -18,11 +25,12 @@ class tflow_optimizer(object):
 
         #run newton's method
         best_fit = init_pars
-        for i in range(1000):
+        for i in range(self.maxit):
             up = self.tb.session.run(update, feed_dict={pars: best_fit})
-            best_fit = best_fit-up
-            if np.abs(np.max(up)) < 1e-4:
+            best_fit = best_fit-self.relax*up
+            if np.abs(np.max(up)) < self.eps:
                 break
+
         return best_fit.tolist()
 
     def constrained_bestfit(self, objective, constrained_mu, data, pdf, init_pars, par_bounds):
@@ -42,11 +50,12 @@ class tflow_optimizer(object):
 
         #run newton's method
         best_fit_nuis = [x for i,x in enumerate(init_pars) if i!= pdf.config.poi_index]
-        for i in range(1000):
+        for i in range(self.maxit):
             up = self.tb.session.run(update, feed_dict={nuis_cat: best_fit_nuis})
-            best_fit_nuis = best_fit_nuis-up
-            if np.abs(np.max(up)) < 1e-4:
+            best_fit_nuis = best_fit_nuis-self.relax*up
+            if np.abs(np.max(up)) < self.eps:
                 break
+
         best_fit = best_fit_nuis.tolist()
         best_fit.insert(pdf.config.poi_index,constrained_mu)
         return best_fit
