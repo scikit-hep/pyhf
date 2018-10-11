35c72c7220c4bc665460948f3b5ad69f18a4ce8d
Lukas <lukas.heinrich@gmail.com>
2018-10-11 23:20:07 +0200

    Get rid of modifier.constraint terms, only use parsets (#309)
    This removes the modifier.constraint everywhere and uses parameter sets for notation.

--- pyhf/modifiers/shapesys.py
+++ pyhf/modifiers/shapesys.py
@@ -9,24 +9,23 @@ class shapesys(object):
     def __init__(self, nom_data, modifier_data):
         self.n_parameters = len(nom_data)
 
-        self.bkg_over_db_squared = []
+        bkg_over_db_squared = []
         for b, deltab in zip(nom_data, modifier_data):
             bkg_over_bsq = b * b / deltab / deltab  # tau*b
             log.info('shapesys for b,delta b (%s, %s) -> tau*b = %s',
                      b, deltab, bkg_over_bsq)
-            self.bkg_over_db_squared.append(bkg_over_bsq)
+            bkg_over_db_squared.append(bkg_over_bsq)
 
-        self.constraint = constrained_by_poisson(
+        self.parset = constrained_by_poisson(
             n_parameters = self.n_parameters,
             inits = [1.0] * self.n_parameters,
             bounds = [[0., 10.]] * self.n_parameters,
-            auxdata = self.bkg_over_db_squared,
-            factors = self.bkg_over_db_squared
+            auxdata = bkg_over_db_squared,
+            factors = bkg_over_db_squared
         )
-        self.parset = self.constraint
 
-        assert self.n_parameters == self.constraint.n_parameters
-        assert self.pdf_type == self.constraint.pdf_type
+        assert self.n_parameters == self.parset.n_parameters
+        assert self.pdf_type == self.parset.pdf_type
 
     def add_sample(self, channel, sample, modifier_def):
         pass
