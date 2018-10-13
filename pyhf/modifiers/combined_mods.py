af26b308cfe060038bba7bec1a0c312854751a39
lukasheinrich <lukas.heinrich@gmail.com>
2018-10-13 14:31:29 +0200

    remove .channel

--- pyhf/modifiers/combined_mods.py
+++ pyhf/modifiers/combined_mods.py
@@ -33,7 +33,8 @@ class normsys_combinedmod(object):
             self.parindices[pdfconfig.par_slice(m)] for m in normsys_mods
         ], dtype='int')
 
-        self.interpolator = _hfinterpolator_code1(self.normsys_histoset)
+        if len(normsys_mods):
+            self.interpolator = _hfinterpolator_code1(self.normsys_histoset)
 
 
     def apply(self,pars):
@@ -79,7 +80,8 @@ class histosys_combinedmod(object):
             self.parindices[pdfconfig.par_slice(m)] for m in histosys_mods
         ], dtype='int')
 
-        self.interpolator = _hfinterpolator_code0(self.histosys_histoset)
+        if len(histosys_mods):
+            self.interpolator = _hfinterpolator_code0(self.histosys_histoset)
 
     def apply(self,pars):
         tensorlib, _ = get_backend()
@@ -138,6 +140,7 @@ class staterror_combined(object):
         }
 
         parindices = list(range(len(pdfconfig.suggested_init())))
+        self.parindices = parindices
 
         self._staterror_mask = default_backend.astensor([
             [
@@ -147,45 +150,49 @@ class staterror_combined(object):
                 for s in samples
             ] for m in staterr_mods
         ])
-        self._staterror_default = default_backend.ones(default_backend.shape(self._staterror_mask))
+        self._staterror_default = default_backend.ones(
+            default_backend.shape(self._staterror_mask)
+        )
 
         stat_parslices  = [pdfconfig.par_slice(m) for m in staterr_mods]
-        stat_targetind  = [channel_slice_map[pdfconfig.modifier(m).channel] for m in staterr_mods]
-
-        default_row =  [1.]*default_backend.shape(self._staterror_default)[-1]
-        befores     = []
-        afters      = []
-        for sl,t in zip(stat_parslices,stat_targetind):
-            before = default_backend.astensor(default_row[:t[0]])
-            after  = default_backend.astensor(default_row[t[-1]+1:])
-            befores.append(before)
-            afters.append(after)
+        self.stat_parslices = stat_parslices
         if stat_parslices:
             tensorlib, _ = get_backend()
-            factor_row_indices = default_backend.tolist(default_backend.stack([
-                default_backend.concatenate([b,default_backend.astensor(parindices[sl]),a])
-                for b,sl,a in zip(befores,stat_parslices,afters)
-            ]))
-            self.factor_row_indices = tensorlib.astensor(factor_row_indices,dtype='int')
-            self.default_value = tensorlib.astensor([1.])
+
+            factor_access_indices = default_backend.concatenate([
+                    default_backend.where(
+                        default_backend.sum(msk,axis=0) > 0,
+                        default_backend.ones(msk[0].shape)*self.parindices[sl][0],
+                        -default_backend.ones(msk[0].shape)
+                )
+                for msk,sl in zip(self._staterror_mask,stat_parslices)
+            ])
+
+            self.factor_access_indices = tensorlib.astensor(factor_access_indices,dtype='int')
+            self.default_value = tensorlib.astensor([1.0])
             self.sample_ones   = tensorlib.ones(len(samples))
             self.alpha_ones    = tensorlib.astensor([1])
             self.staterror_mask = default_backend.astensor(default_backend.tolist(self._staterror_mask))
             self.staterror_default = default_backend.astensor(default_backend.tolist(self._staterror_default))
         else:
-            self.factor_row_indices = None
+            self.factor_access_indices = None
 
 
     def apply(self,pars):
         tensorlib, _ = get_backend()
-        if self.factor_row_indices is None:
+        if self.factor_access_indices is None:
             return
-        factor_row = tensorlib.gather(tensorlib.concatenate([pars,self.default_value]),self.factor_row_indices)
+        select_from = tensorlib.concatenate([pars,self.default_value])
+        factor_row = tensorlib.gather(
+            select_from,self.factor_access_indices
+        )
+
         results_staterr = tensorlib.einsum('s,a,mb->msab',
                 self.sample_ones,
                 self.alpha_ones,
                 factor_row
         )
+
         results_staterr = tensorlib.where(
             self.staterror_mask,
             results_staterr,
@@ -223,28 +230,24 @@ class shapesys_combined(object):
         self._shapesys_default = default_backend.ones(self._shapesys_mask.shape)
 
         shapesys_parslices  = [pdfconfig.par_slice(m) for m in shapesys_mods]
-        shapesys_targetind  = [
-            channel_slice_map[pdfconfig.modifier(m).channel] for m in shapesys_mods
-        ]
-        default_row =  [1.]*self._shapesys_default.shape[-1]
-
-        befores = []
-        afters = []
-        for sl,t in zip(shapesys_parslices,shapesys_targetind):
-            before = default_backend.astensor(default_row[:t[0]])
-            after  = default_backend.astensor(default_row[t[-1]+1:])
-            befores.append(before)
-            afters.append(after)
 
         if shapesys_parslices:
             tensorlib, _ = get_backend()
-            factor_indices = default_backend.tolist(default_backend.stack([
-                default_backend.concatenate([b,default_backend.astensor(parindices[sl]),a])
-                for b,sl,a in zip(befores,shapesys_parslices,afters)
-            ]))
+
+            factor_access_indices = default_backend.concatenate([
+                    default_backend.where(
+                        default_backend.sum(msk,axis=0) > 0,
+                        default_backend.ones(msk[0].shape)*self.parindices[sl][0],
+                        -default_backend.ones(msk[0].shape)
+                )
+                for msk,sl in zip(self._shapesys_mask,shapesys_parslices)
+            ])
+
+
+
             self.sample_ones = tensorlib.ones(len(samples))
             self.alpha_ones = tensorlib.astensor([1])
-            self.factor_row_indices = tensorlib.astensor(factor_indices,dtype='int')
+            self.factor_access_indices = tensorlib.astensor(factor_access_indices,dtype='int')
             self.default_value = tensorlib.astensor([1.])
         else:
             self.factor_row_indices = None
