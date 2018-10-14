470b75f069644d658b4f2fbab13450f38a2d48ed
Giordon Stark <kratsg@gmail.com>
2018-10-14 00:09:43 -0700

    change up to allow for enabling/disabling events, rename things, and remove the module screwy stuff

--- pyhf/events.py
+++ pyhf/events.py
@@ -1,56 +1,54 @@
-import sys
+import logging
+log = logging.getLogger(__name__)
 
-# this is meant to wrap the Wrapper object
-class Wrapper(object):
-    __events = {}
-    class Events(list):
-        def __call__(self, *args, **kwargs):
-            for f in self:
-                f(*args, **kwargs)
+__events = {}
+__disabled_events = set([])
+class Events(list):
+    def __call__(self, *args, **kwargs):
+        for f in self:
+            f(*args, **kwargs)
 
-        def __repr__(self):
-            return "Events(%s)" % list.__repr__(self)
+    def __repr__(self):
+        return "Events(%s)" % list.__repr__(self)
 
-    def __init__(self, name, filepath):
-        import logging
-        self.log = logging.getLogger(name)
-        self.__name__ = name
-        self.__file__ = filepath
-        self.prefix = 'trigger_'
+"""
 
-    """
-    If we reach this, we already know the attribute is missing...
-    """
-    def __getattr__(self, name):
-        def noop(*args, **kwargs): pass
-        if name.startswith(self.prefix):
-          self.log.error('Triggering "{0:s}" but nobody is listening!'.format(name[len(self.prefix):]))
-          return noop
-        raise AttributeError
+    This is meant to be used as a decorator.
 
-    """
+    >>> @pyhf.events.subscribe('myevent')
+    ... def test(a,b):
+    ...   print a+b
+    ...
+    >>> pyhf.events.trigger_myevent(1,2)
+    3
+"""
+def subscribe(event):
+    global __events
+    def __decorator(func):
+        __events.setdefault(event, Events()).append(func)
+        return func
+    return __decorator
 
-        This is meant to be used as a decorator.
+"""
+Trigger an event if not disabled.
+"""
+def trigger(event):
+    global __events, __disabled_events
+    def noop(*args, **kwargs): pass
+    def _trigger(*args, **kwargs):
+        return __events.get(event, noop)(*args, **kwargs)
+    return noop if event in __disabled_events else _trigger
 
-        >>> @pyhf.events.subscribe('myevent')
-        ... def test(a,b):
-        ...   print a+b
-        ...
-        >>> pyhf.events.trigger_myevent(1,2)
-        3
-    """
-    def subscribe(self, event):
-        def __decorator(func):
-            self.__events.setdefault(event, self.Events()).append(func)
-            setattr(self, '{0:s}{1:s}'.format(self.prefix, event), self.__events.get(event))
-            return func
-        return __decorator
+"""
+Disable an event from firing.
+"""
+def disable(event):
+    global __disabled_events
+    __disabled_events.add(event)
 
-    def unsubscribe(self, event):
-        if event in self.__events:
-            events = self.__events.pop(event)
-            del events[:]
-            delattr(self, '{0:s}{1:s}'.format(self.prefix, event))
-        return
-
-sys.modules[__name__] = Wrapper(__name__, __file__)
+"""
+Enable an event to be fired if disabled.
+"""
+def enable(event):
+    global __disabled_events
+    __disabled_events.remove(event)
