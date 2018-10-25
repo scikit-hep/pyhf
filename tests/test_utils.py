7ed99f6f7fe630bf4cc473a8f163e2e9bb560d5a
Matthew Feickert <matthew.feickert@cern.ch>
2018-10-25 02:11:38 -0500

    Modularize pyhf.utils.hypotest kwarg tests
    Split the tests up and use a pytest fixture

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -25,37 +25,60 @@ def test_load_custom_schema(tmpdir):
     assert pyhf.utils.load_schema(temp.strpath)
 
 
-def test_hypotest(tmpdir):
-    """
-    Check that the return structure of pyhf.utils.hypotest is as expected
-    """
-    tb = pyhf.tensorlib
-
-    def check_uniform_type(in_list):
-        return all([isinstance(item, type(tb.astensor(item))) for item in in_list])
-
+@pytest.fixture(scope='module')
+def hypotest_args():
     pdf = pyhf.simplemodels.hepdata_like(
         signal_data=[12.0, 11.0], bkg_data=[50.0, 52.0], bkg_uncerts=[3.0, 7.0]
     )
     mu_test = 1.0
     data = [51, 48] + pdf.config.auxdata
-    kwargs = {}
+    return mu_test, data, pdf
 
-    result = pyhf.utils.hypotest(mu_test, data, pdf, **kwargs)
+
+def check_uniform_type(in_list):
+    return all(
+        [isinstance(item, type(pyhf.tensorlib.astensor(item))) for item in in_list]
+    )
+
+
+def test_hypotest_default(tmpdir, hypotest_args):
+    """
+    Check that the default return structure of pyhf.utils.hypotest is as expected
+    """
+    tb = pyhf.tensorlib
+
+    kwargs = {}
+    result = pyhf.utils.hypotest(*hypotest_args, **kwargs)
     # CLs_obs
     assert len(list(result)) == 1
     assert isinstance(result, type(tb.astensor(result)))
 
-    kwargs['return_p_values'] = True
-    result = pyhf.utils.hypotest(mu_test, data, pdf, **kwargs)
+
+def test_hypotest_return_p_values(tmpdir, hypotest_args):
+    """
+    Check that the return structure of pyhf.utils.hypotest with the
+    return_p_values keyword arg is as expected
+    """
+    tb = pyhf.tensorlib
+
+    kwargs = {'return_p_values': True}
+    result = pyhf.utils.hypotest(*hypotest_args, **kwargs)
     # CLs_obs, [CL_sb, CL_b]
     assert len(list(result)) == 2
     assert isinstance(result[0], type(tb.astensor(result[0])))
     assert len(result[1]) == 2
     assert check_uniform_type(result[1])
 
-    kwargs['return_expected'] = True
-    result = pyhf.utils.hypotest(mu_test, data, pdf, **kwargs)
+
+def test_hypotest_return_expected(tmpdir, hypotest_args):
+    """
+    Check that the return structure of pyhf.utils.hypotest with the
+    additon of the return_expected keyword arg is as expected
+    """
+    tb = pyhf.tensorlib
+
+    kwargs = {'return_p_values': True, 'return_expected': True}
+    result = pyhf.utils.hypotest(*hypotest_args, **kwargs)
     # CLs_obs, [CLsb, CLb], CLs_exp
     assert len(list(result)) == 3
     assert isinstance(result[0], type(tb.astensor(result[0])))
@@ -63,8 +86,20 @@ def test_hypotest(tmpdir):
     assert check_uniform_type(result[1])
     assert isinstance(result[2], type(tb.astensor(result[2])))
 
-    kwargs['return_expected_set'] = True
-    result = pyhf.utils.hypotest(mu_test, data, pdf, **kwargs)
+
+def test_hypotest_return_expected_set(tmpdir, hypotest_args):
+    """
+    Check that the return structure of pyhf.utils.hypotest with the
+    additon of the return_expected_set keyword arg is as expected
+    """
+    tb = pyhf.tensorlib
+
+    kwargs = {
+        'return_p_values': True,
+        'return_expected': True,
+        'return_expected_set': True,
+    }
+    result = pyhf.utils.hypotest(*hypotest_args, **kwargs)
     # CLs_obs, [CLsb, CLb], CLs_exp, CLs_exp @[-2, -1, 0, +1, +2]sigma
     assert len(list(result)) == 4
     assert isinstance(result[0], type(tb.astensor(result[0])))
@@ -74,8 +109,21 @@ def test_hypotest(tmpdir):
     assert len(result[3]) == 5
     assert check_uniform_type(result[3])
 
-    kwargs['return_test_statistics'] = True
-    result = pyhf.utils.hypotest(mu_test, data, pdf, **kwargs)
+
+def test_hypotest_return_test_statistics(tmpdir, hypotest_args):
+    """
+    Check that the return structure of pyhf.utils.hypotest with the
+    additon of the return_test_statistics keyword arg is as expected
+    """
+    tb = pyhf.tensorlib
+
+    kwargs = {
+        'return_p_values': True,
+        'return_expected': True,
+        'return_expected_set': True,
+        'return_test_statistics': True,
+    }
+    result = pyhf.utils.hypotest(*hypotest_args, **kwargs)
     # CLs_obs, [CLsb, CLb], CLs_exp, CLs_exp @[-2, -1, 0, +1, +2]sigma, [q_mu, q_mu_Asimov]
     assert len(list(result)) == 5
     assert isinstance(result[0], type(tb.astensor(result[0])))
