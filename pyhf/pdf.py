8721a040fff224e1ef9109c1d61d932f57b90d67
Giordon Stark <kratsg@gmail.com>
2018-11-04 12:57:27 -0800

    more typos

--- pyhf/pdf.py
+++ pyhf/pdf.py
@@ -58,9 +58,7 @@ class _ModelConfig(object):
                             modifier_def['type']
                         ].required_parset(
                             len(sample['data']),
-                            default=self.parameter_configs.get(
-                                modifier_def['name'], {}
-                            ),
+                            config=self.parameter_configs.get(modifier_def['name'], {}),
                         )
                     except KeyError:
                         log.exception(
