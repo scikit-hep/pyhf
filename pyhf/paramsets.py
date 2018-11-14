35ee8bea3276fd5c7b505a5a04e8534548bb2ee8
Giordon Stark <kratsg@gmail.com>
2018-11-14 14:40:08 +0100

    add sigmas to constrained_by_normal

--- pyhf/paramsets.py
+++ pyhf/paramsets.py
@@ -18,6 +18,7 @@ class constrained_by_normal(paramset):
         super(constrained_by_normal, self).__init__(**kwargs)
         self.pdf_type = 'normal'
         self.auxdata = kwargs.pop('auxdata')
+        self.sigmas = kwargs.pop('sigmas')
 
     def expected_data(self, pars):
         return pars
