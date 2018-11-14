d8024edf1c8a454eff1e0ff4ed6a6680e1b6b498
Giordon Stark <kratsg@gmail.com>
2018-11-14 14:19:12 +0100

    pass in parameters into spec from command line

--- pyhf/commandline.py
+++ pyhf/commandline.py
@@ -110,7 +110,7 @@ def cls(workspace, output_file, measurement, patch):
                 measurements[measurement_index]['name']
             )
         )
-        spec = {'channels': d['channels']}
+        spec = {'channels': d['channels'], 'parameters': d['toplvl']['measurements'][measurement_index].get('parameters',[])}
         for p in patch:
             with click.open_file(p, 'r') as read_file:
                 p = jsonpatch.JsonPatch(json.loads(read_file.read()))
