1563c58e32b6d59a4c20ae994575e2d4a5b8e5ba
Lukas Heinrich <lukas.heinrich@gmail.com>
2019-04-13 12:45:41 +0200

    Bump version: 0.0.17 → 0.1.0

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ def _is_test_pypi():
 
 setup(
     name='pyhf',
-    version='0.0.17',
+    version='0.1.0',
     description='(partial) pure python histfactory implementation',
     long_description=long_description,
     long_description_content_type='text/markdown',
