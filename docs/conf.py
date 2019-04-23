ba59ebbd37a5913e9d8fd1e620cf983b19c5bb0d
Giordon Stark <kratsg@gmail.com>
2019-04-23 07:30:05 -0700

    add schemas/ to documentation

--- docs/conf.py
+++ docs/conf.py
@@ -181,13 +181,13 @@ html_theme_path = []
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['_static','schemas']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
 #
-# html_extra_path = []
+html_extra_path = []
 
 # If not None, a 'Last updated on:' timestamp is inserted at every page
 # bottom, using the given strftime format.
