scikit-hep/pyhf

commit 1187a672c0283144f76cd869bfdef78f0b80006f

Add configurable optimizer (and options) through pyhf cls CLI (#403)
- Raise an exception (InvalidOptimizer) if an optimizer is requested that does not exist
- Allow for **kwargs for all optimizer initializations so can extract out the additional options for configuration
- Create click.ParamType (EqDelimStringParamType) to parse CLI options for configuring optimizer using equal-delimited key-value pairs, inspired by yadage

pyhf/commandline.py
@@ -4,9 +4,10 @@ import click
 import json
 import os
 
-from .utils import hypotest
+from .utils import hypotest, EqDelimStringParamType
 from .pdf import Workspace
 from .version import __version__
+from . import tensorlib, set_backend, optimize
 
 logging.basicConfig()
 log = logging.getLogger(__name__)
@@ -201,6 +202,7 @@ def inspect(workspace, output_file, measurement):
                 else '(none)',
             )
         )
+
     click.echo()
 
     if output_file:
@@ -220,7 +222,11 @@ def inspect(workspace, output_file, measurement):
 @click.option('-p', '--patch', multiple=True)
 @click.option('--testpoi', default=1.0)
 @click.option('--teststat', type=click.Choice(['q', 'qtilde']), default='qtilde')
-def cls(workspace, output_file, measurement, patch, testpoi, teststat):
+@click.option('--optimizer')
+@click.option('--optconf', type=EqDelimStringParamType(), multiple=True)
+def cls(
+    workspace, output_file, measurement, patch, testpoi, teststat, optimizer, optconf
+):
     with click.open_file(workspace, 'r') as specstream:
         wspec = json.load(specstream)
 
@@ -234,10 +240,19 @@ def cls(workspace, output_file, measurement, patch, testpoi, teststat):
         patches=patches,
         modifier_settings={'normsys': {'interpcode': 'code4'}},
     )
+
+    optconf = {k: v for item in optconf for k, v in item.items()}
+
+    # set the new optimizer
+    if optimizer:
+        new_optimizer = getattr(optimize, optimizer)
+        set_backend(tensorlib, new_optimizer(**optconf))
+
     result = hypotest(testpoi, w.data(p), p, qtilde=is_qtilde, return_expected_set=True)
     result = {'CLs_obs': result[0].tolist()[0], 'CLs_exp': result[-1].ravel().tolist()}
+
     if output_file is None:
-        click.echo(json.dumps(result, indent=4, sort_keys=True))
+        print(json.dumps(result, indent=4, sort_keys=True))
     else:
         with open(output_file, 'w+') as out_file:
             json.dump(result, out_file, indent=4, sort_keys=True)

pyhf/exceptions/__init__.py
@@ -70,3 +70,9 @@ class ImportBackendError(Exception):
     """
 
     pass
+
+
+class InvalidOptimizer(Exception):
+    """
+    InvalidOptimizer is raised when trying to set using an optimizer that does not exist.
+    """

pyhf/optimize/__init__.py
@@ -49,6 +49,12 @@ class _OptimizerRetriever(object):
                     "There was a problem importing Minuit. The minuit optimizer cannot be used.",
                     e,
                 )
+        elif name == '__wrapped__':  # doctest
+            pass
+        else:
+            raise exceptions.InvalidOptimizer(
+                "The requested optimizer \"{0:s}\" does not exist.".format(name)
+            )
 
 
 OptimizerRetriever = _OptimizerRetriever()

pyhf/optimize/opt_scipy.py
@@ -5,8 +5,8 @@ log = logging.getLogger(__name__)
 
 
 class scipy_optimizer(object):
-    def __init__(self):
-        pass
+    def __init__(self, **kwargs):
+        self.maxiter = kwargs.get('maxiter', 100000)
 
     def unconstrained_bestfit(self, objective, data, pdf, init_pars, par_bounds):
         # The Global Fit
@@ -16,7 +16,7 @@ class scipy_optimizer(object):
             method='SLSQP',
             args=(data, pdf),
             bounds=par_bounds,
-            options=dict(maxiter=100000),
+            options=dict(maxiter=self.maxiter),
         )
         try:
             assert result.success
@@ -37,7 +37,7 @@ class scipy_optimizer(object):
             method='SLSQP',
             args=(data, pdf),
             bounds=par_bounds,
-            options=dict(maxiter=100000),
+            options=dict(maxiter=self.maxiter),
         )
         try:
             assert result.success

pyhf/optimize/opt_tflow.py
@@ -6,11 +6,11 @@ log = logging.getLogger(__name__)
 
 
 class tflow_optimizer(object):
-    def __init__(self, tensorlib):
+    def __init__(self, tensorlib, **kwargs):
         self.tb = tensorlib
-        self.relax = 0.1
-        self.maxit = 1000
-        self.eps = 1e-4
+        self.relax = kwargs.get('relax', 0.1)
+        self.maxiter = kwargs.get('maxiter', 100000)
+        self.eps = kwargs.get('eps', 1e-4)
 
     def unconstrained_bestfit(self, objective, data, pdf, init_pars, par_bounds):
         # the graph
@@ -26,7 +26,7 @@ class tflow_optimizer(object):
 
         # run newton's method
         best_fit = init_pars
-        for i in range(self.maxit):
+        for i in range(self.maxiter):
             up = self.tb.session.run(update, feed_dict={pars: best_fit})
             best_fit = best_fit - self.relax * up
             if np.abs(np.max(up)) < self.eps:
@@ -65,7 +65,7 @@ class tflow_optimizer(object):
         best_fit_nuis = [
             x for i, x in enumerate(init_pars) if i != pdf.config.poi_index
         ]
-        for i in range(self.maxit):
+        for i in range(self.maxiter):
             up = self.tb.session.run(update, feed_dict={nuis_cat: best_fit_nuis})
             best_fit_nuis = best_fit_nuis - self.relax * up
             if np.abs(np.max(up)) < self.eps:

pyhf/utils.py
@@ -2,6 +2,8 @@ import json
 import jsonschema
 import pkg_resources
 import os
+import yaml
+import click
 
 from .exceptions import InvalidSpecification
 from . import get_backend
@@ -44,6 +46,23 @@ def validate(spec, schema_name):
         return validator.validate(spec)
     except jsonschema.ValidationError as err:
         raise InvalidSpecification(err)
+
+
+def options_from_eqdelimstring(opts):
+    document = '\n'.join('{0}: {1}'.format(*opt.split('=', 1)) for opt in opts)
+    return yaml.full_load(document)
+
+
+class EqDelimStringParamType(click.ParamType):
+    name = 'equal-delimited option'
+
+    def convert(self, value, param, ctx):
+        try:
+            return options_from_eqdelimstring([value])
+        except IndexError:
+            self.fail(
+                '{0:s} is not a valid equal-delimited string'.format(value), param, ctx
+            )
 
 
 def loglambdav(pars, data, pdf):

setup.py
@@ -108,6 +108,7 @@ setup(
         'six',  # for modifiers
         'jsonschema>=v3.0.0a2',  # for utils, alpha-release for draft 6
         'jsonpatch',
+        'pyyaml',  # for parsing CLI equal-delimited options
     ],
     extras_require=extras_require,
     entry_points={'console_scripts': ['pyhf=pyhf.commandline:pyhf']},

tests/test_optim.py
@@ -2,6 +2,11 @@ import pyhf
 import pytest
 
 
+def test_get_invalid_optimizer():
+    with pytest.raises(pyhf.exceptions.InvalidOptimizer):
+        assert pyhf.optimize.scipy
+
+
 @pytest.fixture(scope='module')
 def source():
     source = {

commit 42e6ec8bf14dff653734e9d56e4a4ab8d4671547

Remove print function in CLI (#461)
- Fix bug introduced in #403, no `print` functions in the CLI. Only use `click.echo`.

pyhf/commandline.py
@@ -252,7 +252,7 @@ def cls(
     result = {'CLs_obs': result[0].tolist()[0], 'CLs_exp': result[-1].ravel().tolist()}
 
     if output_file is None:
-        print(json.dumps(result, indent=4, sort_keys=True))
+        click.echo(json.dumps(result, indent=4, sort_keys=True))
     else:
         with open(output_file, 'w+') as out_file:
             json.dump(result, out_file, indent=4, sort_keys=True)