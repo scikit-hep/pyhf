d36fe97dd5e90d0401c147d5d44e2de911615fcc
Giordon Stark <kratsg@gmail.com>
2019-06-27 10:14:24 -0700

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ def _is_test_pypi():
 
 setup(
     name='pyhf',
-    version='0.1.0',
+    version='0.1.1',
     description='(partial) pure python histfactory implementation',
     long_description=long_description,
     long_description_content_type='text/markdown',
