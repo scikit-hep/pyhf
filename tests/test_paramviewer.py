2587a86af898653eb856d3f09077d6a2d9413ca3
Lukas Heinrich <lukas.heinrich@gmail.com>
2019-08-01 13:36:50 +0200

    updated tests

--- tests/test_paramviewer.py
+++ tests/test_paramviewer.py
@@ -1,27 +1,58 @@
 import pyhf
+import numpy as np
 from pyhf.paramview import ParamViewer
 
 
 def test_paramviewer(backend):
-    pars = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
+    pars = [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12]
+    ]
 
     nbatch = len(pars)
     npars = len(pars[0])
 
     v = ParamViewer((nbatch, npars), {'hello': {'slice': slice(0, 2)}}, ['hello'])
-    sl = v.get_slice(pars)
-    assert pyhf.tensorlib.tolist(sl[0]) == [[1, 2], [5, 6], [9, 10]]
+    sl = v.get(pars)
+    assert pyhf.tensorlib.tolist(sl[v.slices[0]]) == [[1,5,9],[2,6,10]]
 
     v = ParamViewer((nbatch, npars), {'hello': {'slice': slice(0, 2)}}, 'hello')
-    sl = v.get_slice(pars)
-    assert pyhf.tensorlib.tolist(sl) == [[1, 2], [5, 6], [9, 10]]
+    sl = v.get(pars)
+    assert pyhf.tensorlib.tolist(sl) == [[1,5,9],[2,6,10]]
 
-    pars = [1, 2, 3, 4]
-    npars = len(pars)
-    v = ParamViewer((npars,), {'hello': {'slice': slice(0, 2)}}, ['hello'])
-    sl = v.get_slice(pars)
-    assert pyhf.tensorlib.tolist(sl[0]) == [1, 2]
+    # pars = [1, 2, 3, 4]
+    # npars = len(pars)
+    # v = ParamViewer((npars,), {'hello': {'slice': slice(0, 2)}}, ['hello'])
+    # sl = v.get(pars)
+    # assert pyhf.tensorlib.tolist(sl[0]) == [1]
 
-    v = ParamViewer((npars,), {'hello': {'slice': slice(0, 2)}}, 'hello')
-    sl = v.get_slice(pars)
-    assert pyhf.tensorlib.tolist(sl) == [1, 2]
+    # v = ParamViewer((npars,), {'hello': {'slice': slice(0, 2)}}, 'hello')
+    # sl = v.get(pars)
+    # assert pyhf.tensorlib.tolist(sl) == [1, 2]
+
+
+    # pars = [
+    #     list(range(5))
+    # ]*10
+    # print('pars',pars)
+    # v = ParamViewer(
+    #     (10,5),
+    #     {'first': {'slice': slice(0, 2)},
+    #     'second': {'slice': slice(2,5)}}, ['first','second'],
+    #     regular = False
+    # )
+    # sl = v.get_slice(pars)
+    # assert np.all(np.isclose(pyhf.tensorlib.tolist(sl[0]),[[0,1]]*10))
+    # assert np.all(np.isclose(pyhf.tensorlib.tolist(sl[1]),[[2,3,4]]*10))
+
+
+    # pars = [
+    #     list(range(4))
+    # ]*10
+    # print('pars',pars)
+    # v = ParamViewer(
+    #     (10,5),
+    #     {'first': {'slice': slice(0, 2)},
+    #     'second': {'slice': slice(0, 4)}}, ['first','second'],
+    # )
