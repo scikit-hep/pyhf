0f215942291798e99be5f19f9ef65d619a605772
Matthew Feickert <matthew.feickert@cern.ch>
2019-09-04 01:32:48 -0500

    Deprecate TF v1.0 Sessions in tests

--- tests/test_backend_consistency.py
+++ tests/test_backend_consistency.py
@@ -106,15 +106,12 @@ def test_hypotest_q_mu(
 
     backends = [
         pyhf.tensor.numpy_backend(),
-        pyhf.tensor.tensorflow_backend(session=tf.compat.v1.Session()),
+        pyhf.tensor.tensorflow_backend(),
         pyhf.tensor.pytorch_backend(),
     ]
 
     test_statistic = []
     for backend in backends:
-        if backend.name == 'tensorflow':
-            tf.reset_default_graph()
-            backend.session = tf.compat.v1.Session()
         pyhf.set_backend(backend)
 
         q_mu = pyhf.infer.test_statistics.qmu(
