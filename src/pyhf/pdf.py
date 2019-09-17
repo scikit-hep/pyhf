593fe8c4d082f95a1da00905cbc09aee1502fde5
Matthew Feickert <matthew.feickert@cern.ch>
2019-09-16 23:34:20 -0500

    Add check for pars and data shape for pdf
    At the moment raise a ValueError for testing

--- src/pyhf/pdf.py
+++ src/pyhf/pdf.py
@@ -538,6 +538,30 @@ class Model(object):
         try:
             tensorlib, _ = get_backend()
             pars, data = tensorlib.astensor(pars), tensorlib.astensor(data)
+            # Verify parameter and data shapes
+            try:
+                if pars.shape[-1] != len(self.config.suggested_init()):
+                    raise ValueError
+            except ValueError:
+                log.error(
+                    'eval failed as pars has len {} but {} was expected'.format(
+                        pars.shape[-1], len(self.config.suggested_init())
+                    )
+                )
+                raise
+            try:
+                if data.shape[-1] != self.nominal_rates.shape[-1] + len(
+                    self.config.auxdata
+                ):
+                    raise ValueError
+            except ValueError:
+                log.error(
+                    'eval failed as data has len {} but {} was expected'.format(
+                        data.shape[-1],
+                        self.nominal_rates.shape[-1] + len(self.config.auxdata),
+                    )
+                )
+                raise
 
             actual_data = self.main_model._dataprojection(data)
             aux_data = self.constraint_model._dataprojection(data)
