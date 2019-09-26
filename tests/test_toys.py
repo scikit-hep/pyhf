5df871d33315dc3f3b481e9619ec7299bae1bdfc
Lukas Heinrich <lukas.heinrich@gmail.com>
2019-09-26 12:52:55 +0200

    lets go

--- tests/test_toys.py
+++ tests/test_toys.py
@@ -7,3 +7,8 @@ def test_smoketest_toys(backend):
     m = pyhf.simplemodels.hepdata_like([6], [9], [3])
     s = m.make_pdf(pyhf.tensorlib.astensor(m.config.suggested_init()))
     assert np.asarray(tb.tolist(s.log_prob(s.sample((1000,))))).shape == (1000,)
+
+    tb, _ = backend
+    m = pyhf.simplemodels.hepdata_like([6, 6], [9, 9], [3, 3], batch_size=13)
+    s = m.make_pdf(pyhf.tensorlib.astensor(m.batch_size * [m.config.suggested_init()]))
+    np.asarray(tb.tolist(s.sample((10,)))).shape == (10, 13, 4)
