a3473391509e9757e8e032b50e5e07ceb1cf8faf
Matthew Feickert <matthew.feickert@cern.ch>
2019-10-25 19:46:12 -0500

    Add region B and region C fixtures and refactor CLs calc

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -34,6 +34,30 @@ def regionA_bkgonly_json(sbottom_likelihoods_download):
 
 
 @pytest.fixture(scope='module')
+def regionB_bkgonly_json(sbottom_likelihoods_download):
+    """Extract the background only model from sbottom Region B"""
+    tarfile = sbottom_likelihoods_download
+    bkgonly_json = (
+        tarfile.extractfile(tarfile.getmember("RegionB/BkgOnly.json"))
+        .read()
+        .decode("utf8")
+    )
+    return json.loads(bkgonly_json)
+
+
+@pytest.fixture(scope='module')
+def regionC_bkgonly_json(sbottom_likelihoods_download):
+    """Extract the background only model from sbottom Region C"""
+    tarfile = sbottom_likelihoods_download
+    bkgonly_json = (
+        tarfile.extractfile(tarfile.getmember("RegionC/BkgOnly.json"))
+        .read()
+        .decode("utf8")
+    )
+    return json.loads(bkgonly_json)
+
+
+@pytest.fixture(scope='module')
 def regionA_signal_patch_json(sbottom_likelihoods_download):
     """Extract a signal model from sbottom Region A"""
     tarfile = sbottom_likelihoods_download
@@ -45,11 +69,47 @@ def regionA_signal_patch_json(sbottom_likelihoods_download):
     return json.loads(signal_patch_json)
 
 
-def test_sbottom_regionA(regionA_bkgonly_json, regionA_signal_patch_json):
-    workspace = pyhf.workspace.Workspace(regionA_bkgonly_json)
+@pytest.fixture(scope='module')
+def regionB_signal_patch_json(sbottom_likelihoods_download):
+    """Extract a signal model from sbottom Region B"""
+    tarfile = sbottom_likelihoods_download
+    signal_patch_json = (
+        tarfile.extractfile(tarfile.getmember("RegionB/patch.sbottom_1000_205_60.json"))
+        .read()
+        .decode("utf8")
+    )
+    return json.loads(signal_patch_json)
+
+
+@pytest.fixture(scope='module')
+def regionC_signal_patch_json(sbottom_likelihoods_download):
+    """Extract a signal model from sbottom Region C"""
+    tarfile = sbottom_likelihoods_download
+    signal_patch_json = (
+        tarfile.extractfile(tarfile.getmember("RegionC/patch.sbottom_1000_205_60.json"))
+        .read()
+        .decode("utf8")
+    )
+    return json.loads(signal_patch_json)
+
+
+def calculate_CLs(bkgonly_json, signal_patch_json):
+    """
+    Calculate the observed CLs and the expected CLs band from a background only
+    and signal patch.
+
+    Args:
+        bkgonly_json
+        signal_patch_json
+
+    Returns:
+        CLs_obs: The observed CLs value
+        CLs_exp: List of the expected CLs value band
+    """
+    workspace = pyhf.workspace.Workspace(bkgonly_json)
     model = workspace.model(
         measurement_name=None,
-        patches=[regionA_signal_patch_json],
+        patches=[signal_patch_json],
         modifier_settings={
             'normsys': {'interpcode': 'code4'},
             'histosys': {'interpcode': 'code4p'},
@@ -58,8 +118,11 @@ def test_sbottom_regionA(regionA_bkgonly_json, regionA_signal_patch_json):
     result = pyhf.utils.hypotest(
         1.0, workspace.data(model), model, qtilde=True, return_expected_set=True
     )
-    CLs_obs = result[0].tolist()[0]
-    CLs_exp = result[-1].ravel().tolist()
+    return result[0].tolist()[0], result[-1].ravel().tolist()
+
+
+def test_sbottom_regionA(regionA_bkgonly_json, regionA_signal_patch_json):
+    CLs_obs, CLs_exp = calculate_CLs(regionA_bkgonly_json, regionA_signal_patch_json)
     assert CLs_obs == pytest.approx(0.2444363575448201, rel=1e-5)
     assert np.all(
         np.isclose(
