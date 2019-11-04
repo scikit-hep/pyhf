819f131b3219ae597a238527d82e7e678ceeb253
Matthew Feckert <matthew.feickert@cern.ch>
2019-11-04 14:14:35 +1030

    Placate CodeFactor
    CodeFactor doesn't like tests that do a computation without assigning
    anything. This simply assigns the results that are going to fail to _
    
    fixes following error:
    
    Used when an expression that is not a function call is assigned to nothing. Probably something else was intended.

--- tests/test_tensor.py
+++ tests/test_tensor.py
@@ -148,13 +148,13 @@ def test_shape(backend):
     assert tb.shape(tb.astensor((1.0, 1.0))) == tb.shape(tb.astensor([1.0, 1.0]))
     assert tb.shape(tb.astensor((0.0, 0.0))) == tb.shape(tb.astensor([0.0, 0.0]))
     with pytest.raises((ValueError, RuntimeError)):
-        tb.astensor([1, 2]) + tb.astensor([3, 4, 5])
+        _ = tb.astensor([1, 2]) + tb.astensor([3, 4, 5])
     with pytest.raises((ValueError, RuntimeError)):
-        tb.astensor([1, 2]) - tb.astensor([3, 4, 5])
+        _ = tb.astensor([1, 2]) - tb.astensor([3, 4, 5])
     with pytest.raises((ValueError, RuntimeError)):
-        (tb.astensor([1, 2]) < tb.astensor([3, 4, 5]))
+        _ = tb.astensor([1, 2]) < tb.astensor([3, 4, 5])
     with pytest.raises((ValueError, RuntimeError)):
-        (tb.astensor([1, 2]) > tb.astensor([3, 4, 5]))
+        _ = tb.astensor([1, 2]) > tb.astensor([3, 4, 5])
     with pytest.raises((ValueError, RuntimeError)):
         tb.conditional(
             (tb.astensor([1, 2]) < tb.astensor([3, 4])),
