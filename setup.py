e7b201c5c291ea0cd5a9ef678507d53edc5ced3d
Matthew Feickert <matthew.feickert@cern.ch>
2019-11-14 12:20:05 -0600

    Bump version: 0.1.2 → 0.2.0

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ def _is_test_pypi():
 
 setup(
     name='pyhf',
-    version='0.1.2',
+    version='0.2.0',
     description='(partial) pure python histfactory implementation',
     long_description=long_description,
     long_description_content_type='text/markdown',
