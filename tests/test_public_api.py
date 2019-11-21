aee0d15ef5843ad053688e4dd2474cc8725b2af0
Matthew Feickert <matthew.feickert@cern.ch>
2019-11-21 01:20:35 -0600

    Add batched example test

--- tests/test_public_api.py
+++ tests/test_public_api.py
@@ -39,3 +39,31 @@ def test_prob_models(backend):
     pyhf.probability.Normal(tb.astensor([10.0]), tb.astensor([1])).log_prob(
         tb.astensor(2.0)
     )
+
+
+def test_pdf_batched(backend):
+    tb, _ = backend
+    source = {
+        "binning": [2, -0.5, 1.5],
+        "bindata": {"data": [55.0], "bkg": [50.0], "bkgerr": [7.0], "sig": [10.0]},
+    }
+    pdf = pyhf.simplemodels.hepdata_like(
+        source['bindata']['sig'],
+        source['bindata']['bkg'],
+        source['bindata']['bkgerr'],
+        batch_size=2,
+    )
+
+    pars = [pdf.config.suggested_init()] * 2
+    data = source['bindata']['data'] + pdf.config.auxdata
+
+    assert tb.tolist(pdf.pdf(pars, data)) == pytest.approx(
+        [0.002417118312751542] * 2, 2.5e-05
+    )
+    assert tb.tolist(pdf.expected_data(pars))
+    assert tb.tolist(pdf.expected_data(pars)[0]) == pytest.approx(
+        [60.0, 51.020408630], 1e-08
+    )
+    assert tb.tolist(pdf.expected_data(pars)[1]) == pytest.approx(
+        [60.0, 51.020408630], 1e-08
+    )
