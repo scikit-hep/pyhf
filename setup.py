scikit-hep/pyhf

commit f3b0680c115ef64cfb61a7631d9a550ec50c095f

Add CI through GitHub Actions (#527)
* Add CI tests with GitHub Actions for pytest test suite and docs
* Add daily cron job of tests and benchmarks with GitHub Actions

.github/workflows/ci.yml
@@ -1,0 +1,72 @@
+name: CI/CD
+
+on:
+  push:
+  pull_request:
+  # Run daily at 0:01 UTC
+  schedule:
+  - cron:  '1 0 * * *'
+
+jobs:
+  test:
+
+    strategy:
+      matrix:
+        platform: [ubuntu-latest]
+        python-version: [3.6, 3.7]
+    runs-on: ${{ matrix.platform }}
+
+    steps:
+    - uses: actions/checkout@master
+    - name: Set up Python ${{ matrix.python-version }}
+      uses: actions/setup-python@v1
+      with:
+        version: ${{ matrix.python-version }}
+    - name: Install dependencies
+      run: |
+        python -m pip install --upgrade pip setuptools wheel
+        pip install --ignore-installed -U -q --no-cache-dir -e .[complete]
+        pip list
+    - name: Lint with Pyflakes
+      run: |
+        pyflakes src
+        check-manifest
+    - name: Lint with Black
+      run: |
+        black --check --diff --verbose .
+    - name: Check MANIFEST
+      run: |
+        check-manifest
+    - name: Test with pytest
+      run: |
+        python -m pytest -r sx --ignore tests/benchmarks/
+    - name: Run benchmarks
+      if: github.event_name == 'schedule' && matrix.python-version == 3.7
+      run: |
+        python -m pytest -r sx tests/benchmarks/test_benchmark.py
+
+  docs:
+
+    runs-on: ubuntu-latest
+    strategy:
+      matrix:
+        python-version: [3.7]
+
+    steps:
+    - uses: actions/checkout@master
+    - name: Set up Python 3.7
+      uses: actions/setup-python@v1
+      with:
+        version: ${{ matrix.python-version }}
+    - name: Install dependencies
+      run: |
+        python -m pip install --upgrade pip setuptools wheel
+        pip install --ignore-installed -U -q --no-cache-dir -e .[develop]
+        pip list
+        sudo apt-get update
+        sudo apt-get -qq install pandoc
+    - name: Test and build docs
+      run: |
+        python -m doctest README.md
+        cd docs && make html && cd -
+        touch docs/_build/html/.nojekyll

commit 3443e891acbe25df501707400b41b078ec62fe60

refactor: Add better segmented environments (#648)
* Redistribute the "develop" environment into separate "test", "docs", and "develop" environments
   - "docs" contains all of the backend environments to successfully build all the API docs
   - "develop" still combines "test", "docs", and the "develop" dependencies together
* Use more specific environments for testing in CI to make environment dependencies more clear
* In doing so, fix the API section of the docs by building the docs in an environment with all the backends
   - Fixes bug introduced in PR #527

.github/workflows/ci.yml
@@ -25,8 +25,8 @@ jobs:
     - name: Install dependencies
       run: |
         python -m pip install --upgrade pip setuptools wheel
-        pip install --ignore-installed -U -q --no-cache-dir -e .[complete]
-        pip list
+        python -m pip install --ignore-installed -U -q --no-cache-dir -e .[test]
+        python -m pip list
     - name: Lint with Pyflakes
       if: matrix.python-version == 3.7 && matrix.os == 'ubuntu-latest'
       run: |
@@ -70,8 +70,8 @@ jobs:
     - name: Install dependencies
       run: |
         python -m pip install --upgrade pip setuptools wheel
-        pip install --ignore-installed -U -q --no-cache-dir -e .[complete]
-        pip list
+        python -m pip install --ignore-installed -U -q --no-cache-dir -e .[complete]
+        python -m pip list
     - name: Test example notebooks
       run: |
         python -m pytest -r sx tests/test_notebooks.py
@@ -92,8 +92,8 @@ jobs:
     - name: Install dependencies
       run: |
         python -m pip install --upgrade pip setuptools wheel
-        pip install --ignore-installed -U -q --no-cache-dir -e .[develop]
-        pip list
+        python -m pip install --ignore-installed -U -q --no-cache-dir -e .[docs]
+        python -m pip list
         sudo apt-get update
         sudo apt-get -qq install pandoc
     - name: Check docstrings

setup.py
@@ -13,38 +13,54 @@ extras_require = {
     'torch': ['torch~=1.2'],
     'xmlio': ['uproot'],
     'minuit': ['iminuit'],
-    'develop': [
-        'pyflakes',
-        'pytest~=3.5',
-        'pytest-cov>=2.5.1',
-        'pytest-mock',
-        'pytest-benchmark[histogram]',
-        'pytest-console-scripts',
-        'pydocstyle',
-        'coverage>=4.0',  # coveralls
-        'matplotlib',
-        'jupyter',
-        'nbdime',
-        'uproot~=3.3',
-        'papermill~=1.0',
-        'nteract-scrapbook~=0.2',
-        'graphviz',
-        'bumpversion',
-        'sphinx',
-        'sphinxcontrib-bibtex',
-        'sphinxcontrib-napoleon',
-        'sphinx_rtd_theme',
-        'nbsphinx',
-        'sphinx-issues',
-        'm2r',
-        'jsonpatch',
-        'ipython',
-        'pre-commit',
-        'black;python_version>="3.6"',  # Black is Python3 only
-        'twine',
-        'check-manifest',
-    ],
 }
+extras_require['test'] = sorted(
+    set(
+        extras_require['tensorflow']
+        + extras_require['torch']
+        + extras_require['xmlio']
+        + extras_require['minuit']
+        + [
+            'pyflakes',
+            'pytest~=3.5',
+            'pytest-cov>=2.5.1',
+            'pytest-mock',
+            'pytest-benchmark[histogram]',
+            'pytest-console-scripts',
+            'pydocstyle',
+            'coverage>=4.0',  # coveralls
+            'papermill~=1.0',
+            'nteract-scrapbook~=0.2',
+            'check-manifest',
+            'matplotlib',
+            'jupyter',
+            'uproot~=3.3',
+            'graphviz',
+            'jsonpatch',
+            'black;python_version>="3.6"',  # Black is Python3 only
+        ]
+    )
+)
+extras_require['docs'] = sorted(
+    set(
+        extras_require['test']
+        + [
+            'sphinx',
+            'sphinxcontrib-bibtex',
+            'sphinxcontrib-napoleon',
+            'sphinx_rtd_theme',
+            'nbsphinx',
+            'sphinx-issues',
+            'm2r',
+        ]
+    )
+)
+extras_require['develop'] = sorted(
+    set(
+        extras_require['docs']
+        + ['nbdime', 'bumpversion', 'ipython', 'pre-commit', 'twine']
+    )
+)
 extras_require['complete'] = sorted(set(sum(extras_require.values(), [])))
 
 