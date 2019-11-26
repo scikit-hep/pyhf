f14dfee4e51cd4d1b6f0ea060de4f1714280d8af
Giordon Stark <kratsg@users.noreply.github.com>
2019-11-26 14:07:07 -0800

    Update src/pyhf/cli/rootio.py
    Co-Authored-By: Matthew Feickert <matthew.feickert@cern.ch>

--- src/pyhf/cli/rootio.py
+++ src/pyhf/cli/rootio.py
@@ -74,7 +74,7 @@ def json2xml(workspace, output_dir, specroot, dataroot, resultprefix, patch):
     except ImportError:
         log.error(
             "json2xml requires uproot, please install pyhf using the "
-            "xmlio extra: pip install pyhf[xmlio] or install uproot "
+            "xmlio extra: pip install pyhf[xmlio]."
             "manually: pip install uproot"
         )
     from .. import writexml
