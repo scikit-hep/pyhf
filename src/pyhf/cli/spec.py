8c77f828735da0c1896102da18f7d3e06fa5cf58
Giordon Stark <kratsg@gmail.com>
2019-11-26 22:26:33 -0800

    fix up help options, add rename

--- src/pyhf/cli/spec.py
+++ src/pyhf/cli/spec.py
@@ -131,11 +131,19 @@ def inspect(workspace, output_file, measurement):
     help='The location of the output json file. If not specified, prints to screen.',
     default=None,
 )
-@click.option('-c', '--channel', default=[], multiple=True)
-@click.option('-s', '--sample', default=[], multiple=True)
-@click.option('-m', '--modifier', default=[], multiple=True)
-@click.option('-t', '--modifier-type', default=[], multiple=True)
-@click.option('-e', '--measurement', default=[], multiple=True)
+@click.option('-c', '--channel', default=[], multiple=True, metavar='<CHANNEL>...')
+@click.option('-s', '--sample', default=[], multiple=True, metavar='<SAMPLE>...')
+@click.option('-m', '--modifier', default=[], multiple=True, metavar='<MODIFIER>...')
+@click.option(
+    '-t',
+    '--modifier-type',
+    default=[],
+    multiple=True,
+    type=click.Choice(modifiers.uncombined.keys()),
+)
+@click.option(
+    '-e', '--measurement', default=[], multiple=True, metavar='<MEASUREMENT>...'
+)
 def prune(
     workspace, output_file, channel, sample, modifier, modifier_type, measurement
 ):
@@ -149,6 +157,65 @@ def prune(
         modifiers=modifier,
         modifier_types=modifier_type,
         measurements=measurement,
+    )
+
+    if output_file is None:
+        click.echo(json.dumps(pruned_ws, indent=4, sort_keys=True))
+    else:
+        with open(output_file, 'w+') as out_file:
+            json.dump(pruned_ws, out_file, indent=4, sort_keys=True)
+        log.debug("Written to {0:s}".format(output_file))
+
+
+@cli.command()
+@click.argument('workspace', default='-')
+@click.option(
+    '--output-file',
+    help='The location of the output json file. If not specified, prints to screen.',
+    default=None,
+)
+@click.option(
+    '-c',
+    '--channel',
+    default=[],
+    multiple=True,
+    type=click.Tuple([str, str]),
+    metavar='<PATTERN> <REPLACE>...',
+)
+@click.option(
+    '-s',
+    '--sample',
+    default=[],
+    multiple=True,
+    type=click.Tuple([str, str]),
+    metavar='<PATTERN> <REPLACE>...',
+)
+@click.option(
+    '-m',
+    '--modifier',
+    default=[],
+    multiple=True,
+    type=click.Tuple([str, str]),
+    metavar='<PATTERN> <REPLACE>...',
+)
+@click.option(
+    '-e',
+    '--measurement',
+    default=[],
+    multiple=True,
+    type=click.Tuple([str, str]),
+    metavar='<PATTERN> <REPLACE>...',
+)
+def rename(workspace, output_file, channel, sample, modifier, measurement):
+    with click.open_file(workspace, 'r') as specstream:
+        spec = json.load(specstream)
+
+    ws = Workspace(spec)
+    renamed_ws = ws.prune(
+        channels=dict(channel),
+        samples=dict(sample),
+        modifiers=dict(modifier),
+        measurements=dict(measurement),
     )
 
     if output_file is None:
