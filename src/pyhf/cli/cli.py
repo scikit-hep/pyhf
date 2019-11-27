712e224f96027e305a7eeb4250ce8172a3d14df0
Giordon Stark <kratsg@gmail.com>
2019-11-26 21:56:44 -0800

    add prune to cli

--- src/pyhf/cli/cli.py
+++ src/pyhf/cli/cli.py
@@ -21,6 +21,7 @@ pyhf.add_command(rootio.xml2json)
 
 # pyhf.add_command(spec.cli)
 pyhf.add_command(spec.inspect)
+pyhf.add_command(spec.prune)
 
 # pyhf.add_command(stats.cli)
 pyhf.add_command(stats.cls)
