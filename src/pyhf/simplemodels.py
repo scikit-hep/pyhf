8e501f42bd6bc262d4d48de3d4a3b199ec8c38bd
Lukas Heinrich <lukas.heinrich@gmail.com>
2019-11-30 17:33:47 +0100

    adapt other optimizers as well

--- src/pyhf/simplemodels.py
+++ src/pyhf/simplemodels.py
@@ -30,36 +30,3 @@ def hepdata_like(signal_data, bkg_data, bkg_uncerts, batch_size=None):
         ]
     }
     return Model(spec, batch_size=batch_size)
-
-
-def overall_bkguncert(
-    signal_data, bkg_data, bkg_uncert_hi, bkg_uncert_lo, batch_size=None
-):
-    spec = {
-        'channels': [
-            {
-                'name': 'singlechannel',
-                'samples': [
-                    {
-                        'name': 'signal',
-                        'data': signal_data,
-                        'modifiers': [
-                            {'name': 'mu', 'type': 'normfactor', 'data': None}
-                        ],
-                    },
-                    {
-                        'name': 'background',
-                        'data': bkg_data,
-                        'modifiers': [
-                            {
-                                'name': 'corr_bkguncrt',
-                                'type': 'normsys',
-                                'data': {'hi': bkg_uncert_hi, 'lo': bkg_uncert_lo},
-                            }
-                        ],
-                    },
-                ],
-            }
-        ]
-    }
-    return Model(spec, batch_size=batch_size)
