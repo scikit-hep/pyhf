fab2805774398a6b41069b3f7f792558ad78e90d
Matthew Feickert <matthew.feickert@cern.ch>
2019-12-05 01:06:25 -0600

    Set lowerbounds on jax to correspond to Poissons PR

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as readme_md
 extras_require = {
     'tensorflow': ['tensorflow~=1.15', 'tensorflow-probability~=0.8', 'numpy~=1.16'],
     'torch': ['torch~=1.2'],
-    'jax': ['jax~=0.1,>0.1.16', 'jaxlib~=0.1,>0.1.4'],
+    'jax': ['jax~=0.1,>0.1.51', 'jaxlib~=0.1,>0.1.33'],
     'xmlio': ['uproot'],
     'minuit': ['iminuit'],
 }
