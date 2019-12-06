fe2caa062d387278cebca4be651b51937e29facd
Matthew Feickert <matthew.feickert@cern.ch>
2019-12-05 21:09:05 -0600

    Add coverage of 'numpy_minuit'

--- tests/test_top_level.py
+++ tests/test_top_level.py
@@ -8,6 +8,13 @@ def test_set_backend(backend):
     pyhf.set_backend(tb.name)
 
 
+@pytest.mark.parametrize(
+    "backend_name", ["numpy", "tensorflow", "pytorch", "numpy_minuit"]
+)
+def test_set_backend_by_string(backend_name):
+    pyhf.set_backend(backend_name)
+
+
 @pytest.mark.xfail(raises=ValueError)
 def test_supported_backends():
     pyhf.set_backend("fail")
