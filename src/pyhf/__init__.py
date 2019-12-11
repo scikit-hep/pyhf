8c70db4e07b7e7ce5e92670975e6e6d611647770
Matthew Feickert <matthew.feickert@cern.ch>
2019-12-10 20:41:06 -0600

    Try to signal this is a check variable

--- src/pyhf/__init__.py
+++ src/pyhf/__init__.py
@@ -68,9 +68,9 @@ def set_backend(backend, custom_optimizer=None, _session=None):
                     )
                 )
 
-    is_name_supported = getattr(tensor, "{0:s}_backend".format(backend.name))
-    if is_name_supported:
-        if not isinstance(backend, is_name_supported):
+    _name_supported = getattr(tensor, "{0:s}_backend".format(backend.name))
+    if _name_supported:
+        if not isinstance(backend, _name_supported):
             raise AttributeError(
                 "'{0:s}' is not a valid name attribute for backend type {1}\n                 Custom backends must have names unique from supported backends".format(
                     backend.name, type(backend)
