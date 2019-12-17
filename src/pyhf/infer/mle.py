3303a6bb199a67dc0710a494b90b664270ecb8ac
Lukas Heinrich <lukas.heinrich@gmail.com>
2019-12-17 03:48:17 +0100

    fix

--- src/pyhf/infer/mle.py
+++ src/pyhf/infer/mle.py
@@ -15,7 +15,9 @@ def fit(data, pdf, init_pars=None, par_bounds=None, **kwargs):
     return opt.minimize(loglambdav, data, pdf, init_pars, par_bounds, **kwargs)
 
 
-def fixed_poi_fit(poi_val, data, pdf, init_pars=None, par_bounds=None, poi_index=None, **kwargs):
+def fixed_poi_fit(
+    poi_val, data, pdf, init_pars=None, par_bounds=None, poi_index=None, **kwargs
+):
     _, opt = get_backend()
     init_pars = init_pars or pdf.config.suggested_init()
     par_bounds = par_bounds or pdf.config.suggested_bounds()
