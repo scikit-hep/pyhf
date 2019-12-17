788279e7e83ddaff9e5e80edb05bcfb2abedf5aa
Lukas Heinrich <lukas.heinrich@gmail.com>
2019-12-17 17:31:38 +0100

    add docstrings

--- src/pyhf/infer/mle.py
+++ src/pyhf/infer/mle.py
@@ -1,28 +1,61 @@
-"""
-Module for Maximum Likelihood Estimation
-"""
+"""Module for Maximum Likelihood Estimation."""
 from .. import get_backend
 
 
-def loglambdav(pars, data, pdf):
+def twice_nll(pars, data, pdf):
+    """
+    Twice the negative Log-Likelihood.
+    
+    Args:
+        data (`tensor`): the data
+        pdf (`pyhf.Model`): the statistical model
+
+    Returns:
+        Twice the negative log likelihood.
+    
+    """
     return -2 * pdf.logpdf(pars, data)
 
 
 def fit(data, pdf, init_pars=None, par_bounds=None, **kwargs):
+    """
+    Run a unconstrained maximum likelihood fit.
+    
+    Args:
+        data (`tensor`): the data
+        pdf (`pyhf.Model`): the statistical model
+        kwargs: keyword arguments passed through to the optimizer API
+    
+    Returns:
+        see optimizer API
+    
+    """
     _, opt = get_backend()
     init_pars = init_pars or pdf.config.suggested_init()
     par_bounds = par_bounds or pdf.config.suggested_bounds()
-    return opt.minimize(loglambdav, data, pdf, init_pars, par_bounds, **kwargs)
+    return opt.minimize(twice_nll, data, pdf, init_pars, par_bounds, **kwargs)
 
 
 def fixed_poi_fit(
     poi_val, data, pdf, init_pars=None, par_bounds=None, poi_index=None, **kwargs
 ):
+    """
+    Run a maximum likelihood fit with the POI value fixzed.
+    
+    Args:
+        data: the data
+        pdf (`pyhf.Model`): the statistical model
+        kwargs: keyword arguments passed through to the optimizer API
+
+    Returns:
+        see optimizer API
+    
+    """
     _, opt = get_backend()
     init_pars = init_pars or pdf.config.suggested_init()
     par_bounds = par_bounds or pdf.config.suggested_bounds()
     return opt.minimize(
-        loglambdav,
+        twice_nll,
         data,
         pdf,
         init_pars,
