27e41d6eb525a7203397c09773157a02ebf11e16
Giordon Stark <kratsg@users.noreply.github.com>
2019-12-19 11:37:09 -0600

    Update src/pyhf/contrib/viz/brazil.py
    Co-Authored-By: Matthew Feickert <matthew.feickert@cern.ch>

--- src/pyhf/contrib/viz/brazil.py
+++ src/pyhf/contrib/viz/brazil.py
@@ -4,7 +4,7 @@ import numpy as np
 def plot_results(ax, mutests, tests, test_size=0.05):
     cls_obs = np.array([test[0] for test in tests]).flatten()
     cls_exp = [np.array([test[1][i] for test in tests]).flatten() for i in range(5)]
-    ax.plot(mutests, cls_obs, c='k')
+    ax.plot(mutests, cls_obs, c='black')
     for i, c in zip(range(5), ['k', 'k', 'k', 'k', 'k']):
         ax.plot(mutests, cls_exp[i], c=c, linestyle='dotted' if i != 2 else 'dashed')
     ax.fill_between(mutests, cls_exp[0], cls_exp[-1], facecolor='y')
