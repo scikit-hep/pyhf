25c82e8ef124d737d0e18f2407d6030b671bac40
Giordon Stark <kratsg@users.noreply.github.com>
2019-12-19 11:37:31 -0600

    Update src/pyhf/contrib/viz/brazil.py
    Co-Authored-By: Matthew Feickert <matthew.feickert@cern.ch>

--- src/pyhf/contrib/viz/brazil.py
+++ src/pyhf/contrib/viz/brazil.py
@@ -8,6 +8,6 @@ def plot_results(ax, mutests, tests, test_size=0.05):
     for i, c in zip(range(5), ['k', 'k', 'k', 'k', 'k']):
         ax.plot(mutests, cls_exp[i], c=c, linestyle='dotted' if i != 2 else 'dashed')
     ax.fill_between(mutests, cls_exp[0], cls_exp[-1], facecolor='yellow')
-    ax.fill_between(mutests, cls_exp[1], cls_exp[-2], facecolor='g')
+    ax.fill_between(mutests, cls_exp[1], cls_exp[-2], facecolor='green')
     ax.plot(mutests, [test_size] * len(mutests), c='r')
     ax.set_ylim(0, 1)
