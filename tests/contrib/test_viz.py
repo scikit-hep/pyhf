17fd0528b8d510ec96d70ac53c9c5b7a34411ace
Lukas Heinrich <lukas.heinrich@gmail.com>
2019-12-20 12:16:06 +0100

    add MPL tests

---
+++ tests/contrib/test_viz.py
@@ -0,0 +1,14 @@
+import pytest
+import json
+import pyhf.contrib.viz.brazil as brazil
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.testing.decorators import image_comparison
+
+@pytest.mark.mpl_image_compare
+def test_brazil():
+    data = json.load(open('tests/contrib/hypotestresults.json'))
+    f,ax = plt.subplots(1,1)
+    testmus = np.linspace(0,5)
+    brazil.plot_results(ax,data['testmus'],data['results'],test_size = 0.05)
+    return f
