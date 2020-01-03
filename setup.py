97f5ea956b87e70ad5cd59ce8d38c3ed6fa7916d
GitHub Action <action@github.com>
2020-01-03 01:14:43 +0000

    Bump version: 0.3.1 → 0.3.2
    Triggered by #712 via GitHub Actions.

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ extras_require['complete'] = sorted(set(sum(extras_require.values(), [])))
 
 setup(
     name='pyhf',
-    version='0.3.1',
+    version='0.3.2',
     description='(partial) pure python histfactory implementation',
     long_description=long_description,
     long_description_content_type='text/markdown',
