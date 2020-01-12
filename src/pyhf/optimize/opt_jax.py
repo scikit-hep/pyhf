30ffff41b699355c9ac8e554f3915bdb97dcd2b0
Lukas Heinrich <lukas.heinrich@gmail.com>
2020-01-12 14:04:26 +0100

    some tweaks

--- src/pyhf/optimize/opt_jax.py
+++ src/pyhf/optimize/opt_jax.py
@@ -24,10 +24,11 @@ class jax_optimizer(AutoDiffOptimizerMixin):
             fixed_vals: fixed parameter values
 
         """
-        def scalar_objective(pars,data):
-            return objective(pars,data,pdf)[0]
-        scalar_objective = jax.jit(scalar_objective)
 
+        def scalar_objective(pars, data):
+            return objective(pars, data, pdf)[0]
+
+        scalar_objective = jax.jit(scalar_objective)
 
         tensorlib, _ = get_backend()
         all_idx = default_backend.astensor(range(pdf.config.npars), dtype='int')
@@ -46,13 +47,15 @@ class jax_optimizer(AutoDiffOptimizerMixin):
         data = tensorlib.astensor(data)
         fixed_values_tensor = tensorlib.astensor(fixed_values, dtype='float')
 
-
         def final_objective(pars):
             pars = tensorlib.astensor(pars)
             constrained_pars = tv.stitch([fixed_values_tensor, pars])
-            return scalar_objective(constrained_pars,data)
+            return scalar_objective(constrained_pars, data)
+
         jitted_objective_and_grad = jax.jit(jax.value_and_grad(final_objective))
+
         def func(pars):
-            a,b = jitted_objective_and_grad(pars)
-            return a.reshape((1,)),b
+            a, b = jitted_objective_and_grad(pars)
+            return a.reshape((1,)), b
+
         return tv, fixed_values_tensor, func, variable_init, variable_bounds
