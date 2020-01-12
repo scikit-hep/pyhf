d56b884c6f3e9e08010a9bab9ff40c978f518637
Lukas Heinrich <lukas.heinrich@gmail.com>
2020-01-12 14:16:14 +0100

    some tweaks

--- src/pyhf/optimize/opt_jax.py
+++ src/pyhf/optimize/opt_jax.py
@@ -6,6 +6,18 @@ from .autodiff import AutoDiffOptimizerMixin
 import jax
 
 
+def final_objective(pars, objective, tv, fixed_vals, data, model):
+    tensorlib, _ = get_backend()
+    pars = tensorlib.astensor(pars)
+    constrained_pars = tv.stitch([fixed_vals, pars])
+    return objective(constrained_pars, data, model)[0]
+
+
+jitted_objective_and_grad = jax.jit(
+    jax.value_and_grad(final_objective), static_argnums=(1, 2, 3, 4, 5)
+)
+
+
 class jax_optimizer(AutoDiffOptimizerMixin):
     """JAX Optimizer Backend."""
 
@@ -25,11 +37,6 @@ class jax_optimizer(AutoDiffOptimizerMixin):
 
         """
 
-        def scalar_objective(pars, data):
-            return objective(pars, data, pdf)[0]
-
-        scalar_objective = jax.jit(scalar_objective)
-
         tensorlib, _ = get_backend()
         all_idx = default_backend.astensor(range(pdf.config.npars), dtype='int')
         all_init = default_backend.astensor(init_pars)
@@ -47,15 +54,10 @@ class jax_optimizer(AutoDiffOptimizerMixin):
         data = tensorlib.astensor(data)
         fixed_values_tensor = tensorlib.astensor(fixed_values, dtype='float')
 
-        def final_objective(pars):
-            pars = tensorlib.astensor(pars)
-            constrained_pars = tv.stitch([fixed_values_tensor, pars])
-            return scalar_objective(constrained_pars, data)
-
-        jitted_objective_and_grad = jax.jit(jax.value_and_grad(final_objective))
-
         def func(pars):
-            a, b = jitted_objective_and_grad(pars)
+            a, b = jitted_objective_and_grad(
+                pars, objective, tv, fixed_values_tensor, data, pdf
+            )
             return a.reshape((1,)), b
 
         return tv, fixed_values_tensor, func, variable_init, variable_bounds
