4732724575c90119687584016ead4725023fe3a0
Lukas Heinrich <lukas.heinrich@gmail.com>
2020-01-12 15:01:41 +0100

    better jitting

--- src/pyhf/optimize/opt_jax.py
+++ src/pyhf/optimize/opt_jax.py
@@ -6,15 +6,16 @@ from .autodiff import AutoDiffOptimizerMixin
 import jax
 
 
-def final_objective(pars, objective, tv, fixed_vals, data, model):
+def final_objective(pars, data, fixed_vals, model,objective, fixed_idx,variable_idx):
     tensorlib, _ = get_backend()
+    tv = _TensorViewer([fixed_idx, variable_idx])
     pars = tensorlib.astensor(pars)
     constrained_pars = tv.stitch([fixed_vals, pars])
     return objective(constrained_pars, data, model)[0]
 
 
 jitted_objective_and_grad = jax.jit(
-    jax.value_and_grad(final_objective), static_argnums=(1, 2, 3, 4, 5)
+    jax.value_and_grad(final_objective), static_argnums=(3,4,5,6)
 )
 
 
@@ -55,8 +56,9 @@ class jax_optimizer(AutoDiffOptimizerMixin):
         fixed_values_tensor = tensorlib.astensor(fixed_values, dtype='float')
 
         def func(pars):
+            #need to conver to tuple to make args hashable
             a, b = jitted_objective_and_grad(
-                pars, objective, tv, fixed_values_tensor, data, pdf
+                pars, data, fixed_values_tensor, pdf, objective, tuple(fixed_idx),tuple(variable_idx)
             )
             return a.reshape((1,)), b
 
