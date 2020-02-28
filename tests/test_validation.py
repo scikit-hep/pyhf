7691c0e6540db7eaa3a667d7f6619bb9890482b8
Lukas Heinrich <lukas.heinrich@gmail.com>
2020-02-28 17:06:13 +0100

    add integration tests

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -4,6 +4,7 @@ import pyhf.readxml
 import json
 import pytest
 import os
+import numpy as np
 
 
 @pytest.fixture(scope='module')
@@ -917,7 +918,6 @@ def test_shapesys_nuisparfilter_validation():
     obs, exp = pyhf.infer.hypotest(1.0, d, m, return_expected_set=True)
     pyhf_results = {'CLs_obs': obs[0], 'CLs_exp': [e[0] for e in exp]}
 
-    import numpy as np
 
     assert np.allclose(
         reference_root_results['CLs_obs'], pyhf_results['CLs_obs'], atol=1e-4, rtol=1e-5
