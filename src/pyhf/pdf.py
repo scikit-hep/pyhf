75af799ef96fa9e25fcc021170b45722f5ffde8e
Kanishk <36276423+kanishk16@users.noreply.github.com>
2020-03-21 15:38:14 +0530

    refactor + correct comment

--- src/pyhf/pdf.py
+++ src/pyhf/pdf.py
@@ -224,8 +224,8 @@ class _ModelConfig(_ChannelSummaryMixin):
 
         poiname = config_kwargs.pop('poiname', 'mu')
 
-        # remove measurement_name in case model is created using poiname
-        if poiname and 'measurement_name' in config_kwargs:
+        # remove measurement_name in case model is created using poi_name
+        if 'measurement_name' in config_kwargs:
             del config_kwargs['measurement_name']
 
         default_modifier_settings = {'normsys': {'interpcode': 'code1'}}
