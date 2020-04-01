9d1a6ea08867cc36470fbd178ba7a0a26db605ea
Matthew Feickert <matthew.feickert@cern.ch>
2020-03-31 23:07:48 -0500

    Mark toys notebook as 'slow' test to skip

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -62,6 +62,7 @@ def test_impactplot(common_kwargs):
     pm.execute_notebook('docs/examples/notebooks/ImpactPlot.ipynb', **common_kwargs)
 
 
+@pytest.mark.slow
 def test_toys(common_kwargs):
     pm.execute_notebook('docs/examples/notebooks/ImpactPlot.ipynb', **common_kwargs)
 
