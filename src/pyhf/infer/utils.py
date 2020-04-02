552019b2b08033c941663894013a1cd4887e429b
Matthew Feickert <matthew.feickert@cern.ch>
2020-04-02 01:40:38 -0500

    Add examples to infer.utils

--- src/pyhf/infer/utils.py
+++ src/pyhf/infer/utils.py
@@ -10,6 +10,24 @@ def create_calculator(calctype, *args, **kwargs):
 
     See :py:class:`~pyhf.infer.calculators.AsymptoticCalculator` and :py:class:`~pyhf.infer.calculators.ToyCalculator` on additional arguments to be specified.
 
+    Example:
+
+        >>> import pyhf
+        >>> import numpy.random as random
+        >>> random.seed(0)
+        >>> model = pyhf.simplemodels.hepdata_like(
+        ...     signal_data=[12.0, 11.0], bkg_data=[50.0, 52.0], bkg_uncerts=[3.0, 7.0],
+        ... )
+        >>> observations = [51, 48]
+        >>> data = observations + model.config.auxdata
+        >>> mu_test = 1.0
+        >>> toy_calculator = pyhf.infer.utils.create_calculator(
+        ...     "toybased", data, model, ntoys=100, track_progress=False
+        ... )
+        >>> qmu_sig, qmu_bkg = toy_calculator.distributions(mu_test)
+        >>> qmu_sig.pvalue(mu_test), qmu_bkg.pvalue(mu_test)
+        (0.14, 0.76)
+
     Args:
         calctype (`str`): The calculator to create. Choose either 'asymptotics' or 'toybased'.
 
@@ -45,9 +63,9 @@ def hypotest(
         ... )
         >>> observations = [51, 48]
         >>> data = pyhf.tensorlib.astensor(observations + model.config.auxdata)
-        >>> test_poi = 1.0
+        >>> mu_test = 1.0
         >>> CLs_obs, CLs_exp_band = pyhf.infer.hypotest(
-        ...     test_poi, data, model, qtilde=True, return_expected_set=True
+        ...     mu_test, data, model, return_expected_set=True
         ... )
         >>> CLs_obs
         array(0.05251497)
