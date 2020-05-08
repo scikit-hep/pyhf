e8ad7382bd408502807ad87a3fdf56396fbd1df9
Matthew Feickert <matthew.feickert@cern.ch>
2020-05-08 05:20:04 -0500

    ci: Add Python 3.8 to tests (#860)
    * Add Python 3.8 to runtimes tested in CI tests
    * Add Python 3.8 to PyPI classifiers

--- setup.py
+++ setup.py
@@ -87,6 +87,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
