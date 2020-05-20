7c5ac5a7c42d15ba87cb6ff3f9ae1082fef3ae94
Matthew Feickert <matthew.feickert@cern.ch>
2020-05-20 00:31:51 -0500

    Use flake8 in setup.py

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ extras_require['test'] = sorted(
         + extras_require['contrib']
         + extras_require['shellcomplete']
         + [
+            'flake8',
             'pytest~=6.0',
             'pytest-cov>=2.5.1',
             'pytest-mock',
