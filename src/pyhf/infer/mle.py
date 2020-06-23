ab533ca5a9f1fd7252ab8c88357872e7f9dee9f5
Matthew Feickert <matthew.feickert@cern.ch>
2020-06-23 06:32:21 -0500

    fix: Update infer.mle docstrings to match SciPy v1.5.0 (#904)
    * Show a NumPy array return type for the returned objective function (2*NLL) in the docs examples

--- src/pyhf/infer/mle.py
+++ src/pyhf/infer/mle.py
@@ -30,7 +30,7 @@ def fit(data, pdf, init_pars=None, par_bounds=None, **kwargs):
         >>> observations = [51, 48]
         >>> data = pyhf.tensorlib.astensor(observations + model.config.auxdata)
         >>> pyhf.infer.mle.fit(data, model, return_fitted_val=True)
-        (array([0.        , 1.0030512 , 0.96266961]), 24.98393521454011)
+        (array([0.        , 1.0030512 , 0.96266961]), array([24.98393521]))
 
     Args:
         data (`tensor`): The data
@@ -63,7 +63,7 @@ def fixed_poi_fit(poi_val, data, pdf, init_pars=None, par_bounds=None, **kwargs)
         >>> data = pyhf.tensorlib.astensor(observations + model.config.auxdata)
         >>> test_poi = 1.0
         >>> pyhf.infer.mle.fixed_poi_fit(test_poi, data, model, return_fitted_val=True)
-        (array([1.        , 0.97224597, 0.87553894]), 28.92218013492061)
+        (array([1.        , 0.97224597, 0.87553894]), array([28.92218013]))
 
     Args:
         data: The data
