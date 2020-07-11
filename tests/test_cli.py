ceac34f0633cf988068511191a77820c7e67b1e0
Lukas Heinrich <lukas.heinrich@gmail.com>
2020-07-11 13:56:18 +0200

    black

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,5 +4,5 @@ from pyhf.cli.cli import pyhf
 
 def test_shllcomplete_cli():
     runner = CliRunner()
-    result = runner.invoke(pyhf, ['shellcomplete', 'bash'])
+    result = runner.invoke(pyhf, ['completions', 'bash'])
     assert 'complete -F _pyhf_completion -o default pyhf' in result.output
