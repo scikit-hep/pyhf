052f80f0bd62e72404e5fc56f5af7d102bdeb27a
Lukas Heinrich <lukas.heinrich@gmail.com>
2020-07-11 13:54:47 +0200

    black

--- src/pyhf/cli/complete.py
+++ src/pyhf/cli/complete.py
@@ -1,6 +1,8 @@
 import click
+
 try:
     import click_completion
+
     click_completion.init()
 
     @click.command(help='Generate shell completion code.', name='completions')
@@ -12,11 +14,11 @@ try:
     def cli(shell):
         click.echo(click_completion.core.get_code(shell))
 
+
 except ImportError:
+
     @click.command(help='Generate shell completion code.', name='completions')
-    @click.argument(
-        'shell', default = None
-    )
+    @click.argument('shell', default=None)
     def cli(shell):
         click.secho(
             'This requires the click_completion module.\n'
