1a65ca7c76ea180844835bbff3f3e03903b1aac3
Matthew Feickert <matthew.feickert@cern.ch>
2020-07-21 13:54:25 -0500

    Add atleast_1d method

--- src/pyhf/tensor/numpy_backend.py
+++ src/pyhf/tensor/numpy_backend.py
@@ -53,6 +53,26 @@ class numpy_backend(object):
         Run any global setups for the numpy lib.
         """
 
+    def atleast_1d(self, *args):
+        """
+        Convert inputs to tensors with at least one dimension.
+
+        Example:
+
+            >>> import pyhf
+            >>> pyhf.set_backend("numpy")
+            >>> pyhf.tensorlib.atleast_1d(1., [3., 4.]))
+            [array([1.]), array([3., 4.])]
+
+        Args:
+            args (Array of Tensors): Sequence of arrays
+
+        Returns:
+            NumPy ndarray: An array, or list of arrays, each with `ndim >= 1`
+
+        """
+        return np.atleast_1d(*args)
+
     def clip(self, tensor_in, min_value, max_value):
         """
         Clips (limits) the tensor values to be within a specified min and max.
