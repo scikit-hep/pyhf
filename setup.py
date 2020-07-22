17dd0f35cc6c2251f9d71aaa1281e00ff692342a
Matthew Feickert <matthew.feickert@cern.ch>
2020-07-22 01:16:12 -0500

    chore: Set minimum version for iminuit to v1.4.3
    Corresponds to use of 'name' keyword in Minuit class in the MINUIT
    optimizer
    c.f. https://github.com/scikit-hep/iminuit/releases/tag/v1.4.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ extras_require = {
     'torch': ['torch~=1.2'],
     'jax': ['jax~=0.1,>0.1.51', 'jaxlib~=0.1,>0.1.33'],
     'xmlio': ['uproot~=3.6'],  # Future proof against uproot4 API changes
-    'minuit': ['iminuit'],
+    'minuit': ['iminuit>=1.4.3'],  # Use "name" keyword in MINUIT optimizer
 }
 extras_require['backends'] = sorted(
     set(
