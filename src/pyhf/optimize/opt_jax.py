66d7292efbd2e871db62e01dbf2ee793dbab6dbe
Giordon Stark <kratsg@gmail.com>
2020-07-22 22:57:53 -0400

    data should not be compiled in to jit

--- src/pyhf/optimize/opt_jax.py
+++ src/pyhf/optimize/opt_jax.py
@@ -8,7 +8,7 @@ import logging
 log = logging.getLogger(__name__)
 
 
-def _final_objective(pars, fixed_values, fixed_idx, variable_idx, objective, data, pdf):
+def _final_objective(pars, data, fixed_values, fixed_idx, variable_idx, objective, pdf):
     log.debug('jitting function')
     tensorlib, _ = get_backend()
     tv = _TensorViewer([fixed_idx, variable_idx])
@@ -20,10 +20,10 @@ def _final_objective(pars, fixed_values, fixed_idx, variable_idx, objective, dat
 
 
 _jitted_objective_and_grad = jax.jit(
-    jax.value_and_grad(_final_objective, argnums=0), static_argnums=(2, 3, 4, 5, 6)
+    jax.value_and_grad(_final_objective, argnums=0), static_argnums=(3, 4, 5, 6)
 )
 
-_jitted_objective = jax.jit(_final_objective, static_argnums=(2, 3, 4, 5, 6))
+_jitted_objective = jax.jit(_final_objective, static_argnums=(3, 4, 5, 6))
 
 
 def wrap_objective(objective, data, pdf, stitch_pars, do_grad=False, jit_pieces=None):
@@ -41,7 +41,6 @@ def wrap_objective(objective, data, pdf, stitch_pars, do_grad=False, jit_pieces=
         objective_and_grad (`func`): tensor backend wrapped objective,gradient pair
     """
     tensorlib, _ = get_backend()
-
     # NB: tuple arguments that need to be hashable (static_argnums)
     if do_grad:
 
@@ -49,11 +48,11 @@ def wrap_objective(objective, data, pdf, stitch_pars, do_grad=False, jit_pieces=
             # need to conver to tuple to make args hashable
             return _jitted_objective_and_grad(
                 pars,
+                data,
                 jit_pieces['fixed_values'],
                 tuple(jit_pieces['fixed_idx']),
                 tuple(jit_pieces['variable_idx']),
                 objective,
-                data,
                 pdf,
             )
 
@@ -63,11 +62,11 @@ def wrap_objective(objective, data, pdf, stitch_pars, do_grad=False, jit_pieces=
             # need to conver to tuple to make args hashable
             return _jitted_objective(
                 pars,
+                data,
                 jit_pieces['fixed_values'],
                 tuple(jit_pieces['fixed_idx']),
                 tuple(jit_pieces['variable_idx']),
                 objective,
-                data,
                 pdf,
             )
 
