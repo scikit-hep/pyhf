80bf3d021bfda08219177c7c08191f5f3c04457c
Matthew Feickert <matthew.feickert@cern.ch>
2020-07-28 14:11:31 -0500

    Change to FIXME to make easier to find

--- src/pyhf/writexml.py
+++ src/pyhf/writexml.py
@@ -32,12 +32,12 @@ def _make_hist_name(channel, sample, modifier='', prefix='hist', suffix=''):
 
 
 def _export_root_histogram(histname, data):
-    h = TH1((np.asarray(data), np.arange(len(data) + 1)))
-    h._fName = histname
-    # NB: uproot crashes for some reason, figure out why later
+    hist = TH1((np.asarray(data), np.arange(len(data) + 1)))
+    hist._fName = histname
+    # FIXME: uproot crashes for some reason, figure out why later
     # if histname in _ROOT_DATA_FILE:
     #    raise KeyError(f'Duplicate key {histname} being written.')
-    _ROOT_DATA_FILE[histname] = h
+    _ROOT_DATA_FILE[histname] = hist
 
 
 # https://stackoverflow.com/a/4590052
