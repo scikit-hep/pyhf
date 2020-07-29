c4e3dd2e1ccebda94a3e7326acc812f53c3db313
Lukas Heinrich <lukas.heinrich@gmail.com>
2020-07-29 22:29:54 +0200

    add cdf

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,6 +1,7 @@
 import pyhf
 import pyhf.infer.calculators
 
+
 def test_calc_distr():
-    a = pyhf.infer.calculators.AsymptoticTestStatDistribution(0.0)    
+    a = pyhf.infer.calculators.AsymptoticTestStatDistribution(0.0)
     assert a.pvalue(1) == a.cdf(-1)
