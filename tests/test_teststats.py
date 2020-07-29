67699ec9308adde5ed1156411e1d6d7738e0d10b
Lukas Heinrich <lukas.heinrich@gmail.com>
2020-07-29 14:57:20 +0200

    tests

--- tests/test_teststats.py
+++ tests/test_teststats.py
@@ -2,9 +2,10 @@ import pyhf
 import pyhf.infer.test_statistics
 import logging
 
+
 def test_qmu(caplog):
     mu = 1.0
-    pdf = pyhf.simplemodels.hepdata_like([6],[9],[3]) 
+    pdf = pyhf.simplemodels.hepdata_like([6], [9], [3])
     data = [9] + pdf.config.auxdata
     init_pars = pdf.config.suggested_init()
     par_bounds = pdf.config.suggested_bounds()
@@ -16,19 +17,20 @@ def test_qmu(caplog):
 
 def test_qmu_tilde(caplog):
     mu = 1.0
-    pdf = pyhf.simplemodels.hepdata_like([6],[9],[3]) 
+    pdf = pyhf.simplemodels.hepdata_like([6], [9], [3])
     data = [9] + pdf.config.auxdata
     init_pars = pdf.config.suggested_init()
     par_bounds = pdf.config.suggested_bounds()
 
-    par_bounds[pdf.config.poi_index] = [-10,10]
+    par_bounds[pdf.config.poi_index] = [-10, 10]
     with caplog.at_level(logging.WARNING, 'pyhf.infer.test_statistics'):
         pyhf.infer.test_statistics.qmu_tilde(mu, data, pdf, init_pars, par_bounds)
         assert 'WARNING  qmu tilde test statistic used for fit' in caplog.text
 
+
 def test_tmu(caplog):
     mu = 1.0
-    pdf = pyhf.simplemodels.hepdata_like([6],[9],[3]) 
+    pdf = pyhf.simplemodels.hepdata_like([6], [9], [3])
     data = [9] + pdf.config.auxdata
     init_pars = pdf.config.suggested_init()
     par_bounds = pdf.config.suggested_bounds()
@@ -36,16 +38,17 @@ def test_tmu(caplog):
         pyhf.infer.test_statistics.tmu(mu, data, pdf, init_pars, par_bounds)
         assert 'WARNING  tmu test statistic used for fit' in caplog.text
 
-    par_bounds[pdf.config.poi_index] = [-10,10]
+    par_bounds[pdf.config.poi_index] = [-10, 10]
+
 
 def test_tmu_tilde(caplog):
     mu = 1.0
-    pdf = pyhf.simplemodels.hepdata_like([6],[9],[3]) 
+    pdf = pyhf.simplemodels.hepdata_like([6], [9], [3])
     data = [9] + pdf.config.auxdata
     init_pars = pdf.config.suggested_init()
     par_bounds = pdf.config.suggested_bounds()
 
-    par_bounds[pdf.config.poi_index] = [-10,10]
+    par_bounds[pdf.config.poi_index] = [-10, 10]
     with caplog.at_level(logging.WARNING, 'pyhf.infer.test_statistics'):
         pyhf.infer.test_statistics.tmu_tilde(mu, data, pdf, init_pars, par_bounds)
         assert 'WARNING  tmu tilde test statistic used for fit' in caplog.text
