bf83f11adb8456f4310d286435975e6523fac489
Matthew Feickert <matthew.feickert@cern.ch>
2020-07-30 23:39:31 -0500

    Remove FIXME for model as is minimum spec already

--- tests/test_teststats.py
+++ tests/test_teststats.py
@@ -6,72 +6,71 @@ import logging
 
 def test_qmu(caplog):
     mu = 1.0
-    pdf = pyhf.simplemodels.hepdata_like([6], [9], [3])
-    data = [9] + pdf.config.auxdata
-    init_pars = pdf.config.suggested_init()
-    par_bounds = pdf.config.suggested_bounds()
+    model = pyhf.simplemodels.hepdata_like([6], [9], [3])
+    data = [9] + model.config.auxdata
+    init_pars = model.config.suggested_init()
+    par_bounds = model.config.suggested_bounds()
 
     with caplog.at_level(logging.WARNING, 'pyhf.infer.test_statistics'):
-        pyhf.infer.test_statistics.qmu(mu, data, pdf, init_pars, par_bounds)
+        pyhf.infer.test_statistics.qmu(mu, data, model, init_pars, par_bounds)
         assert 'WARNING  qmu test statistic used for fit' in caplog.text
         caplog.clear()
 
 
 def test_qmu_tilde(caplog):
     mu = 1.0
-    pdf = pyhf.simplemodels.hepdata_like([6], [9], [3])
-    data = [9] + pdf.config.auxdata
-    init_pars = pdf.config.suggested_init()
-    par_bounds = pdf.config.suggested_bounds()
+    model = pyhf.simplemodels.hepdata_like([6], [9], [3])
+    data = [9] + model.config.auxdata
+    init_pars = model.config.suggested_init()
+    par_bounds = model.config.suggested_bounds()
 
-    par_bounds[pdf.config.poi_index] = [-10, 10]
+    par_bounds[model.config.poi_index] = [-10, 10]
     with caplog.at_level(logging.WARNING, 'pyhf.infer.test_statistics'):
-        pyhf.infer.test_statistics.qmu_tilde(mu, data, pdf, init_pars, par_bounds)
+        pyhf.infer.test_statistics.qmu_tilde(mu, data, model, init_pars, par_bounds)
         assert 'WARNING  qmu_tilde test statistic used for fit' in caplog.text
         caplog.clear()
 
 
 def test_tmu(caplog):
     mu = 1.0
-    pdf = pyhf.simplemodels.hepdata_like([6], [9], [3])
-    data = [9] + pdf.config.auxdata
-    init_pars = pdf.config.suggested_init()
-    par_bounds = pdf.config.suggested_bounds()
+    model = pyhf.simplemodels.hepdata_like([6], [9], [3])
+    data = [9] + model.config.auxdata
+    init_pars = model.config.suggested_init()
+    par_bounds = model.config.suggested_bounds()
     with caplog.at_level(logging.WARNING, 'pyhf.infer.test_statistics'):
-        pyhf.infer.test_statistics.tmu(mu, data, pdf, init_pars, par_bounds)
+        pyhf.infer.test_statistics.tmu(mu, data, model, init_pars, par_bounds)
         assert 'WARNING  tmu test statistic used for fit' in caplog.text
         caplog.clear()
 
 
 def test_tmu_tilde(caplog):
     mu = 1.0
-    pdf = pyhf.simplemodels.hepdata_like([6], [9], [3])
-    data = [9] + pdf.config.auxdata
-    init_pars = pdf.config.suggested_init()
-    par_bounds = pdf.config.suggested_bounds()
+    model = pyhf.simplemodels.hepdata_like([6], [9], [3])
+    data = [9] + model.config.auxdata
+    init_pars = model.config.suggested_init()
+    par_bounds = model.config.suggested_bounds()
 
-    par_bounds[pdf.config.poi_index] = [-10, 10]
+    par_bounds[model.config.poi_index] = [-10, 10]
     with caplog.at_level(logging.WARNING, 'pyhf.infer.test_statistics'):
-        pyhf.infer.test_statistics.tmu_tilde(mu, data, pdf, init_pars, par_bounds)
+        pyhf.infer.test_statistics.tmu_tilde(mu, data, model, init_pars, par_bounds)
         assert 'WARNING  tmu_tilde test statistic used for fit' in caplog.text
         caplog.clear()
 
 
 def test_no_poi_test_stats(caplog):
-    # FIXME: Can reduce the model more
     spec = {
-        'channels': [
+        "channels": [
             {
-                'name': 'channel',
-                'samples': [
+                "name": "channel",
+                "samples": [
                     {
-                        'name': 'goodsample',
-                        'data': [10.0],
-                        'modifiers': [
+                        "name": "sample",
+                        "data": [10.0],
+                        "modifiers": [
                             {
-                                'type': 'normsys',
-                                'name': 'shape',
-                                'data': {"hi": 0.5, "lo": 1.5},
+                                "type": "normsys",
+                                "name": "shape",
+                                "data": {"hi": 0.5, "lo": 1.5},
                             }
                         ],
                     },
