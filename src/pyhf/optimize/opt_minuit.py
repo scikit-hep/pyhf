2d1b8efef36de4db2593c0285f108a908284ac90
Matthew Feickert <matthew.feickert@cern.ch>
2020-11-30 17:55:09 -0600

    feat: Use iminuit v1.5.X API (#1202)
    * Require iminuit compatible with v1.5.3 in 'minuit' extra
    * Adopt iminuit v1.5.3+ API
       - ncalls -> ncalls_total
       - ngrads -> ngrads_total
    * Remove mocking of iminuit.Minuit().from_array_func from tests
       - c.f. https://github.com/scikit-hep/iminuit/pull/464

--- src/pyhf/optimize/opt_minuit.py
+++ src/pyhf/optimize/opt_minuit.py
@@ -113,7 +113,7 @@ class minuit_optimizer(OptimizerMixin):
         minimizer.tol = tolerance
         minimizer.migrad(ncall=maxiter)
         # Following lines below come from:
-        # https://github.com/scikit-hep/iminuit/blob/22f6ed7146c1d1f3274309656d8c04461dde5ba3/src/iminuit/_minimize.py#L106-L125
+        # https://github.com/scikit-hep/iminuit/blob/64acac11cfa2fb91ccbd02d1b3c51f8a9e2cc484/src/iminuit/_minimize.py#L102-L121
         message = "Optimization terminated successfully."
         if not minimizer.valid:
             message = "Optimization failed."
@@ -141,7 +141,7 @@ class minuit_optimizer(OptimizerMixin):
             fun=minimizer.fval,
             hess_inv=hess_inv,
             message=message,
-            nfev=minimizer.ncalls,
-            njev=minimizer.ngrads,
+            nfev=minimizer.ncalls_total,
+            njev=minimizer.ngrads_total,
             minuit=minimizer,
         )
