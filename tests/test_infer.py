90dfddff267a0dfdde45acbe6d4efaa09bfebb98
Giordon Stark <kratsg@gmail.com>
2020-12-18 16:37:52 -0500

    fix warning codefactor

--- tests/test_infer.py
+++ tests/test_infer.py
@@ -25,9 +25,9 @@ def test_upperlimit(tmpdir, hypotest_args):
     Check that the default return structure of pyhf.infer.hypotest is as expected
     """
     _, data, model = hypotest_args
-    observed_limit, expected_limits = pyhf.infer.intervals.upperlimit(
-        data, model, scan=np.linspace(0, 5, 11)
-    )
+    results = pyhf.infer.intervals.upperlimit(data, model, scan=np.linspace(0, 5, 11))
+    assert len(results) == 2
+    observed_limit, expected_limits = results
     assert observed_limit == pytest.approx(1.0262704738584554)
     assert expected_limits == pytest.approx(
         [0.65765653, 0.87999725, 1.12453992, 1.50243428, 2.09232927]
