1794af7f64e719223af20bd7283d2e35e6f7a620
Lukas Heinrich <lukas.heinrich@gmail.com>
2020-12-22 22:23:18 +0100

    reset scipy

--- src/pyhf/optimize/opt_scipy.py
+++ src/pyhf/optimize/opt_scipy.py
@@ -29,56 +29,6 @@ class scipy_optimizer(OptimizerMixin):
     ):
         return scipy.optimize.minimize
 
-
-
-    def _custom_internal_minimize(self,objective, init_pars, maxiter = 1000,rtol = 1e-7):
-        import jax.experimental.optimizers as optimizers
-        import jax
-        opt_init, opt_update, opt_getpars = optimizers.adam(step_size = 1e-2)
-        state = opt_init(init_pars)
-        vold,_ = objective(init_pars)
-        def cond(loop_state):
-            delta = loop_state['delta']
-            i = loop_state['i']
-            delta_below  = jax.numpy.logical_and(loop_state['delta'] > 0,loop_state['delta'] < rtol)
-            delta_below  = jax.numpy.logical_and(loop_state['i'] > 1, delta_below)
-            maxed_iter = loop_state['i'] > maxiter
-            return ~jax.numpy.logical_or(maxed_iter,delta_below)
-            
-        def body(loop_state):
-            i = loop_state['i']
-            state = loop_state['state'] 
-            pars = opt_getpars(state)
-            v,g = objective(pars)
-            newopt_state = opt_update(0,g,state)
-            vold = loop_state['vold']
-            delta = jax.numpy.abs(v-vold)/v
-            new_state = {}
-            new_state['delta'] =  delta
-            new_state['state'] =  newopt_state
-            new_state['vold'] = v
-            new_state['i'] = i+1
-            return new_state
-
-        loop_state = {'delta': 0, 'i': 0, 'state': state, 'vold': vold}
-        # import time
-        # start = time.time()
-        # # while(cond(loop_state)):
-        #     loop_state = body(loop_state)
-        loop_state = jax.lax.while_loop(cond,body,loop_state)
-        # print(time.time()-start)
-
-
-        minimized = opt_getpars(loop_state['state'])
-        class Result:
-            pass
-        r = Result()
-        r.x = minimized
-        r.success = True
-        r.fun = objective(minimized)[0]
-        return r
-
-        
     def _minimize(
         self,
         minimizer,
@@ -122,18 +72,13 @@ class scipy_optimizer(OptimizerMixin):
         else:
             constraints = []
 
-        result = self._custom_internal_minimize(func,x0)
-        return result
-
-        result =  minimizer(
+        return minimizer(
             func,
             x0,
             method=method,
             jac=do_grad,
-            # bounds=bounds,
-            # constraints=constraints,
-            # tol=tolerance,
-            # options=dict(maxiter=maxiter, disp=bool(verbose)),
+            bounds=bounds,
+            constraints=constraints,
+            tol=tolerance,
+            options=dict(maxiter=maxiter, disp=bool(verbose)),
         )
-        print(result.fun)
-        return result
