096b28ba75a54181da9a04104612d2d73c554bd4
Matthew Feickert <matthew.feickert@cern.ch>
2021-03-22 03:12:22 -0500

    Add dtype for ones and zeroes to numpy

--- src/pyhf/tensor/numpy_backend.py
+++ src/pyhf/tensor/numpy_backend.py
@@ -222,11 +222,27 @@ class numpy_backend:
     def abs(self, tensor):
         return np.abs(tensor)
 
-    def ones(self, shape):
-        return np.ones(shape)
+    def ones(self, shape, dtype="float"):
+        try:
+            dtype = self.dtypemap[dtype]
+        except KeyError:
+            log.error(
+                "Invalid dtype: dtype must be float, int, or bool.", exc_info=True
+            )
+            raise
 
-    def zeros(self, shape):
-        return np.zeros(shape)
+        return np.ones(shape, dtype=dtype)
+
+    def zeros(self, shape, dtype="float"):
+        try:
+            dtype = self.dtypemap[dtype]
+        except KeyError:
+            log.error(
+                "Invalid dtype: dtype must be float, int, or bool.", exc_info=True
+            )
+            raise
+
+        return np.zeros(shape, dtype=dtype)
 
     def power(self, tensor_in_1, tensor_in_2):
         return np.power(tensor_in_1, tensor_in_2)
