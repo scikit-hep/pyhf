fix: Set precision for ones and zeros through dtype arg (#1369)
* Add dtype arg to tensorlib .ones and .zeros to set precision
   - Ensures that precision set at tensorlib set time is propagated
   - Default to the value of the tensor backend dtypemap
* Add test_tensor tests to cover invalid dtype to .ones and .zeros
* Update test_optim to use 32b values

src/pyhf/tensor/jax_backend.py
@@ -197,7 +197,7 @@ class jax_backend:
     def isfinite(self, tensor):
         return jnp.isfinite(tensor)
 
-    def astensor(self, tensor_in, dtype='float'):
+    def astensor(self, tensor_in, dtype="float"):
         """
         Convert to a JAX ndarray.
 
@@ -237,11 +237,29 @@ class jax_backend:
     def abs(self, tensor):
         return jnp.abs(tensor)
 
-    def ones(self, shape):
-        return jnp.ones(shape)
+    def ones(self, shape, dtype="float"):
+        try:
+            dtype = self.dtypemap[dtype]
+        except KeyError:
+            log.error(
+                f"Invalid dtype: dtype must be one of {list(self.dtypemap.keys())}.",
+                exc_info=True,
+            )
+            raise
 
-    def zeros(self, shape):
-        return jnp.zeros(shape)
+        return jnp.ones(shape, dtype=dtype)
+
+    def zeros(self, shape, dtype="float"):
+        try:
+            dtype = self.dtypemap[dtype]
+        except KeyError:
+            log.error(
+                f"Invalid dtype: dtype must be one of {list(self.dtypemap.keys())}.",
+                exc_info=True,
+            )
+            raise
+
+        return jnp.zeros(shape, dtype=dtype)
 
     def power(self, tensor_in_1, tensor_in_2):
         return jnp.power(tensor_in_1, tensor_in_2)

src/pyhf/tensor/pytorch_backend.py
@@ -245,11 +245,29 @@ class pytorch_backend:
     def abs(self, tensor):
         return torch.abs(tensor)
 
-    def ones(self, shape):
-        return torch.ones(shape, dtype=self.dtypemap['float'])
+    def ones(self, shape, dtype="float"):
+        try:
+            dtype = self.dtypemap[dtype]
+        except KeyError:
+            log.error(
+                f"Invalid dtype: dtype must be one of {list(self.dtypemap.keys())}.",
+                exc_info=True,
+            )
+            raise
 
-    def zeros(self, shape):
-        return torch.zeros(shape, dtype=self.dtypemap['float'])
+        return torch.ones(shape, dtype=dtype)
+
+    def zeros(self, shape, dtype="float"):
+        try:
+            dtype = self.dtypemap[dtype]
+        except KeyError:
+            log.error(
+                f"Invalid dtype: dtype must be one of {list(self.dtypemap.keys())}.",
+                exc_info=True,
+            )
+            raise
+
+        return torch.zeros(shape, dtype=dtype)
 
     def power(self, tensor_in_1, tensor_in_2):
         return torch.pow(tensor_in_1, tensor_in_2)

src/pyhf/tensor/tensorflow_backend.py
@@ -240,11 +240,29 @@ class tensorflow_backend:
     def abs(self, tensor):
         return tf.abs(tensor)
 
-    def ones(self, shape):
-        return tf.ones(shape, dtype=self.dtypemap['float'])
+    def ones(self, shape, dtype="float"):
+        try:
+            dtype = self.dtypemap[dtype]
+        except KeyError:
+            log.error(
+                f"Invalid dtype: dtype must be one of {list(self.dtypemap.keys())}.",
+                exc_info=True,
+            )
+            raise
 
-    def zeros(self, shape):
-        return tf.zeros(shape, dtype=self.dtypemap['float'])
+        return tf.ones(shape, dtype=dtype)
+
+    def zeros(self, shape, dtype="float"):
+        try:
+            dtype = self.dtypemap[dtype]
+        except KeyError:
+            log.error(
+                f"Invalid dtype: dtype must be one of {list(self.dtypemap.keys())}.",
+                exc_info=True,
+            )
+            raise
+
+        return tf.zeros(shape, dtype=dtype)
 
     def power(self, tensor_in_1, tensor_in_2):
         return tf.pow(tensor_in_1, tensor_in_2)