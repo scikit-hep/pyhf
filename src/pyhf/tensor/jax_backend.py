098b6c650c47ae7857001821b8d4379270f2cdfd
Matthew Feickert <matthew.feickert@cern.ch>
2021-03-25 11:10:16 -0500

    fix: Correct JAX tensor type in docstring (#1375)
    * Update JAX tensor type to jaxlib.xla_extension.DeviceArray in docs
       - Changed in JAX v0.2.11

--- src/pyhf/tensor/jax_backend.py
+++ src/pyhf/tensor/jax_backend.py
@@ -210,13 +210,13 @@ class jax_backend:
             DeviceArray([[1., 2., 3.],
                          [4., 5., 6.]], dtype=float64)
             >>> type(tensor)
-            <class 'jax.interpreters.xla._DeviceArray'>
+            <class 'jaxlib.xla_extension.DeviceArray'>
 
         Args:
             tensor_in (Number or Tensor): Tensor object
 
         Returns:
-            `jax.interpreters.xla._DeviceArray`: A multi-dimensional, fixed-size homogenous array.
+            `jaxlib.xla_extension.DeviceArray`: A multi-dimensional, fixed-size homogenous array.
         """
         try:
             dtype = self.dtypemap[dtype]
@@ -340,7 +340,7 @@ class jax_backend:
             tensor (Tensor): Tensor object
 
         Returns:
-            `jax.interpreters.xla._DeviceArray`: A flattened array.
+            `jaxlib.xla_extension.DeviceArray`: A flattened array.
         """
         return jnp.ravel(tensor)
 
