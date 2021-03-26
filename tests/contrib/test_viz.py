8d945cb4669f18b5bc6f2a5cfeb2705fe8a50fff
Matthew Feickert <matthew.feickert@cern.ch>
2021-03-26 01:03:55 -0500

    Add test for plot_cls_components

--- tests/contrib/test_viz.py
+++ tests/contrib/test_viz.py
@@ -3,10 +3,21 @@ import json
 import pyhf.contrib.viz.brazil as brazil
 import matplotlib.pyplot as plt
 
+# Compare against a reference image generated via:
+# pytest --mpl-generate-path=tests/contrib/baseline tests/contrib/test_viz.py
+
 
 @pytest.mark.mpl_image_compare
-def test_brazil():
+def test_plot_results():
     data = json.load(open('tests/contrib/hypotestresults.json'))
     fig, ax = plt.subplots(1, 1)
     brazil.plot_results(ax, data['testmus'], data['results'], test_size=0.05)
+    return fig
+
+
+@pytest.mark.mpl_image_compare
+def test_plot_cls_components():
+    data = json.load(open("tests/contrib/tail_probs_hypotest_results.json"))
+    fig, ax = plt.subplots(1, 1)
+    brazil.plot_cls_components(ax, data["testmus"], data["results"], test_size=0.05)
     return fig
