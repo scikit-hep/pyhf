15ce7f5276f2e40bc0effc0fd02c0120eff3ec6e
Matthew Feickert <matthew.feickert@cern.ch>
2021-05-09 01:31:54 -0500

    Add deprecated API warning test

--- tests/test_simplemodels.py
+++ tests/test_simplemodels.py
@@ -1,3 +1,5 @@
+import warnings
+
 import pyhf
 
 
@@ -22,3 +24,17 @@ def test_uncorrelated_background():
     assert model.config.samples == ["background", "signal"]
     assert model.config.par_order == ["mu", "uncorr_bkguncrt"]
     assert model.config.suggested_init() == [1.0, 1.0, 1.0]
+
+
+# TODO: Remove after pyhf v0.6.2 is released
+def test_deprecated_apis():
+    with warnings.catch_warnings(record=True) as _warning:
+        # Cause all warnings to always be triggered
+        warnings.simplefilter("always")
+        pyhf.simplemodels.hepdata_like([12.0, 11.0], [50.0, 52.0], [3.0, 7.0])
+        assert len(_warning) == 1
+        assert issubclass(_warning[-1].category, DeprecationWarning)
+        assert (
+            "pyhf.simplemodels.hepdata_like is deprecated in favor of pyhf.simplemodels.uncorrelated_background"
+            in str(_warning[-1].message)
+        )
