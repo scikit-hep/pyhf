16a9c8745c80189040a706d990b195ec05dae68b
Lukas Heinrich <lukas.heinrich@gmail.com>
2021-05-09 21:11:28 +0200

    commit

--- tests/test_compat.py
+++ tests/test_compat.py
@@ -1,3 +1,4 @@
+import pyhf
 import pyhf.compat
 
 def test_interpretation():
@@ -18,3 +19,25 @@ def test_interpretation():
     assert interp['is_scalar'] == True
     assert interp['name'] == 'mu'
     assert interp['element'] == 'n/a'
+
+
+def test_torootname():
+    m1 = pyhf.simplemodels.correlated_background([5],[50],[52],[48])
+    m2 = pyhf.simplemodels.hepdata_like([5],[50],[7])
+    m3 = pyhf.simplemodels.hepdata_like([5,6],[50,50],[7,8])
+
+    assert pyhf.compat.parset_to_rootnames(
+        m1.config.param_set('mu')
+    ) == 'mu'
+
+    assert pyhf.compat.parset_to_rootnames(
+        m1.config.param_set('correlated_bkg_uncertainty')
+    ) == 'alpha_correlated_bkg_uncertainty'
+
+    assert pyhf.compat.parset_to_rootnames(
+        m2.config.param_set('uncorr_bkguncrt')
+    ) == ['gamma_uncorr_bkguncrt_0']
+
+    assert pyhf.compat.parset_to_rootnames(
+        m3.config.param_set('uncorr_bkguncrt')
+    ) == ['gamma_uncorr_bkguncrt_0','gamma_uncorr_bkguncrt_1']
