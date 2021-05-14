9b8f0622962fee36afe91a146bde68080e035df2
Matthew Feickert <matthew.feickert@cern.ch>
2021-05-14 18:14:56 -0500

    update minimum sphinx release

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ extras_require['docs'] = sorted(
         extras_require['xmlio']
         + extras_require['contrib']
         + [
-            'sphinx>=3.1.2',
+            'sphinx>=4.0.0',
             'sphinxcontrib-bibtex~=2.1',
             'sphinx-click',
             'sphinx_rtd_theme',
