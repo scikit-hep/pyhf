b38784364b6e15c26bcdebc0ce0c8bf5c954c3ec
Matthew Feickert <matthew.feickert@cern.ch>
2021-06-09 10:56:49 -0500

    switch to tmpdir all

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -549,18 +549,18 @@ def test_patchset_download(tmpdir, script_runner, archive):
     assert ret.success
 
     # Run with all optional flags
-    command = f'pyhf contrib download --verbose --force {archive} {datadir.join("likelihoods").strpath}'
+    command = f'pyhf contrib download --verbose --force {archive} {tmpdir.join("likelihoods").strpath}'
     ret = script_runner.run(*shlex.split(command))
     assert ret.success
 
-    command = f'pyhf contrib download --verbose https://www.fail.org/record/resource/1234567 {datadir.join("likelihoods").strpath}'
+    command = f'pyhf contrib download --verbose https://www.fail.org/record/resource/1234567 {tmpdir.join("likelihoods").strpath}'
     ret = script_runner.run(*shlex.split(command))
     assert not ret.success
     assert (
         "pyhf.exceptions.InvalidArchiveHost: www.fail.org is not an approved archive host"
         in ret.stderr
     )
-    command = f'pyhf contrib download --verbose --force https://www.fail.org/record/resource/1234567 {datadir.join("likelihoods").strpath}'
+    command = f'pyhf contrib download --verbose --force https://www.fail.org/record/resource/1234567 {tmpdir.join("likelihoods").strpath}'
     ret = script_runner.run(*shlex.split(command))
     assert not ret.success
     assert (
