5e46d0848d36e85938b2256d2b948bf50df3261b
Matthew Feickert <matthew.feickert@cern.ch>
2021-07-21 23:37:15 -0500

    Revert "try to remove all casting to func and see if that works?"
    This reverts commit 9c3e558e36606a2268458f06c8e30c600d6cb321.

--- src/pyhf/events.py
+++ src/pyhf/events.py
@@ -28,7 +28,15 @@ class Callables:
         self._callbacks = []
 
     def _flush(self):
-        self._callbacks = [(func, arg) for func, arg in self._callbacks]
+        _callbacks = []
+        for func, arg in self._callbacks:
+            if arg is not None:
+                arg = arg()
+                if arg is None:
+                    print(func, arg, 'None')
+                    continue
+            _callbacks.append((func, arg))
+        self._callbacks = _callbacks
 
     def append(self, callback):
         try:
