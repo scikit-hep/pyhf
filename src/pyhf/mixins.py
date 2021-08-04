affbca5f004047893cbedada66ac17e03a2be09e
Matthew Feickert <matthew.feickert@cern.ch>
2021-08-04 10:42:28 -0500

    feat: sort channel_nbins with channels

--- src/pyhf/mixins.py
+++ src/pyhf/mixins.py
@@ -42,6 +42,9 @@ class _ChannelSummaryMixin:
         self.samples = sorted(list(set(self.samples)))
         self.parameters = sorted(list(set(self.parameters)))
         self.modifiers = sorted(list(set(self.modifiers)))
+        self.channel_nbins = {
+            self.channel_name: self.channel_nbins[channel] for channel in self.channels
+        }
 
         self.channel_slices = {}
         begin = 0
