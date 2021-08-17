51c3d15268c7ed268aeced6be74c56e168e1c619
Lars Henkelmann <lars.henkelmann@cern.ch>
2021-08-17 14:29:55 +0100

    failing tests for return_fitted_pars in generate_asimov_data

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,3 +1,5 @@
+import pytest
+
 import pyhf
 import pyhf.infer.calculators
 
@@ -5,3 +7,33 @@ import pyhf.infer.calculators
 def test_calc_dist():
     asymptotic_dist = pyhf.infer.calculators.AsymptoticTestStatDistribution(0.0)
     assert asymptotic_dist.pvalue(-1) == 1 - asymptotic_dist.cdf(-1)
+
+
+@pytest.mark.parametrize("return_fitted_pars", [False, True])
+def test_generate_asimov_can_return_fitted_pars(return_fitted_pars):
+    model = pyhf.simplemodels.uncorrelated_background([1, 1], [1, 1], [1, 1])
+    data = [2, 2, 1, 1]  # [main x 2, aux x 2]
+    init_pars = model.config.suggested_init()
+    par_bounds = model.config.suggested_bounds()
+    fixed_params = model.config.suggested_fixed()
+
+    result = pyhf.infer.calculators.generate_asimov_data(
+        1.0,
+        data,
+        model,
+        init_pars,
+        par_bounds,
+        fixed_params,
+        return_fitted_pars=return_fitted_pars,
+    )
+
+    if return_fitted_pars:
+        assert len(result) == 2
+        result, asimov_pars = result
+        # asimov_pars and result could be tensors, so convert to List[float] first
+        assert list(map(float, asimov_pars)) == [
+            1.0,
+            1.0,
+            1.0,
+        ]  # mu, 2x uncorr_bkguncrt
+    assert list(map(float, result)) == [2.0, 2.0, 1.0, 1.0]
