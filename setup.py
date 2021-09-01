4d17c22537721c9704cf866ce707dc356b6779c5
Matthew Feickert <matthew.feickert@cern.ch>
2021-08-31 22:45:07 -0500

    Set lower bounds for TF and TFP

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 extras_require = {
     'shellcomplete': ['click_completion'],
     'tensorflow': [
-        'tensorflow~=2.3.1',  # TensorFlow minor releases are as volatile as major
-        'tensorflow-probability~=0.11.0',
+        'tensorflow~=2.2,>=2.2.1',
+        'tensorflow-probability~=0.10,>=0.10.1',
     ],
     'torch': ['torch~=1.8'],
     'jax': ['jax~=0.2.8', 'jaxlib~=0.1.58,!=0.1.68'],  # c.f. Issue 1501
