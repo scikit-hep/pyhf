scikit-hep/pyhf

commit 6f4ca1a5cd0b629f5d88c9d6d9f57c4d9c07d99d

feat: Add setup for custom modifiers (#1625)
* This API breaking change allows for the start of addition of custom
modifiers.
* `pyhf.workspace.Workspace.parameters` are fully removed from the
API through removal from `pyhf.mixins`. Instead, parameters is added
as an attribute to the model config.
* Remove the modifier registry and in place add {modifier}_builder
classes for the modifiers. Additionally add
pyhf.modifiers.histfactory_set which is the map of all the
predefined modifiers that HistFactory specifies.
* All workspace parameters related tests are removed and tests are
added for the change to model.config
* The change from workspace.parameters to model.config.parameters
is made in all affected example notebooks.
* Apply a tiny relative tolerance change in the regression tests to
increase to rel=1.5e-5. The resulting tolerance is on the order of 1e-7
and so the change is inconsequential.

src/pyhf/pdf.py
@@ -3,9 +3,9 @@
 import copy
 import logging
 
+import pyhf.parameters
 from pyhf import get_backend, default_backend
 from pyhf import exceptions
-from pyhf import modifiers
 from pyhf import utils
 from pyhf import events
 from pyhf import probability as prob
@@ -13,6 +13,7 @@ from pyhf.constraints import gaussian_constraint_combined, poisson_constraint_co
 from pyhf.parameters import reduce_paramsets_requirements, ParamViewer
 from pyhf.tensor.common import _TensorViewer, _tensorviewer_from_sizes
 from pyhf.mixins import _ChannelSummaryMixin
+from pyhf.modifiers import histfactory_set
 
 log = logging.getLogger(__name__)
 
@@ -23,88 +24,62 @@ def __dir__():
     return __all__
 
 
-def _paramset_requirements_from_channelspec(spec, channel_nbins):
-    # bookkeep all requirements for paramsets we need to build
-    _paramsets_requirements = {}
-    # need to keep track in which order we added the constraints
-    # so that we can generate correctly-ordered data
-    for channel in spec['channels']:
-        for sample in channel['samples']:
-            if len(sample['data']) != channel_nbins[channel['name']]:
-                raise exceptions.InvalidModel(
-                    f"The sample {sample['name']:s} has {len(sample['data']):d} bins, but the channel it belongs to ({channel['name']:s}) has {channel_nbins[channel['name']]:d} bins."
-                )
-            for modifier_def in sample['modifiers']:
-                # get the paramset requirements for the given modifier. If
-                # modifier does not exist, we'll have a KeyError
-                try:
-                    paramset_requirements = modifiers.registry[
-                        modifier_def['type']
-                    ].required_parset(sample['data'], modifier_def['data'])
-                except KeyError:
-                    log.exception(
-                        f"Modifier not implemented yet (processing {modifier_def['type']:s}). Available modifiers: {modifiers.registry.keys()}"
-                    )
-                    raise exceptions.InvalidModifier()
-
-                # check the shareability (e.g. for shapesys for example)
-                is_shared = paramset_requirements['is_shared']
-                if not (is_shared) and modifier_def['name'] in _paramsets_requirements:
-                    raise ValueError(
-                        "Trying to add unshared-paramset but other paramsets exist with the same name."
-                    )
-                if is_shared and not (
-                    _paramsets_requirements.get(
-                        modifier_def['name'], [{'is_shared': True}]
-                    )[0]['is_shared']
-                ):
-                    raise ValueError(
-                        "Trying to add shared-paramset but other paramset of same name is indicated to be unshared."
-                    )
-                _paramsets_requirements.setdefault(modifier_def['name'], []).append(
-                    paramset_requirements
-                )
-    return _paramsets_requirements
-
-
-def _paramset_requirements_from_modelspec(spec, channel_nbins):
-    _paramsets_requirements = _paramset_requirements_from_channelspec(
-        spec, channel_nbins
-    )
-
+def _finalize_parameters(user_parameters, _paramsets_requirements):
     # build up a dictionary of the parameter configurations provided by the user
     _paramsets_user_configs = {}
-    for parameter in spec.get('parameters', []):
+    for parameter in user_parameters:
         if parameter['name'] in _paramsets_user_configs:
             raise exceptions.InvalidModel(
                 f"Multiple parameter configurations for {parameter['name']} were found."
             )
         _paramsets_user_configs[parameter.get('name')] = parameter
-
     _reqs = reduce_paramsets_requirements(
         _paramsets_requirements, _paramsets_user_configs
     )
-
     _sets = {}
     for param_name, paramset_requirements in _reqs.items():
-        paramset_type = paramset_requirements.get('paramset_type')
+        paramset_type = getattr(pyhf.parameters, paramset_requirements['paramset_type'])
         paramset = paramset_type(**paramset_requirements)
         _sets[param_name] = paramset
 
     return _sets
 
 
-def _nominal_and_modifiers_from_spec(config, spec):
-    default_data_makers = {
-        'histosys': lambda: {'hi_data': [], 'lo_data': [], 'nom_data': [], 'mask': []},
-        'lumi': lambda: {'mask': []},
-        'normsys': lambda: {'hi': [], 'lo': [], 'nom_data': [], 'mask': []},
-        'normfactor': lambda: {'mask': []},
-        'shapefactor': lambda: {'mask': []},
-        'shapesys': lambda: {'mask': [], 'uncrt': [], 'nom_data': []},
-        'staterror': lambda: {'mask': [], 'uncrt': [], 'nom_data': []},
-    }
+class _nominal_builder:
+    def __init__(self, config):
+        self.mega_samples = {}
+        self.config = config
 
+    def append(self, channel, sample, defined_samp):
+        self.mega_samples.setdefault(sample, {'name': f'mega_{sample}', 'nom': []})
+        nom = (
+            defined_samp['data']
+            if defined_samp
+            else [0.0] * self.config.channel_nbins[channel]
+        )
+        if not len(nom) == self.config.channel_nbins[channel]:
+            raise exceptions.InvalidModel(
+                f'expected {self.config.channel_nbins[channel]} size sample data but got {len(nom)}'
+            )
+        self.mega_samples[sample]['nom'] += nom
+
+    def finalize(self):
+        nominal_rates = default_backend.astensor(
+            [self.mega_samples[sample]['nom'] for sample in self.config.samples]
+        )
+        _nominal_rates = default_backend.reshape(
+            nominal_rates,
+            (
+                1,  # modifier dimension.. nominal_rates is the base
+                len(self.config.samples),
+                1,  # alphaset dimension
+                sum(list(self.config.channel_nbins.values())),
+            ),
+        )
+        return _nominal_rates
+
+
+def _nominal_and_modifiers_from_spec(modifier_set, config, spec, batch_size):
     # the mega-channel will consist of mega-samples that subscribe to
     # mega-modifiers. i.e. while in normal histfactory, each sample might
     # be affected by some modifiers and some not, here we change it so that
@@ -115,99 +90,72 @@ def _nominal_and_modifiers_from_spec(config, spec):
     #
     # We don't actually set up the modifier data here for no-ops, but we do
     # set up the entire structure
-    mega_mods = {}
-    for m, mtype in config.modifiers:
-        for s in config.samples:
-            key = f'{mtype}/{m}'
-            mega_mods.setdefault(key, {})[s] = {
-                'type': mtype,
-                'name': m,
-                'data': default_data_makers[mtype](),
-            }
 
     # helper maps channel-name/sample-name to pairs of channel-sample structs
     helper = {}
     for c in spec['channels']:
         for s in c['samples']:
-            helper.setdefault(c['name'], {})[s['name']] = (c, s)
+            moddict = {}
+            for x in s['modifiers']:
+                if x['type'] not in modifier_set:
+                    raise exceptions.InvalidModifier(
+                        f'{x["type"]} not among {list(modifier_set.keys())}'
+                    )
+                moddict[f"{x['type']}/{x['name']}"] = x
+            helper.setdefault(c['name'], {})[s['name']] = (s, moddict)
 
-    mega_samples = {}
-    for s in config.samples:
-        mega_nom = []
-        for c in config.channels:
-            defined_samp = helper.get(c, {}).get(s)
-            defined_samp = None if not defined_samp else defined_samp[1]
-            # set nominal to 0 for channel/sample if the pair doesn't exist
-            nom = (
-                defined_samp['data']
-                if defined_samp
-                else [0.0] * config.channel_nbins[c]
+    modifiers_builders = {}
+    for k, (builder, applier) in modifier_set.items():
+        modifiers_builders[k] = builder(config)
+
+    nominal = _nominal_builder(config)
+
+    for c in config.channels:
+        for s in config.samples:
+            helper_data = helper.get(c, {}).get(s)
+            defined_samp, defined_mods = (
+                (None, None) if not helper_data else helper_data
             )
-            mega_nom += nom
-            defined_mods = (
-                {f"{x['type']}/{x['name']}": x for x in defined_samp['modifiers']}
-                if defined_samp
-                else {}
-            )
+            nominal.append(c, s, defined_samp)
             for m, mtype in config.modifiers:
                 key = f'{mtype}/{m}'
                 # this is None if modifier doesn't affect channel/sample.
-                thismod = defined_mods.get(key)
-                # print('key',key,thismod['data'] if thismod else None)
-                if mtype == 'histosys':
-                    lo_data = thismod['data']['lo_data'] if thismod else nom
-                    hi_data = thismod['data']['hi_data'] if thismod else nom
-                    maskval = bool(thismod)
-                    mega_mods[key][s]['data']['lo_data'] += lo_data
-                    mega_mods[key][s]['data']['hi_data'] += hi_data
-                    mega_mods[key][s]['data']['nom_data'] += nom
-                    mega_mods[key][s]['data']['mask'] += [maskval] * len(
-                        nom
-                    )  # broadcasting
-                elif mtype == 'normsys':
-                    maskval = bool(thismod)
-                    lo_factor = thismod['data']['lo'] if thismod else 1.0
-                    hi_factor = thismod['data']['hi'] if thismod else 1.0
-                    mega_mods[key][s]['data']['nom_data'] += [1.0] * len(nom)
-                    mega_mods[key][s]['data']['lo'] += [lo_factor] * len(
-                        nom
-                    )  # broadcasting
-                    mega_mods[key][s]['data']['hi'] += [hi_factor] * len(nom)
-                    mega_mods[key][s]['data']['mask'] += [maskval] * len(
-                        nom
-                    )  # broadcasting
-                elif mtype in ['normfactor', 'shapefactor', 'lumi']:
-                    maskval = bool(thismod)
-                    mega_mods[key][s]['data']['mask'] += [maskval] * len(
-                        nom
-                    )  # broadcasting
-                elif mtype in ['shapesys', 'staterror']:
-                    uncrt = thismod['data'] if thismod else [0.0] * len(nom)
-                    if mtype == 'shapesys':
-                        maskval = [(x > 0 and y > 0) for x, y in zip(uncrt, nom)]
-                    else:
-                        maskval = [bool(thismod)] * len(nom)
-                    mega_mods[key][s]['data']['mask'] += maskval
-                    mega_mods[key][s]['data']['uncrt'] += uncrt
-                    mega_mods[key][s]['data']['nom_data'] += nom
+                thismod = defined_mods.get(key) if defined_mods else None
+                modifiers_builders[mtype].append(key, c, s, thismod, defined_samp)
+    nominal_rates = nominal.finalize()
 
-        sample_dict = {'name': f'mega_{s}', 'nom': mega_nom}
-        mega_samples[s] = sample_dict
+    _required_paramsets = {}
 
-    nominal_rates = default_backend.astensor(
-        [mega_samples[s]['nom'] for s in config.samples]
+    for v in list(modifiers_builders.values()):
+        for pname, req_list in v.required_parsets.items():
+            _required_paramsets.setdefault(pname, [])
+            _required_paramsets[pname] += req_list
+
+    user_parameters = spec.get('parameters', [])
+
+    _required_paramsets = _finalize_parameters(
+        user_parameters,
+        _required_paramsets,
     )
-    _nominal_rates = default_backend.reshape(
-        nominal_rates,
-        (
-            1,  # modifier dimension.. nominal_rates is the base
-            len(config.samples),
-            1,  # alphaset dimension
-            sum(list(config.channel_nbins.values())),
-        ),
-    )
+    if not _required_paramsets:
+        raise exceptions.InvalidModel('No parameters specified for the Model.')
 
-    return mega_mods, _nominal_rates
+    config.set_parameters(_required_paramsets)
+    the_modifiers = {}
+    for k, (builder, applier) in modifier_set.items():
+        the_modifiers[k] = applier(
+            modifiers=[
+                x for x in config.modifiers if x[1] == k
+            ],  # filter modifier names for that mtype (x[1])
+            pdfconfig=config,
+            builder_data=modifiers_builders[k].finalize()
+            if k in modifiers_builders
+            else None,
+            batch_size=batch_size,
+            **config.modifier_settings.get(k, {}),
+        )
+
+    return the_modifiers, nominal_rates
 
 
 class _ModelConfig(_ChannelSummaryMixin):
@@ -228,10 +176,6 @@ class _ModelConfig(_ChannelSummaryMixin):
             spec (:obj:`jsonable`): The HistFactory JSON specification.
         """
         super().__init__(channels=spec['channels'])
-        _required_paramsets = _paramset_requirements_from_modelspec(
-            spec, self.channel_nbins
-        )
-        poi_name = config_kwargs.pop('poi_name', 'mu')
 
         default_modifier_settings = {
             'normsys': {'interpcode': 'code4'},
@@ -253,13 +197,15 @@ class _ModelConfig(_ChannelSummaryMixin):
         self.poi_index = None
         self.auxdata = []
         self.auxdata_order = []
-
-        self._create_and_register_paramsets(_required_paramsets)
-        if poi_name is not None:
-            self.set_poi(poi_name)
-
-        self.npars = len(self.suggested_init())
         self.nmaindata = sum(self.channel_nbins.values())
+
+    def set_parameters(self, _required_paramsets):
+        """
+        Evaluate the required parameters for the model configuration.
+        """
+        self._create_and_register_paramsets(_required_paramsets)
+        self.npars = len(self.suggested_init())
+        self.parameters = sorted(k for k in self.par_map.keys())
 
     def suggested_init(self):
         """
@@ -523,34 +469,24 @@ class _ConstraintModel:
 class _MainModel:
     """Factory class to create pdfs for the main measurement."""
 
-    def __init__(self, config, mega_mods, nominal_rates, batch_size):
+    def __init__(self, config, modifiers, nominal_rates, batch_size=None):
         self.config = config
-        self._factor_mods = [
-            modtype
-            for modtype, mod in modifiers.uncombined.items()
-            if mod.op_code == 'multiplication'
-        ]
-        self._delta_mods = [
-            modtype
-            for modtype, mod in modifiers.uncombined.items()
-            if mod.op_code == 'addition'
-        ]
+
+        self._factor_mods = []
+        self._delta_mods = []
         self.batch_size = batch_size
 
         self._nominal_rates = default_backend.tile(
             nominal_rates, (1, 1, self.batch_size or 1, 1)
         )
 
-        self.modifiers_appliers = {
-            k: c(
-                [x for x in config.modifiers if x[1] == k],  # x[1] is mtype
-                config,
-                mega_mods,
-                batch_size=self.batch_size,
-                **config.modifier_settings.get(k, {}),
-            )
-            for k, c in modifiers.combined.items()
-        }
+        self.modifiers_appliers = modifiers
+
+        for k, v in self.modifiers_appliers.items():
+            if v.op_code == 'addition':
+                self._delta_mods.append(v.name)
+            elif v.op_code == 'multiplication':
+                self._factor_mods.append(v.name)
 
         self._precompute()
         events.subscribe('tensorlib_changed')(self._precompute)
@@ -659,7 +595,9 @@ class _MainModel:
 class Model:
     """The main pyhf model class."""
 
-    def __init__(self, spec, batch_size=None, **config_kwargs):
+    def __init__(
+        self, spec, modifier_set=None, batch_size=None, validate=True, **config_kwargs
+    ):
         """
         Construct a HistFactory Model.
 
@@ -672,6 +610,8 @@ class Model:
             model (:class:`~pyhf.pdf.Model`): The Model instance.
 
         """
+        modifier_set = modifier_set or histfactory_set
+
         self.batch_size = batch_size
         # deep-copy "spec" as it may be modified by config
         self.spec = copy.deepcopy(spec)
@@ -679,14 +619,22 @@ class Model:
         self.version = config_kwargs.pop('version', None)
         # run jsonschema validation of input specification against the (provided) schema
         log.info(f"Validating spec against schema: {self.schema:s}")
-        utils.validate(self.spec, self.schema, version=self.version)
+        if validate:
+            utils.validate(self.spec, self.schema, version=self.version)
         # build up our representation of the specification
-        self.config = _ModelConfig(spec, **config_kwargs)
+        poi_name = config_kwargs.pop('poi_name', 'mu')
+        self.config = _ModelConfig(self.spec, **config_kwargs)
 
-        mega_mods, _nominal_rates = _nominal_and_modifiers_from_spec(self.config, spec)
+        modifiers, _nominal_rates = _nominal_and_modifiers_from_spec(
+            modifier_set, self.config, self.spec, self.batch_size
+        )
+
+        if poi_name is not None:
+            self.config.set_poi(poi_name)
+
         self.main_model = _MainModel(
             self.config,
-            mega_mods=mega_mods,
+            modifiers=modifiers,
             nominal_rates=_nominal_rates,
             batch_size=self.batch_size,
         )

docs/examples/notebooks/binderexample/StatisticalAnalysis.ipynb
@@ -148,8 +148,8 @@
     }
    ],
    "source": [
-    "print(f'Samples:\\n {workspace.samples}')\n",
-    "print(f'Parameters:\\n {workspace.parameters}')"
+    "print(f'Samples:\\n {pdf.config.samples}')\n",
+    "print(f'Parameters:\\n {pdf.config.parameters}')"
    ]
   },
   {

src/pyhf/cli/spec.py
@@ -6,6 +6,7 @@ import json
 
 from pyhf.workspace import Workspace
 from pyhf import modifiers
+from pyhf import parameters
 from pyhf import utils
 
 log = logging.getLogger(__name__)
@@ -72,14 +73,16 @@ def inspect(workspace, output_file, measurement):
     ]
     result['modifiers'] = dict(ws.modifiers)
 
+    parset_descr = {
+        parameters.paramsets.unconstrained: 'unconstrained',
+        parameters.paramsets.constrained_by_normal: 'constrained_by_normal',
+        parameters.paramsets.constrained_by_poisson: 'constrained_by_poisson',
+    }
+
+    model = ws.model()
+
     result['parameters'] = sorted(
-        (
-            parname,
-            modifiers.registry[result['modifiers'][parname]]
-            .required_parset([], [])['paramset_type']
-            .__name__,
-        )
-        for parname in ws.parameters
+        (k, parset_descr[type(v['paramset'])]) for k, v in model.config.par_map.items()
     )
     result['systematics'] = [
         (
@@ -97,7 +100,7 @@ def inspect(workspace, output_file, measurement):
 
     maxlen_channels = max(map(len, ws.channels))
     maxlen_samples = max(map(len, ws.samples))
-    maxlen_parameters = max(map(len, ws.parameters))
+    maxlen_parameters = max(map(len, [p for p, _ in result['parameters']]))
     maxlen_measurements = max(map(lambda x: len(x[0]), result['measurements']))
     maxlen = max(
         [maxlen_channels, maxlen_samples, maxlen_parameters, maxlen_measurements]
@@ -174,7 +177,7 @@ def inspect(workspace, output_file, measurement):
     '--modifier-type',
     default=[],
     multiple=True,
-    type=click.Choice(modifiers.uncombined.keys()),
+    type=click.Choice(modifiers.histfactory_set.keys()),
 )
 @click.option('--measurement', default=[], multiple=True, metavar='<MEASUREMENT>...')
 def prune(

src/pyhf/infer/calculators.py
@@ -628,7 +628,7 @@ class EmpiricalDistribution:
             >>> samples = normal.sample((100,))
             >>> dist = pyhf.infer.calculators.EmpiricalDistribution(samples)
             >>> dist.expected_value(nsigma=1)
-            6.15094381209505
+            6.15094381209...
 
             >>> import pyhf
             >>> import numpy.random as random

src/pyhf/mixins.py
@@ -18,7 +18,6 @@ class _ChannelSummaryMixin:
         super().__init__(*args, **kwargs)
         self.channels = []
         self.samples = []
-        self.parameters = []
         self.modifiers = []
         # keep track of the width of each channel (how many bins)
         self.channel_nbins = {}
@@ -30,7 +29,6 @@ class _ChannelSummaryMixin:
             for sample in channel['samples']:
                 self.samples.append(sample['name'])
                 for modifier_def in sample['modifiers']:
-                    self.parameters.append(modifier_def['name'])
                     self.modifiers.append(
                         (
                             modifier_def['name'],  # mod name
@@ -40,7 +38,6 @@ class _ChannelSummaryMixin:
 
         self.channels = sorted(list(set(self.channels)))
         self.samples = sorted(list(set(self.samples)))
-        self.parameters = sorted(list(set(self.parameters)))
         self.modifiers = sorted(list(set(self.modifiers)))
         self.channel_nbins = {
             channel: self.channel_nbins[channel] for channel in self.channels

commit 2275660e7687f2b26bcd6266f23d2d5e9fa1bba6

fix: custom modifier / new parameter support and test (#1644)
* Fix a regression introduced in PR #1625
* Check that the poi set is an existing parameter in the model
* Add tests for custom modifiers to cover the use case and make sure it's caught

src/pyhf/pdf.py
@@ -357,7 +357,7 @@ class _ModelConfig(_ChannelSummaryMixin):
             >>> model.config.poi_name
             'mu'
         """
-        if name not in [x for x, _ in self.modifiers]:
+        if name not in self.parameters:
             raise exceptions.InvalidModel(
                 f"The parameter of interest '{name:s}' cannot be fit as it is not declared in the model specification."
             )

tests/test_custom_mods.py
@@ -1,0 +1,109 @@
+import pyhf
+import pyhf.exceptions as exceptions
+import pytest
+
+
+class custom_builder:
+    def __init__(self, pdfconfig):
+        self.config = pdfconfig
+        self.required_parsets = {
+            'k1': [
+                {
+                    'paramset_type': 'unconstrained',
+                    'n_parameters': 1,
+                    'is_constrained': False,
+                    'is_shared': True,
+                    'inits': (1.0,),
+                    'bounds': ((-5, 5),),
+                    'fixed': False,
+                }
+            ]
+        }
+        self.builder_data = {}
+
+    def append(self, key, channel, sample, thismod, defined_samp):
+        pass
+
+    def finalize(self):
+        return self.builder_data
+
+
+class custom_applicator:
+    op_code = 'multiplication'
+    name = 'customfunc'
+
+    def __init__(
+        self, modifiers=None, pdfconfig=None, builder_data=None, batch_size=None
+    ):
+        pass
+
+    def apply(self, pars):
+        raise NotImplementedError
+
+
+def test_custom_mods():
+    modifier_set = {custom_applicator.name: (custom_builder, custom_applicator)}
+    modifier_set.update(**pyhf.modifiers.histfactory_set)
+
+    model = pyhf.Model(
+        {
+            'channels': [
+                {
+                    'name': 'singlechannel',
+                    'samples': [
+                        {
+                            'name': 'signal',
+                            'data': [10] * 20,
+                            'modifiers': [
+                                {
+                                    'name': 'singlemod',
+                                    'type': 'customfunc',
+                                    'data': None,
+                                },
+                            ],
+                        },
+                        {'name': 'background', 'data': [300] * 20, 'modifiers': []},
+                    ],
+                }
+            ]
+        },
+        modifier_set=modifier_set,
+        poi_name='k1',
+        validate=False,
+    )
+    assert model
+    assert 'k1' in model.config.parameters
+
+
+def test_missing_poi():
+    modifier_set = {custom_applicator.name: (custom_builder, custom_applicator)}
+    modifier_set.update(**pyhf.modifiers.histfactory_set)
+
+    with pytest.raises(exceptions.InvalidModel):
+        model = pyhf.Model(
+            {
+                'channels': [
+                    {
+                        'name': 'singlechannel',
+                        'samples': [
+                            {
+                                'name': 'signal',
+                                'data': [10] * 20,
+                                'modifiers': [
+                                    {
+                                        'name': 'singlemod',
+                                        'type': 'customfunc',
+                                        'data': None,
+                                    },
+                                ],
+                            },
+                            {'name': 'background', 'data': [300] * 20, 'modifiers': []},
+                        ],
+                    }
+                ]
+            },
+            modifier_set=modifier_set,
+            poi_name='non_existent_poi',
+            validate=False,
+        )
+        assert model