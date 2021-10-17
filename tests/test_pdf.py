d2846b5fa58bfb31ab91c45b9a5a30974140dcad
Lukas Heinrich <lukas.heinrich@gmail.com>
2021-10-17 17:34:33 +0200

    initial cocmmit

--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -1067,7 +1067,7 @@ def test_make_model_with_tensors():
 
         pars = model.config.suggested_init()
         exp_data = model.expected_data(pars)
-        assert exp_data
+        assert exp_data is not None
 
     make_model(
         pyhf.tensorlib.astensor([60.0, 62.0]),
