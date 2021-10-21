e0433b50ea5f1495e87b871cfcf66305d8086880
Matthew Feickert <matthew.feickert@cern.ch>
2021-10-21 18:09:01 -0500

    Update torch requirement for API stability

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ extras_require = {
         'tensorflow~=2.2,>=2.2.1,!=2.3.0',  # c.f. https://github.com/tensorflow/tensorflow/pull/40789
         'tensorflow-probability~=0.10,>=0.10.1',
     ],
-    'torch': ['torch~=1.8'],
+    'torch': ['torch~=1.10'],
     'jax': ['jax~=0.2.8', 'jaxlib~=0.1.58,!=0.1.68'],  # c.f. Issue 1501
     'xmlio': ['uproot>=4.1.1'],
     'minuit': ['iminuit>=2.4'],
