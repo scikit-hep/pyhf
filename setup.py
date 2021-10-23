c3d0acb2591f34fc3bd169fb525fa1f6b4c8f369
Matthew Feickert <matthew.feickert@cern.ch>
2021-10-22 22:44:23 -0500

    build: Update lower bound on jax to v0.2.10 (#1666)
    * Update the lower bound of the supported JAX versions to jax v0.2.10 and jaxlib v0.1.60
       - jax v0.2.10 requires jaxlib v0.1.60+
    * Set only lower bounds for jax and jaxlib in 'jax' extra

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ extras_require = {
         'tensorflow-probability~=0.11',
     ],
     'torch': ['torch~=1.10'],
-    'jax': ['jax~=0.2.8', 'jaxlib~=0.1.58,!=0.1.68'],  # c.f. Issue 1501
+    'jax': ['jax>=0.2.10', 'jaxlib>=0.1.60,!=0.1.68'],  # c.f. Issue 1501
     'xmlio': ['uproot>=4.1.1'],
     'minuit': ['iminuit>=2.4'],
 }
