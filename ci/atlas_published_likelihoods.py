ee779c2c98c048ca0d85b4af5ced1ae66f30a526
Giordon Stark <kratsg@gmail.com>
2021-11-09 09:25:24 -0800

    fix up

--- ci/atlas_published_likelihoods.py
+++ ci/atlas_published_likelihoods.py
@@ -1,8 +1,19 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 url = 'https://twiki.cern.ch/twiki/bin/view/AtlasPublic'
 
-with webdriver.Chrome() as driver:
+options = Options()
+options.add_argument("--headless")
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-gpu')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('disable-infobars')
+options.add_argument("--disable-extensions")
+
+with webdriver.Chrome(
+    chrome_options=options, executable_path='/usr/bin/chromedriver'
+) as driver:
     driver.get(url)
     driver.execute_script("addKeyword('Analysischaracteristics_Likelihood@available');")
     rows = driver.find_elements('css selector', '#paperListTbody tr')
