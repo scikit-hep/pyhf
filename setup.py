8c90450d1ecf2d212d2590541538621f4da8e3ce
Matthew Feickert <matthew.feickert@cern.ch>
2021-11-16 13:53:47 -0600

    build: Set only lower bounds on backend dependencies (#1698)
    * Set only lower bounds on the requirements for all backend extras.
       - This is done to avoid creating installation blocking scenarios
         and in situations where users find that the latest release of
         a dependency breaks pyhf code the user can be recommended to
         install the version of that dependency that was available when
         their version of pyhf was released.
    * Add notes in setup.py pointing to the PR that caused the lower bound
    to be needed for easy future reference

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 extras_require = {
     'shellcomplete': ['click_completion'],
     'tensorflow': [
-        'tensorflow~=2.3,!=2.3.0',  # c.f. https://github.com/tensorflow/tensorflow/pull/40789
-        'tensorflow-probability~=0.11',
+        'tensorflow>=2.3.1',  # c.f. https://github.com/tensorflow/tensorflow/pull/40789
+        'tensorflow-probability>=0.11.0',  # c.f. PR #1657
     ],
-    'torch': ['torch~=1.10'],
+    'torch': ['torch>=1.10.0'],  # c.f. PR #1657
     'jax': ['jax>=0.2.10', 'jaxlib>=0.1.60,!=0.1.68'],  # c.f. Issue 1501
-    'xmlio': ['uproot>=4.1.1'],
-    'minuit': ['iminuit>=2.4'],
+    'xmlio': ['uproot>=4.1.1'],  # c.f. PR #1567
+    'minuit': ['iminuit>=2.4.0'],  # c.f. PR #1306
 }
 extras_require['backends'] = sorted(
     set(
@@ -29,7 +29,7 @@ extras_require['test'] = sorted(
         + extras_require['contrib']
         + extras_require['shellcomplete']
         + [
-            'pytest~=6.0',
+            'pytest>=6.0',
             'pytest-cov>=2.5.1',
             'pytest-mock',
             'pytest-benchmark[histogram]',
