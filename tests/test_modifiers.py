8a4c0011f28107c9fef210670e832ee79fe85d60
Matthew Feickert <matthew.feickert@cern.ch>
2021-11-19 00:42:16 -0600

    Delete old unused code

--- tests/test_modifiers.py
+++ tests/test_modifiers.py
@@ -1,15 +1,6 @@
-import pyhf
 import numpy
 
-# modifiers_to_test = [
-#     "histosys",
-#     "normfactor",
-#     "normsys",
-#     "shapefactor",
-#     "shapesys",
-#     "staterror",
-# ]
-# modifier_pdf_types = ["normal", None, "normal", None, "poisson", "normal"]
+import pyhf
 
 
 def test_shapefactor_build():
