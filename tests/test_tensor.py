93a13186efb72dc1d54ca445e01cd331534d3ebf
Matthew Feickert <matthew.feickert@cern.ch>
2022-02-04 01:35:37 -0600

    [ci all]: Add xfail test to know if stops failing

--- tests/test_tensor.py
+++ tests/test_tensor.py
@@ -1,3 +1,4 @@
+from sys import platform
 import pytest
 import logging
 import numpy as np
@@ -87,6 +88,17 @@ def test_simple_tensor_ops(backend):
         [2.0, 5.0],
         [3.0, 6.0],
     ]
+
+
+@pytest.mark.xfail(platform == "darwin", reason="c.f. Issue #1759")
+@pytest.mark.only_tensorflow
+def test_simple_tensor_ops_floating_point(backend):
+    """
+    xfail test to know if test_simple_tensor_ops stops failing for tensorflow
+    on macos
+    """
+    tb = pyhf.tensorlib
+    assert tb.tolist(tb.log(tb.exp(tb.astensor([2, 3, 4])))) == [2, 3, 4]
 
 
 def test_tensor_where_scalar(backend):
