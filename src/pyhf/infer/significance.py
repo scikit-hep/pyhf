ca5a1ac0e70424bdf3770d9604d2d651dc4a923a
Matthew Feickert <matthew.feickert@cern.ch>
2022-03-11 03:27:35 -0600

    Adandon refactor in favor of kwarg only arguments

--- src/pyhf/infer/significance.py
+++ src/pyhf/infer/significance.py
@@ -1,6 +1,7 @@
 """Significance calculations under Normal hypothesis"""
-from pyhf.infer import hypotest
 from scipy.stats import norm
+
+from pyhf.infer import hypotest
 
 __all__ = ["discovery_significance"]
 
@@ -9,53 +10,39 @@ def __dir__():
     return __all__
 
 
-from pyhf.infer import _check_hypotest_prerequisites
-from pyhf.infer import utils
+from pyhf.infer import _check_hypotest_prerequisites, utils
 
 
-def _minimum_hypotest_api(
+def _minimum_discovery_significance(
     data,
     model,
+    *,
     init_pars=None,
     par_bounds=None,
     fixed_params=None,
     calctype="asymptotics",
-    **kwargs,
+    return_expected=True,
+    return_observed=False,
+    **hypotest_kwargs,
 ):
+    r"""
+    Docstring this
+    """
     init_pars = init_pars or model.config.suggested_init()
     par_bounds = par_bounds or model.config.suggested_bounds()
     fixed_params = fixed_params or model.config.suggested_fixed()
 
     _check_hypotest_prerequisites(model, data, init_pars, par_bounds, fixed_params)
 
-    return utils.create_calculator(
+    calculator = utils.create_calculator(
         calctype,
         data,
         model,
         init_pars,
         par_bounds,
         fixed_params,
-        **kwargs,
-    )
-
-
-def _minimum_discovery_significance(
-    data, model, return_expected=True, return_observed=False, **hypotest_kwargs
-):
-
-    init_pars = hypotest_kwargs.pop("init_pars", None)
-    par_bounds = hypotest_kwargs.pop("par_bounds", None)
-    fixed_params = hypotest_kwargs.pop("fixed_params", None)
-    calctype = hypotest_kwargs.pop("calctype", "asymptotics")
-
-    calculator = _minimum_hypotest_api(
-        data,
-        model,
-        init_pars=init_pars,
-        par_bounds=par_bounds,
-        fixed_params=fixed_params,
-        calctype=calctype,
         test_stat="q0",
+        **hypotest_kwargs,
     )
 
     test_poi = 0.0  # discovery
