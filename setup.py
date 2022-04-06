a27c4b598b438999b7e5aee5eba0a5bd2ef13753
Matthew Feickert <matthew.feickert@cern.ch>
2022-04-05 21:55:18 -0500

    Restrict papermill

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ extras_require['test'] = sorted(
             'pytest-console-scripts',
             'pytest-mpl',
             'pydocstyle',
-            'papermill~=2.0',
+            'papermill~=2.3.4',
             'scrapbook~=0.5.0',
             'jupyter',
             'graphviz',
