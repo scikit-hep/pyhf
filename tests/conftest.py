38253e97295611252b26db4bc51f351714fa9ccf
Matthew Feickert <matthew.feickert@cern.ch>
2022-04-13 23:45:31 -0500

    refactor: Deprecate distutils in favor of setuptools._distutils
    * Use setuptools._distutils over distutils as distutils is deprecated in
    Python 3.10 and scheduled for removal in Python 3.12 as noted in the pip v21.3
    release notes.
       - c.f. https://pip.pypa.io/en/stable/news/#v21-3

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ import sys
 import tarfile
 import json
 import pathlib
-import distutils.dir_util
+from setuptools._distutils import dir_util
 
 
 # Factory as fixture pattern
@@ -134,7 +134,7 @@ def datadir(tmp_path, request):
     test_dir = pathlib.Path(request.module.__file__).with_suffix('')
 
     if test_dir.is_dir():
-        distutils.dir_util.copy_tree(test_dir, str(tmp_path))
+        dir_util.copy_tree(test_dir, str(tmp_path))
         # shutil is nicer, but doesn't work: https://bugs.python.org/issue20849
         # Once pyhf is Python 3.8+ only then the below can be used.
         # shutil.copytree(test_dir, tmpdir)
