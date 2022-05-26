3f6bf79f80de91a99d216c71378abc1edafd2db7
Matthew Feickert <matthew.feickert@cern.ch>
2022-05-25 21:31:47 -0500

    test: Ignore EPJ Web of Conferences DOI URLs during Sphinx linkcheck

--- docs/conf.py
+++ docs/conf.py
@@ -468,5 +468,6 @@ linkcheck_ignore = [
     'cli.html#pyhf-xml2json',
     # https://doi.org/10.31526/lhep.2020.158 is causing linkcheck connection timeouts in CI
     r'https://doi\.org/10\.31526/.*',
+    r'https://doi\.org/10\.1051/epjconf/.*',
 ]
 linkcheck_retries = 50
