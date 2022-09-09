8f9b799c2fc986b9cc37c82edb7dd0962feafebf
Matthew Feickert <matthew.feickert@cern.ch>
2022-09-09 16:15:44 +0200

    Revert "Use reverse condition '>'"
    This reverts commit 4145b4d5e64c1d0c3b404e6453bb611a739aef0a.

--- src/pyhf/infer/calculators.py
+++ src/pyhf/infer/calculators.py
@@ -418,11 +418,9 @@ class AsymptoticCalculator:
                 teststat = (qmu - qmu_A) / (2 * self.sqrtqmuA_v)
                 return teststat
 
-            # Use '>' rather than reverse condition with '<=' to avoid
-            # equating floating point numbers.
-            # This comparison is done to avoid Issue #1992.
+            # Use '<=' rather than '<' to avoid Issue #1992
             teststat = tensorlib.conditional(
-                (self.sqrtqmuA_v > sqrtqmu_v), _true_case, _false_case
+                (sqrtqmu_v <= self.sqrtqmuA_v), _true_case, _false_case
             )
         return tensorlib.astensor(teststat)
 
