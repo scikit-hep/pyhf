d7baca67294bc3e56f4117e94d77542455da9bd9
Matthew Feickert <matthew.feickert@cern.ch>
2022-09-20 18:58:16 -0500

    DEBUG: IF THIS WORKS BUG IN tensor/manager.py
    Try to force it by manually setting jax backend

--- src/pyhf/infer/intervals/upper_limits.py
+++ src/pyhf/infer/intervals/upper_limits.py
@@ -229,6 +229,7 @@ def upper_limit(
         array(1.01764089)
         >>> exp_limits
         [array(0.59576921), array(0.76169166), array(1.08504773), array(1.50170482), array(2.06654952)]
+        >>> pyhf.set_backend("jax")
 
     Args:
         data (:obj:`tensor`): The observed data.
