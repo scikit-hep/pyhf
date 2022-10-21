64e1e767e0bed9392b5dec51c48a101fe3a27d17
Matthew Feickert <matthew.feickert@cern.ch>
2022-10-21 18:27:18 -0500

    Also pyhf.pdf

--- src/pyhf/pdf.py
+++ src/pyhf/pdf.py
@@ -594,11 +594,15 @@ class _MainModel:
 
         if self.clip_sample_data is not None:
             log.warning(
-                f"Clipping expected data per-bin for each sample below {self.clip_sample_data}"
+                f"Clipping expected data per-bin for each sample below {self.clip_sample_data}",
+                stacklevel=2,
             )
 
         if self.clip_bin_data is not None:
-            log.warning(f"Clipping expected data per-bin below {self.clip_bin_data}")
+            log.warning(
+                f"Clipping expected data per-bin below {self.clip_bin_data}",
+                stacklevel=2,
+            )
 
         self._nominal_rates = default_backend.tile(
             nominal_rates, (1, 1, self.batch_size or 1, 1)
