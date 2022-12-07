b15850cfb6739ecfd1056cdc30b0c595d15bb0b7
Giordon Stark <kratsg@gmail.com>
2022-12-06 16:00:00 -0800

    add 1.1.0 and keep latest schema version at 1.0.0

--- src/pyhf/schema/variables.py
+++ src/pyhf/schema/variables.py
@@ -13,7 +13,7 @@ schemas: Traversable = resources.files('pyhf') / "schemas"
 SCHEMA_CACHE: dict[str, Schema] = {}
 SCHEMA_BASE = "https://scikit-hep.org/pyhf/schemas/"
 SCHEMA_VERSION: dict[str, SchemaVersion] = {
-    'model.json': '1.0.1',
-    'workspace.json': '1.0.1',
-    'patchset.json': '1.0.1',
+    'model.json': '1.0.0',
+    'workspace.json': '1.0.0',
+    'patchset.json': '1.0.0',
 }
