f05cb95e071943e91b38e256e941322f2b6ec0f2
Giordon Stark <kratsg@gmail.com>
2023-02-02 10:28:35 -0800

    fix and use context manager

--- tests/test_upgrade.py
+++ tests/test_upgrade.py
@@ -11,10 +11,12 @@ def test_upgrade_bad_version(datadir):
 
 
 def test_upgrade_to_latest(datadir):
-    ws = json.load(open(datadir.joinpath("workspace_1.0.0.json"), encoding="utf-8"))
+    with datadir.joinpath("workspace_1.0.0.json").open(encoding="utf-8") as fp:
+        ws = json.load(fp)
     pyhf.schema.upgrade().workspace(ws)
 
-    ps = json.load(open(datadir.joinpath("workspace_1.0.0.json"), encoding="utf-8"))
+    with datadir.joinpath("workspace_1.0.0.json").open(encoding="utf-8") as fp:
+        ps = json.load(fp)
     pyhf.schema.upgrade().patchset(ps)
 
 
@@ -22,7 +24,8 @@ def test_1_0_0_workspace(datadir, caplog, monkeypatch):
     """
     Test upgrading a workspace from 1.0.0 to 1.0.1
     """
-    spec = json.load(open(datadir.joinpath("workspace_1.0.0.json"), encoding="utf-8"))
+    with datadir.joinpath("workspace_1.0.0.json").open(encoding="utf-8") as fp:
+        spec = json.load(fp)
 
     monkeypatch.setitem(pyhf.schema.versions, 'workspace.json', '1.0.1')
     with caplog.at_level(logging.INFO, 'pyhf.schema'):
@@ -39,7 +42,8 @@ def test_1_0_0_patchset(datadir, caplog, monkeypatch):
     """
     Test upgrading a patchset from 1.0.0 to 1.0.1
     """
-    spec = json.load(open(datadir.joinpath("patchset_1.0.0.json"), encoding="utf-8"))
+    with datadir.joinpath("patchset_1.0.0.json").open(encoding="utf-8") as fp:
+        spec = json.load(fp)
 
     monkeypatch.setitem(pyhf.schema.versions, 'patchset.json', '1.0.1')
     with caplog.at_level(logging.INFO, 'pyhf.schema'):
@@ -56,7 +60,8 @@ def test_1_0_1_workspace(datadir):
     """
     Test upgrading a workspace from 1.0.1 to 1.0.1
     """
-    spec = json.load(open(datadir.joinpath("workspace_1.0.1.json"), encoding="utf-8"))
+    with datadir.joinpath("workspace_1.0.1.json").open(encoding="utf-8") as fp:
+        spec = json.load(fp)
 
     new_spec = pyhf.schema.upgrade(to_version='1.0.1').workspace(spec)
     assert new_spec['version'] == '1.0.1'
@@ -66,7 +71,8 @@ def test_1_0_1_patchset(datadir):
     """
     Test upgrading a patchset from 1.0.1 to 1.0.1
     """
-    spec = json.load(open(datadir.joinpath("patchset_1.0.1.json"), encoding="utf-8"))
+    with datadir.joinpath("patchset_1.0.1.json").open(encoding="utf-8") as fp:
+        spec = json.load(fp)
 
     new_spec = pyhf.schema.upgrade(to_version='1.0.1').patchset(spec)
     assert new_spec['version'] == '1.0.1'
