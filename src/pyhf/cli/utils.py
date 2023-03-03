5756f8e50e8da01e425794b5d425c2e616fbed5f
Matthew Feickert <matthew.feickert@cern.ch>
2023-03-03 00:39:07 -0600

    Add output-file option

--- src/pyhf/cli/utils.py
+++ src/pyhf/cli/utils.py
@@ -1,5 +1,6 @@
 """The pyhf utils CLI subcommand."""
 import logging
+from pathlib import Path
 
 import click
 from pyhf import utils
@@ -13,5 +14,21 @@ def cli():
 
 
 @cli.command()
-def debug():
-    click.echo(utils.debug_info())
+@click.option(
+    "-o",
+    "--output-file",
+    help="The location of the output file. If not specified, prints to screen.",
+    default=None,
+)
+def debug(output_file):
+    debug_info = utils.debug_info()
+
+    if output_file:
+        output_file = Path(output_file)
+        output_file.parent.mkdir(parents=True, exist_ok=True)
+
+        with open(output_file, "w+", encoding="utf-8") as out_file:
+            out_file.write(debug_info)
+        log.debug(f"Written to {output_file}")
+    else:
+        click.echo(debug_info)
