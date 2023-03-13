49c116ea066e83eccb74009c2d780e0f96a40b4f
Matthew Feickert <matthew.feickert@cern.ch>
2023-03-13 01:59:51 -0500

    docs: Add matplotlib to piplite installs for JupyterLite example (#2135)
    * To ensure that all the dependencies for examples in the JupyterLite
      are available add matplotlib to the list of libraries to install with
      piplite.

--- docs/generate_jupyterlite_iframe.py
+++ docs/generate_jupyterlite_iframe.py
@@ -4,7 +4,7 @@ import urllib.parse
 def main():
     code = """\
 import piplite
-await piplite.install(["pyhf==0.7.0"])
+await piplite.install(["pyhf==0.7.0", "matplotlib>=3.0.0"])
 %matplotlib inline
 import pyhf\
 """
