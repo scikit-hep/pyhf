cb05eff596a520a16c62055ae880b06c9e10b77c
Matthew Feickert <matthew.feickert@cern.ch>
2023-04-26 12:09:37 -0500

    Remove metadata from jupytext file

--- docs/lite/jupyterlite.py
+++ docs/lite/jupyterlite.py
@@ -1,27 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     notebook_metadata_filter: all,-jupytext.text_representation.jupytext_version
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-#   language_info:
-#     codemirror_mode:
-#       name: ipython
-#       version: 3
-#     file_extension: .py
-#     mimetype: text/x-python
-#     name: python
-#     nbconvert_exporter: python
-#     pygments_lexer: ipython3
-#     version: 3.11.1
-# ---
-
 # %% [markdown]
 # * To run the code, click on the first cell (gray box) and press <kbd>Shift</kbd>+<kbd>Enter</kbd> or click on the (Run) ▶ button to run each cell.
 # * Alternatively, from the `Run` menu select `Run All Cells`.
