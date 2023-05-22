f6ff813a978795b99445e40335233fec6e3b302c
Matthew Feickert <matthew.feickert@cern.ch>
2023-05-22 18:54:31 -0500

    Remove unpacking of command list args to pass list

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -3,5 +3,5 @@ import shlex
 
 def test_2bin_1channel(tmpdir, script_runner):
     command = f"pyhf inspect {'docs/examples/json/2-bin_1-channel.json':s}"
-    ret = script_runner.run(*shlex.split(command))
+    ret = script_runner.run(shlex.split(command))
     assert ret.success
