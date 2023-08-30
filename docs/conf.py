033ac261eb2cdb2ba850cf17c86fa7f47591dbc3
Matthew Feickert <matthew.feickert@cern.ch>
2023-08-31 00:13:51 +0100

    test: Ignore World Scientific journal URLs during Sphinx linkcheck
    * Ignore URLs for World Scientific journal article
     https://doi.org/10.1142/S0217732321020016 from Sphinx linkcheck tests.
    
     Avoids:
    
    ```
    403 Client Error: Forbidden for url:
    https://www.worldscientific.com/doi/abs/10.1142/S0217732321020016
    ```

--- docs/conf.py
+++ docs/conf.py
@@ -519,6 +519,8 @@ linkcheck_ignore = [
     r'https://indico.desy.de/event/22731/.*',
     # https://indico.belle2.org/event/8470/contributions/55871/ is frequently generating 403 Client Error
     r'https://indico.belle2.org/event/8470/.*',
+    # https://doi.org/10.1142/S0217732321020016 is frequently generating 403 Client Error
+    r'https://doi\.org/10.1142/.*',
     # CERN doesn't maintain its SSL certs well enough to not have SSLErrors
     r'https://twiki.cern.ch/.*',
     # tags for a release won't exist until it is made, but the release notes
