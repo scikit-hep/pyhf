db67210ec76007db1a5b6818644fe721662d5249
Matthew Feickert <matthew.feickert@cern.ch>
2024-03-20 13:06:03 -0500

    chore: Update services and docs to Python 3.12 as default (#2461)
    * Update to using Python 3.12 in linting and validation tools.
    * Update Binder runtime Python to Python 3.12.
    * Update Dockerfile base image to python:3.12-slim-bullseye.
    * Update the docs to advocate using Python 3.12 for development.

--- noxfile.py
+++ noxfile.py
@@ -4,10 +4,10 @@ from pathlib import Path
 
 import nox
 
-ALL_PYTHONS = ["3.8", "3.9", "3.10", "3.11"]
+ALL_PYTHONS = ["3.8", "3.9", "3.10", "3.11", "3.12"]
 
 # Default sessions to run if no session handles are passed
-nox.options.sessions = ["lint", "tests-3.11"]
+nox.options.sessions = ["lint", "tests-3.12"]
 
 
 DIR = Path(__file__).parent.resolve()
@@ -30,10 +30,10 @@ def tests(session):
 
     Examples:
 
-        $ nox --session tests --python 3.11
-        $ nox --session tests --python 3.11 -- contrib  # run the contrib module tests
-        $ nox --session tests --python 3.11 -- tests/test_tensor.py  # run specific tests
-        $ nox --session tests --python 3.11 -- coverage  # run with coverage but slower
+        $ nox --session tests --python 3.12
+        $ nox --session tests --python 3.12 -- contrib  # run the contrib module tests
+        $ nox --session tests --python 3.12 -- tests/test_tensor.py  # run specific tests
+        $ nox --session tests --python 3.12 -- coverage  # run with coverage but slower
     """
     session.install("--upgrade", "--editable", ".[all,test]")
     session.install("--upgrade", "pytest")
