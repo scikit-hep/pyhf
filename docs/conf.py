141215bb3032d7fee806136c753dcbb7da244e63
Melissa Weber Mendonça <melissawm@gmail.com>
2024-06-04 17:53:45 -0700

    Change docs theme to PyData Sphinx theme

--- docs/conf.py
+++ docs/conf.py
@@ -55,7 +55,6 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     'sphinx.ext.intersphinx',
-    'sphinx_rtd_theme',
     'sphinxcontrib.bibtex',
     'sphinx.ext.napoleon',
     'sphinx_click.ext',
@@ -243,7 +242,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'pydata_sphinx_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
