135d029fa2cb59398a9218ba1f512d9d8ff66218
Giordon Stark <kratsg@gmail.com>
2025-10-01 00:26:30 +0200

    fix: More linkcheck ignore for docs (#2607)
    * Add more linkcheck ignores to the docs
      - doi.org routes to journals.aps.org or academic.oup.com
      - fnal.gov blocks GitHub Actions
      - GitHub anchor links do not work

--- docs/conf.py
+++ docs/conf.py
@@ -529,6 +529,20 @@ linkcheck_ignore = [
     # and ReadTheDocs need to reference them
     r'https://github.com/scikit-hep/pyhf/releases/tag/.*',
     r'https://pyhf.readthedocs.io/en/.*',
+    # the following are 403s as they map to journals.aps.org or academic.oup.com
+    r'https://doi.org/10.1093/ptep/ptad144',
+    r'https://doi.org/10.1103/PhysRevD.104.055017',
+    r'https://doi.org/10.1103/PhysRevD.107.095021',
+    r'https://doi.org/10.1103/PhysRevD.108.016002',
+    r'https://doi.org/10.1103/PhysRevD.106.032005',
+    r'https://doi.org/10.1103/PhysRevLett.127.181802',
+    r'https://doi.org/10.1103/PhysRevLett.130.231801',
+    r'https://doi.org/10.1103/PhysRevLett.131.211802',
+    # FNAL blocks GitHub Actions?
+    r'https://indico.fnal.gov/event/17566/contributions/44103/',
+    r'https://indico.fnal.gov/event/17566/session/0/contribution/99',
+    # GitHub anchor links don't work
+    r'https://github.com/scikit-hep/pyhf/issues/850#issuecomment-1239975121',
 ]
 linkcheck_retries = 50
 
