a3b8da936f92f45eab8fa479b707654e3dce14cc
Giordon Stark <kratsg@gmail.com>
2025-11-04 14:31:18 -0800

    central value

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,7 +2,6 @@ import pytest
 
 import pyhf
 import pyhf.infer.calculators
-import sys
 
 
 def test_calc_dist():
@@ -86,13 +85,10 @@ def test_asymptotic_calculator_has_fitted_pars(test_stat):
             fitted_pars.free_fit_to_data
         )
         # lower tolerance for amd64 and arm64 to agree
-        # FIXME: SciPy v1.16.0 gives a different result from SciPy v1.15.3
-        # FIXME: why do mac and linux differ?
-        if sys.platform == "darwin":
-            expectation = [7.6470499e-05, 1.4997178]
-        else:
-            expectation = [7.6321608e-05, 1.4997178]
-
-        assert pytest.approx(expectation, rel=1e-3) == pyhf.tensorlib.tolist(
-            fitted_pars.free_fit_to_asimov
-        )
+        # SciPy v1.16.0 gives a different result from SciPy v1.15.3
+        #   - tune the test tolerance to cover: https://github.com/scikit-hep/pyhf/issues/2593#issuecomment-3411614219
+        # darwin: [7.6470499e-05, 1.4997178]
+        # linux:  [7.6321608e-05, 1.4997178]
+        assert pytest.approx(
+            [7.63960535e-05, 1.4997178], rel=1e-3
+        ) == pyhf.tensorlib.tolist(fitted_pars.free_fit_to_asimov)
