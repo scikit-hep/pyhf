fefe3936e639421fd848bb2abfcec9e76144a59b
Matthew Feickert <matthew.feickert@cern.ch>
2025-11-27 00:19:05 -0700

    Put newline in shell docstring and simplify logic

--- src/pyhf/cli/complete.py
+++ src/pyhf/cli/complete.py
@@ -20,13 +20,13 @@ def cli(shell):
     Bash:
       mkdir -p ~/.completions
       _PYHF_COMPLETE=bash_source pyhf > ~/.completions/pyhf-complete.sh
-      echo ". ~/.completions/pyhf-complete.sh" >> ~/.bashrc
+      echo -e "\n. ~/.completions/pyhf-complete.sh" >> ~/.bashrc
 
     \b
     Zsh:
       mkdir -p ~/.completions
       _PYHF_COMPLETE=zsh_source pyhf > ~/.completions/pyhf-complete.zsh
-      echo ". ~/.completions/pyhf-complete.zsh" >> ~/.zshrc
+      echo -e "\n. ~/.completions/pyhf-complete.zsh" >> ~/.zshrc
 
     \b
     Fish:
@@ -38,29 +38,25 @@ def cli(shell):
 
     click.echo(f"To enable {shell} completion for pyhf run in your {shell} shell:\n")
 
-    if shell == "bash":
-        click.echo(
-            click.style(
-                "mkdir -p ~/.completions\n"
-                + "_PYHF_COMPLETE=bash_source pyhf > ~/.completions/pyhf-complete.sh\n"
-                + 'echo -e "\\n. ~/.completions/pyhf-complete.sh" >> ~/.bashrc\n',
-                bold=True,
-            )
+    instructions = {
+        "bash": (
+            "mkdir -p ~/.completions\n"
+            "_PYHF_COMPLETE=bash_source pyhf > ~/.completions/pyhf-complete.sh\n"
+            'echo -e "\\n. ~/.completions/pyhf-complete.sh" >> ~/.bashrc\n'
+        ),
+        "zsh": (
+            "mkdir -p ~/.completions\n"
+            "_PYHF_COMPLETE=zsh_source pyhf > ~/.completions/pyhf-complete.zsh\n"
+            'echo -e "\\n. ~/.completions/pyhf-complete.zsh" >> ~/.zshrc\n'
+        ),
+        "fish": (
+            "_PYHF_COMPLETE=fish_source pyhf >> ~/.config/fish/completions/pyhf.fish\n"
+        ),
+    }
+    click.echo(
+        click.style(
+            instructions[shell],
+            bold=True,
         )
-    elif shell == "zsh":
-        click.echo(
-            click.style(
-                "mkdir -p ~/.completions\n"
-                + "_PYHF_COMPLETE=zsh_source pyhf > ~/.completions/pyhf-complete.zsh\n"
-                + 'echo -e "\\n. ~/.completions/pyhf-complete.zsh" >> ~/.zshrc\n',
-                bold=True,
-            )
-        )
-    elif shell == "fish":
-        click.echo(
-            click.style(
-                "_PYHF_COMPLETE=fish_source pyhf >> ~/.config/fish/completions/pyhf.fish\n",
-                bold=True,
-            )
-        )
+    )
     click.echo("and then source your shell configuration or restart your shell.")
