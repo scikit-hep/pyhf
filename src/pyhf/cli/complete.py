86c214d0ce6be996e387e92ea1c221f612a3c507
Matthew Feickert <matthew.feickert@cern.ch>
2025-12-18 12:46:09 -0700

    fix: Use Click v8.x native shell completion (#2639)
    * Use click v8.x's native shell completions to produce pyhf shell completions for the CLI API.
       - c.f. https://click.palletsprojects.com/en/stable/shell-completion/
    * Remove shellcomplete extra.
    * Update tests for CLI API.
    * Update documentation for producing shell completions.

--- src/pyhf/cli/complete.py
+++ src/pyhf/cli/complete.py
@@ -1,30 +1,50 @@
-'''Shell completions for pyhf.'''
+"""Shell completions for pyhf."""
 
 import click
 
-try:
-    import click_completion
 
-    click_completion.init()
+@click.command(name="completions")
+@click.argument(
+    "shell",
+    required=False,
+    type=click.Choice(["bash", "zsh", "fish"], case_sensitive=False),
+)
+def cli(shell):
+    """
+    Generate shell completion code for various shells.
 
-    @click.command(help='Generate shell completion code.', name='completions')
-    @click.argument(
-        'shell',
-        required=False,
-        type=click_completion.DocumentedChoice(click_completion.core.shells),
-    )
-    def cli(shell):
-        '''Generate shell completion code for various shells.'''
-        click.echo(click_completion.core.get_code(shell, prog_name='pyhf'))
+    The necessary commands to enable completions for pyhf for the specified
+    shell will be printed to stdout.
+    """
+    if shell is None:
+        click.echo(cli.get_help(click.Context(cli)))
+        return
 
-except ImportError:
+    click.echo(f"To enable {shell} completion for pyhf run in your {shell} shell:\n")
 
-    @click.command(help='Generate shell completion code.', name='completions')
-    @click.argument('shell', default=None)
-    def cli(shell):
-        """Placeholder for shell completion code generatioon function if necessary dependency is missing."""
-        click.secho(
-            "This requires the click_completion module.\n"
-            "You can install it with the shellcomplete extra:\n"
-            "python -m pip install 'pyhf[shellcomplete]'"
+    instructions = {
+        "bash": (
+            "mkdir -p ~/.completions\n"
+            "_PYHF_COMPLETE=bash_source pyhf > ~/.completions/pyhf-complete.sh\n"
+            'echo -e "\\n. ~/.completions/pyhf-complete.sh" >> ~/.bashrc\n'
+        ),
+        "zsh": (
+            "mkdir -p ~/.completions\n"
+            "_PYHF_COMPLETE=zsh_source pyhf > ~/.completions/pyhf-complete.zsh\n"
+            'echo -e "\\n. ~/.completions/pyhf-complete.zsh" >> ~/.zshrc\n'
+        ),
+        "fish": (
+            "_PYHF_COMPLETE=fish_source pyhf >> ~/.config/fish/completions/pyhf.fish\n"
+        ),
+    }
+    click.echo(
+        click.style(
+            instructions[shell],
+            bold=True,
         )
+    )
+    click.echo(
+        "and then source your shell configuration or restart your shell."
+        + "\nPressing tab twice (<TAB><TAB>) will show all available subcommands."
+        + "\nOptions are only listed if at least a dash has been entered (-<TAB><TAB>)."
+    )
